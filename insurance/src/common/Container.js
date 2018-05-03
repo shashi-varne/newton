27be0c50d0e0e84b5caa7eee05af5b4d93285ea2
Yatish <yateesh@fisdom.com>
2018-05-03 11:05:02 +0530

    Fix footer jump

--- insurance/src/common/Container.js
+++ insurance/src/common/Container.js
@@ -38,14 +38,16 @@ class Container extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.history.location.pathname === '/resume' || this.props.history.location.pathname === '/summary') {
-      var body = document.getElementsByTagName('body')[0].offsetHeight;
-      var client = document.getElementsByClassName('ContainerWrapper')[0].offsetHeight;
+    if (prevProps.history.location.pathname !== this.props.history.location.pathname) {  
+      if (this.props.history.location.pathname === '/resume' || this.props.history.location.pathname === '/summary') {
+        var body = document.getElementsByTagName('body')[0].offsetHeight;
+        var client = document.getElementsByClassName('ContainerWrapper')[0].offsetHeight;
 
-      if (client > body) {
-        document.getElementsByClassName('Footer')[0].style.position = "relative" ;
-      } else {
-        document.getElementsByClassName('Footer')[0].style.position = "fixed" ;
+        if (client > body) {
+          document.getElementsByClassName('Footer')[0].style.position = "relative" ;
+        } else {
+          document.getElementsByClassName('Footer')[0].style.position = "fixed" ;
+        }
       }
     }
   }
