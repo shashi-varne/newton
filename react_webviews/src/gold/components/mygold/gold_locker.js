df549cc7d43e5b949bfb037a15e4fd79ee924fcf
Vinod Jat <vinod@fisdom.com>
2019-01-16 17:34:05 +0530

    gold locker and registeration
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/gold/components/mygold/gold_locker.js
+++ react_webviews/src/gold/components/mygold/gold_locker.js
@@ -4,13 +4,40 @@ import qs from 'qs';
 import Container from '../../common/Container';
 import Api from 'utils/api';
 import { nativeCallback } from 'utils/native_callback';
+<<<<<<< 7dfad58e5bbc884c903cd51a3e624f448a5ec32d
 import safegold_logo from 'assets/safegold_logo_60x60.png';
+=======
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+>>>>>>> gold locker and registeration
 
-class GoldLocker extends Component {
+class GoldSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
       show_loader: true,
+      openDialog: false,
+      openPopup: false,
+      popupText: '',
+      apiError: '',
+      goldInfo: {},
+      userInfo: {},
+      goldSellInfo: {},
+      gold_products: {},
+      maxWeight: '',
+      maxAmount: '',
+      isRegistered: false,
+      isWeight: false,
+      isAmount: false,
+      amountError: false,
+      weightError: false,
+      weight: '',
+      amount: '',
       params: qs.parse(props.history.location.search.slice(1)),
       isPrime: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("mypro.fisdom.com") >= 0,
       ismyway: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("api.mywaywealth.com") >= 0,
@@ -38,6 +65,223 @@ class GoldLocker extends Component {
     this.setState({
       show_loader: false,
     });
+
+    Api.get('/api/gold/user/account').then(res => {
+      if (res.pfwresponse.status_code == 200) {
+        let result = res.pfwresponse.result;
+        let isRegistered = true;
+        if (result.gold_user_info.user_info.registration_status == "pending" ||
+          !result.gold_user_info.user_info.registration_status ||
+          result.gold_user_info.is_new_gold_user) {
+          isRegistered = false;
+        }
+        this.setState({
+          show_loader: false,
+          goldInfo: result.gold_user_info.safegold_info,
+          userInfo: result.gold_user_info.user_info,
+          maxWeight: parseFloat(((30 - result.gold_user_info.safegold_info.gold_balance) || 30).toFixed(4)),
+          isRegistered: isRegistered
+        });
+      } else {
+        this.setState({
+          show_loader: false, openDialog: true,
+          apiError: res.pfwresponse.result.error || res.pfwresponse.result.message
+        });
+      }
+
+    }).catch(error => {
+      this.setState({ show_loader: false });
+      console.log(error);
+    });
+
+    Api.get('/api/gold/sell/currentprice').then(res => {
+      if (res.pfwresponse.status_code == 200) {
+        let goldInfo = this.state.goldInfo;
+        let result = res.pfwresponse.result;
+        goldInfo.sell_value = ((result.sell_info.plutus_rate) * (goldInfo.gold_balance || 0)).toFixed(2) || 0;
+        this.setState({
+          show_loader: false,
+          goldSellInfo: result.sell_info,
+          goldInfo: goldInfo
+        });
+      } else {
+        this.setState({
+          show_loader: false, openDialog: true,
+          apiError: res.pfwresponse.result.error || res.pfwresponse.result.message
+        });
+      }
+
+    }).catch(error => {
+      this.setState({ show_loader: false });
+      console.log(error);
+    });
+
+    Api.get('/api/gold/user/sell/balance').then(res => {
+
+      if (res.pfwresponse.status_code == 200) {
+        let result = res.pfwresponse.result;
+        let maxWeight = result.sellable_gold_balance || 0;
+        let maxAmount = ((this.state.goldSellInfo.plutus_rate) * (maxWeight)).toFixed(2);
+
+        this.setState({
+          show_loader: false,
+          maxWeight: maxWeight,
+          maxAmount: maxAmount
+        });
+      } else {
+        this.setState({
+          show_loader: false, openDialog: true,
+          apiError: res.pfwresponse.result.error || res.pfwresponse.result.message
+        });
+      }
+
+    }).catch(error => {
+      this.setState({ show_loader: false });
+      console.log(error);
+    });
+
+    Api.get('/api/gold/delivery/products').then(res => {
+      if (res.pfwresponse.status_code == 200) {
+        this.setState({
+          show_loader: false,
+          gold_products: res.pfwresponse.result.safegold_products
+        });
+      } else {
+        this.setState({
+          show_loader: false, openDialog: true,
+          apiError: res.pfwresponse.result.error || res.pfwresponse.result.message
+        });
+      }
+
+    }).catch(error => {
+      this.setState({ show_loader: false });
+      console.log(error);
+    });
+
+  }
+
+  countdown() {
+    let timeAvailable = this.state.timeAvailable;
+    if (timeAvailable <= 0) {
+      this.setState({
+        minutes: '',
+        seconds: ''
+      })
+      window.location.reload();
+      return;
+    }
+
+    setTimeout(
+      function () {
+        let minutes = Math.floor(timeAvailable / 60);
+        let seconds = Math.floor(timeAvailable - minutes * 60);
+        timeAvailable--;
+        this.setState({
+          timeAvailable: timeAvailable,
+          minutes: minutes,
+          seconds: seconds
+        })
+        this.countdown();
+      }
+        .bind(this),
+      3000
+    );
+  };
+
+  calculate_gold_wt(current_gold_price, tax, buy_price) {
+    tax = 1.0 + parseFloat(tax) / 100.0
+    var current_gold_price_with_tax = (current_gold_price * tax).toFixed(2);
+    var gold_wt = (buy_price / current_gold_price_with_tax).toFixed(4);
+    return gold_wt
+
+  }
+
+  calculate_gold_amount(current_gold_price, tax, weight) {
+    console.log(current_gold_price);
+    tax = 1.0 + parseFloat(tax) / 100.0
+    var current_gold_price_with_tax = (current_gold_price * tax).toFixed(2)
+    var gold_amount = (weight * current_gold_price_with_tax).toFixed(2);
+    return gold_amount
+  }
+
+  buyGold = async (amount, weight) => {
+
+    if (!weight || weight < 0) {
+      // toast('Please enter a correct value for the weight');
+      return;
+    }
+
+    if (!amount || amount < 0) {
+      // toast('Please enter a correct value for the amount');
+      return;
+    }
+
+    if (amount >= 0 && amount < 1) {
+      // toast('Minimum amount should be Rs. 1');
+      return;
+    }
+
+    if (amount > parseFloat(this.state.maxAmount) ||
+      weight > parseFloat(this.state.maxWeight)) {
+      // toast("You don't have enough gold");
+      return;
+    }
+
+    let goldSellInfo = this.state.goldSellInfo;
+    goldSellInfo.amount = amount;
+    goldSellInfo.weight = weight;
+    goldSellInfo.isAmount = this.state.isAmount;
+
+    this.setState({
+      show_loader: true,
+      goldSellInfo: goldSellInfo
+    });
+
+
+    window.localStorage.setItem('timeAvailableSell', this.state.timeAvailable);
+    window.localStorage.setItem('sellData', goldSellInfo);
+
+    this.navigate('bank-details');
+    this.setState({
+      show_loader: false
+    });
+
+  }
+
+  selectGoldProduct(index) {
+    let selectedProduct = this.state.gold_products[index];
+    window.localStorage.setItem('goldProduct', selectedProduct);
+    this.navigate('select-gold-product');
+  };
+
+
+  handleClose = () => {
+    this.setState({
+      openDialog: false,
+      openPopup: false
+    });
+  }
+
+  renderResponseDialog = () => {
+    return (
+      <Dialog
+        open={this.state.openDialog}
+        onClose={this.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {this.state.apiError}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary" autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
   }
 
   navigate = (pathname) => {
@@ -61,6 +305,7 @@ class GoldLocker extends Component {
         buttonTitle="Proceed"
         type={this.state.type}
       >
+<<<<<<< 7dfad58e5bbc884c903cd51a3e624f448a5ec32d
         <div className="FlexRow">
           <div className="FlexRow" style={{justifyContent: 'flex-start', flex: 1, marginRight: '2px', background: '#fff', padding: '10px'}}>
             <img className="img-mygold" src={safegold_logo} width="35" style={{marginRight: 10}} />
@@ -118,9 +363,12 @@ class GoldLocker extends Component {
             </div>
           </div>
         </div>
+=======
+        {this.renderResponseDialog()}
+>>>>>>> gold locker and registeration
       </Container>
     );
   }
 }
 
-export default GoldLocker;
+export default GoldSummary;
