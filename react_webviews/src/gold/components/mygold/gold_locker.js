63ab78fe21794174ce6da8c215cb0e1d171a6753
Yatish <yateesh@fisdom.com>
2019-01-16 17:33:47 +0530

    Add buy-summary and buy-locker screen

--- react_webviews/src/gold/components/mygold/gold_locker.js
+++ react_webviews/src/gold/components/mygold/gold_locker.js
@@ -4,6 +4,7 @@ import qs from 'qs';
 import Container from '../../common/Container';
 import Api from 'utils/api';
 import { nativeCallback } from 'utils/native_callback';
+import safegold_logo from 'assets/safegold_logo_60x60.png';
 
 class GoldLocker extends Component {
   constructor(props) {
@@ -60,7 +61,63 @@ class GoldLocker extends Component {
         buttonTitle="Proceed"
         type={this.state.type}
       >
-        <div>Lorum Ipsum</div>
+        <div className="FlexRow">
+          <div className="FlexRow" style={{justifyContent: 'flex-start', flex: 1, marginRight: '2px', background: '#fff', padding: '10px'}}>
+            <img className="img-mygold" src={safegold_logo} width="35" style={{marginRight: 10}} />
+            <div>
+              <div className="grey-color" style={{marginBottom: 5}}>Gold Quantity</div>
+              <div>3.3329 gm</div>
+            </div>
+          </div>
+          <div style={{flex: 1, background: '#fff', padding: '10px'}}>
+            <div className="grey-color" style={{marginBottom: 5}}>Gold Value</div>
+            <div>₹ 10,498.64</div>
+          </div>
+        </div>
+        <div className="page home" id="goldSection">
+          <div className="page-body-gold" id="goldInput">
+            <div className="buy-info1">
+              <div className="FlexRow">
+                <span className="buy-info2a">Current Buying Price</span>
+                <span className="buy-info2b">Price valid for
+                  <span className="timer-green">1:45</span>
+                </span>
+              </div>
+              <div className="buy-info3">
+                ₹ 3,998.25/gm
+              </div>
+            </div>
+            <div className="buy-input">
+              <div className="buy-input1">
+                Enter amount of gold you want to buy
+              </div>
+              <div className="label">
+                <div className="FlexRow">
+                  <div>
+                    <div className="input-above-text">In Rupees (₹)</div>
+                    <div className="input-box">
+                      <input type="text" placeholder="Amount" />
+                    </div>
+                    <div className="input-below-text">Min ₹1.00 - *Max ₹-0.757.95</div>
+                  </div>
+                  <div className="symbol">
+                    =
+                  </div>
+                  <div>
+                    <div className="input-above-text">In Grams (gm)</div>
+                    <div className="input-box">
+                      <input type="text" placeholder="Weight" />
+                    </div>
+                    <div className="input-below-text">*Max -0.234 gm</div>
+                  </div>
+                </div>
+                <div className="disclaimer">
+                  *You can place your order for sell/delivery after 2 working day from your buying transaction date
+                </div>
+              </div> 
+            </div>
+          </div>
+        </div>
       </Container>
     );
   }
--- react_webviews/src/gold/components/mygold/gold_summary.js
+++ react_webviews/src/gold/components/mygold/gold_summary.js
@@ -4,6 +4,8 @@ import qs from 'qs';
 import Container from '../../common/Container';
 import Api from 'utils/api';
 import { nativeCallback } from 'utils/native_callback';
+import safegold_logo from 'assets/safegold_logo_60x60.png';
+import arrow from 'assets/arrow.png';
 
 class GoldSummary extends Component {
   constructor(props) {
@@ -60,7 +62,70 @@ class GoldSummary extends Component {
         buttonTitle="Proceed"
         type={this.state.type}
       >
-        <div>Lorum Ipsum</div>
+        <div className="page home" id="goldSection">
+          <div className="text-center goldheader">
+            <div className="my-gold-header">
+              <div className="FlexRow row1" >
+                <img className="img-mygold" src={safegold_logo} />
+                <span className="my-gold-title-header">My 24K Safegold Gold Locker</span>
+                <img  className="img-mygold2" src={arrow} />
+              </div>
+              <div className="spacer-header"></div>
+              <div className="my-gold-details-header1">
+                <div className="my-gold-details-header2">
+                  <div className="my-gold-details-header2a">Weight</div>
+                  <div className="my-gold-details-header2b">3.3266 gm</div>
+                </div>
+                <div className="my-gold-details-header3">
+                  <div className="my-gold-details-header2a">Selling Value</div>
+                  <div className="my-gold-details-header2b">₹ 10,325.64</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="page-body-gold" id="goldInput">
+            <div className="buy-info1">
+              <div className="FlexRow">
+                <span className="buy-info2a">Current Buying Price</span>
+                <span className="buy-info2b">Price valid for
+                  <span className="timer-green">1:45</span>
+                </span>
+              </div>
+              <div className="buy-info3">
+                ₹ 3,998.25/gm
+              </div>
+              <div className="buy-info4">
+                All prices are inclusive of 3% GST
+              </div>
+            </div>
+            <div className="buy-input">
+              <div className="buy-input1">
+                Enter amount of gold you want to buy
+              </div>
+              <div className="label">
+                <div className="FlexRow">
+                  <div>
+                    <div className="input-above-text">In Rupees (₹)</div>
+                    <div className="input-box">
+                      <input type="text" placeholder="Amount" />
+                    </div>
+                    <div className="input-below-text">Min ₹1.00</div>
+                  </div>
+                  <div className="symbol">
+                    =
+                  </div>
+                  <div>
+                    <div className="input-above-text">In Grams (gm)</div>
+                    <div className="input-box">
+                      <input type="text" placeholder="Weight" />
+                    </div>
+                    <div className="input-below-text">Max 00.000 gm</div>
+                  </div>
+                </div>
+              </div> 
+            </div>
+          </div>
+        </div>
       </Container>
     );
   }
