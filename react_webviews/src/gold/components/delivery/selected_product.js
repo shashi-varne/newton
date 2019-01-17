f53719db4bd98e5732fde724d8c871504ccc0895
Yatish <yateesh@fisdom.com>
2019-01-17 17:41:52 +0530

    Add delivery select screen

--- react_webviews/src/gold/components/delivery/selected_product.js
+++ react_webviews/src/gold/components/delivery/selected_product.js
@@ -4,6 +4,13 @@ import qs from 'qs';
 import Container from '../../common/Container';
 import Api from 'utils/api';
 import { nativeCallback } from 'utils/native_callback';
+import one_gm_front from 'assets/1gm_front.png';
+import two_gm_front from 'assets/2gm_front.png';
+import five_gm_front from 'assets/5gm_front.png';
+import five_gmbar_front from 'assets/5gmbar_front.png';
+import ten_gm_front from 'assets/10gm_front.png';
+import ten_gmbar_front from 'assets/10gmbar_front.png';
+import twenty_gmbar_front from 'assets/20gmbar_front.png';
 
 class DeliverySelectedProduct extends Component {
   constructor(props) {
@@ -50,6 +57,22 @@ class DeliverySelectedProduct extends Component {
     this.navigate('my-gold');
   }
 
+   productImgMap = () => {
+    const prod_image_map = {
+      2: one_gm_front,
+      3: two_gm_front,
+      1: five_gm_front,
+      14: five_gmbar_front,
+      8: ten_gm_front,
+      12: ten_gmbar_front,
+      15: twenty_gmbar_front,
+    };
+    
+    return (
+      <img alt="Gold" className="delivery-icon" src={prod_image_map[3]} width="200" />
+    );
+  }
+
   render() {
     return (
       <Container
@@ -60,7 +83,66 @@ class DeliverySelectedProduct extends Component {
         buttonTitle="Proceed"
         type={this.state.type}
       >
-        <div>Lorum Ipsum</div>
+        <div className="delivery block">
+          <div className="delivery-select-logo">
+            {this.productImgMap()}
+          </div>
+          <div className="">
+            <div>2gm SafeGold Gold Coin</div>
+            <div style={{fontSize: '16px', color: 'black', marginTop: '5px'}}>Charges 390</div>
+          </div>          
+          <div className="seller-name">
+            Seller : Digital Gold India Private Limited
+          </div>
+          <div className="instock">
+            <span className="green">*(In Stock)</span>
+            <span className="red">(Out of Stock)</span>
+          </div>
+        </div>
+
+        <div className="delivery block">
+          <div className="product-details-heading">
+            Product Details
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Model</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Metal Purity</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Packaging</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Weight</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Est. Arrival</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+          <div class="product-details-content">
+            <div className="product-name">Refund Policy</div>
+            <div className="product-value">: 21dd</div>
+          </div>
+        </div>
+
+        <div className="delivery block">
+          <div className="product-details-heading">
+            Product Hightlight
+          </div>
+          <ul>
+            <li>SafeGold coins are set in 24 Karat Yellow Gold</li>
+            <li>Guaranteed weight and purity: Specially manufactured in state of the art facilities, independent assay certification  with zero negative tolerance for weight and purity</li>
+            <li>Sealed in international quality packaging with a unique serial number, ensures foolproof quality and complete traceability for each coin</li>
+          </ul>
+          <div className="grey-color">
+            *You can place your order for sell/delivery after 2 working day from your buying transaction date
+          </div>
+        </div>
       </Container>
     );
   }
