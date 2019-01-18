0b2cb9e352a5f847b10ce5e2aa7f795c718d5237
Yatish <yateesh@fisdom.com>
2019-01-18 16:05:39 +0530

    Add validation for delivery address

--- react_webviews/src/gold/components/delivery/address.js
+++ react_webviews/src/gold/components/delivery/address.js
@@ -5,6 +5,7 @@ import Container from '../../common/Container';
 import Api from 'utils/api';
 import { nativeCallback } from 'utils/native_callback';
 import Input from '../../ui/Input';
+import { validateNumber, validateStreetName, validateLength, validateMinChar, validateConsecutiveChar, validateEmpty } from 'utils/validators';
 
 class DeliveryAddress extends Component {
   constructor(props) {
@@ -15,6 +16,16 @@ class DeliveryAddress extends Component {
       isPrime: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("mypro.fisdom.com") >= 0,
       ismyway: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("api.mywaywealth.com") >= 0,
       type: '',
+      address: '',
+      address_error: '',
+      landmark: '',
+      landmark_error: '',
+      pincode: '',
+      pincode_error: '',
+      ccity: '',
+      cstate: '',
+      city: '',
+      state: '',
     }
   }
 
@@ -47,43 +58,84 @@ class DeliveryAddress extends Component {
     });
   }
 
-  handleChange = (field) => (value) => {
-    // field == name
+  handleChange = (field) => (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+      [event.target.name+'_error']: ''
+    });
   }
 
   handlePincode = name => async (event) => {
-    // const pincode = event.target.value;
+    const pincode = event.target.value;
 
-    // this.setState({
-    //   [name]: pincode,
-    //   [name+'_error']: ''
-    // });
+    this.setState({
+      [name]: pincode,
+      [name+'_error']: ''
+    });
 
-    // if (pincode.length === 6) {
-    //   const res = await Api.get('/api/pincode/' + pincode);
+    if (pincode.length === 6) {
+      const res = await Api.get('https://nitish-dot-plutus-staging.appspot.com/api/pincode/' + pincode);
 
-    //   if (res.pfwresponse.status_code === 200 && res.pfwresponse.result.length > 0) {
-    //     if (name === 'pincode') {
-    //       this.setState({
-    //         city: res.pfwresponse.result[0].taluk || res.pfwresponse.result[0].district_name,
-    //         state: res.pfwresponse.result[0].state_name
-    //       });
-    //     } else {
-    //       this.setState({
-    //         ccity: res.pfwresponse.result[0].taluk || res.pfwresponse.result[0].district_name,
-    //         cstate: res.pfwresponse.result[0].state_name
-    //       });
-    //     }
-    //   } else {
-    //     this.setState({
-    //       city: '',
-    //       state: '',
-    //       ccity: '',
-    //       cstate: ''
-    //     });
-    //   }
-    // }
+      if (res.pfwresponse.status_code === 200 && res.pfwresponse.result.length > 0) {
+        if (name === 'pincode') {
+          this.setState({
+            city: res.pfwresponse.result[0].taluk || res.pfwresponse.result[0].district_name,
+            state: res.pfwresponse.result[0].state_name
+          });
+        } else {
+          this.setState({
+            ccity: res.pfwresponse.result[0].taluk || res.pfwresponse.result[0].district_name,
+            cstate: res.pfwresponse.result[0].state_name
+          });
+        }
+      } else {
+        this.setState({
+          city: '',
+          state: '',
+          ccity: '',
+          cstate: ''
+        });
+      }
+    }
   }
+
+  handleClick = async () => {
+    if (this.state.pincode.length !== 6 || !validateNumber(this.state.pincode)) {
+      this.setState({
+        pincode_error: 'Please enter valid pincode'
+      });
+    } else if (!validateEmpty(this.state.address)) {
+      this.setState({
+        address_error: 'Enter your address'
+      });
+    } else if (!validateConsecutiveChar(this.state.address)) {
+      this.setState({
+        address_error: 'Address can not contain more than 3 same consecutive characters'
+      });
+    } else if (!validateLength(this.state.address)) {
+      this.setState({
+        address_error: 'Maximum length of address is 30'
+      });
+    } else if (!validateMinChar(this.state.address)) {
+      this.setState({
+        address_error: 'Address should contain minimum two characters'
+      });
+    } else if (!validateEmpty(this.state.landmark)) {
+      this.setState({
+        landmark_error: 'Enter nearest landmark'
+      });
+    } else if (!validateLength(this.state.landmark)) {
+      this.setState({
+        landmark_error: 'Maximum length of landmark is 30'
+      });
+    } else if (!validateStreetName(this.state.landmark)) {
+      this.setState({
+        landmark_error: 'Please enter valid landmark'
+      });
+    } else {
+      // To-do
+    }
+  } 
 
   render() {
     return (
@@ -91,46 +143,50 @@ class DeliveryAddress extends Component {
         showLoader={this.state.show_loader}
         title="Gold Delivery Address"
         edit={this.props.edit}
+        handleClick={this.handleClick}
         buttonTitle="Proceed"
         type={this.state.type}
       >
         <div className="delivery-address">
           <div className="InputField">
             <Input
-              error={false}
-              helperText=''
+              error={(this.state.pincode_error) ? true : false}
+              helperText={this.state.pincode_error}
               type="number"
               width="40"
               label="Pincode *"
               id="pincode"
               name="pincode"
-              value='560052'
+              value={this.state.pincode}
               onChange={this.handlePincode('pincode')} />
+              <div className="filler">
+                {(this.state.city && this.state.state) && <span>{this.state.city} , {this.state.state}</span>}
+              </div>
           </div>
           <div className="InputField">
             <Input
-              error={false}
-              helperText=''
+              error={(this.state.address_error) ? true : false}
+              helperText={this.state.address_error}
               type="text"
               width="40"
               label="Address *"
               class="address"
               id="address"
               name="address"
-              value='#1, 1st Cross, Some Layout, Bengaluru'
+              value={this.state.address}
               onChange={this.handleChange('address')} />
           </div>
           <div className="InputField">
             <Input
-              error={false}
-              helperText=''
+              error={(this.state.landmark_error) ? true : false}
+              helperText={this.state.landmark_error}
               type="text"
               width="40"
               label="Landmark (Optional)"
               class="landmark"
               id="landmark"
               name="landmark"
-              value='Bangalore Central'
+              value={this.state.landmark}
               onChange={this.handleChange('landmark')} />
           </div>
         </div>
