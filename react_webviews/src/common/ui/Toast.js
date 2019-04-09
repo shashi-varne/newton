5f647ba9010533b410ed1db86780e54efd6bb531
satendra_fisdom <satendra@fisdom.com>
2019-04-09 16:49:36 +0530

    cutom toast

--- react_webviews/src/common/ui/Toast.js
+++ react_webviews/src/common/ui/Toast.js
@@ -1,43 +1,47 @@
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import { nativeCallback } from 'utils/native_callback';
-import { getConfig } from '../../utils/functions';
 
 export default function (message, type = 'default') {
 
-  if (getConfig().app === 'android' || getConfig().app === 'ios') {
-    nativeCallback({
-      action: 'show_toast',
-      message: {
-        message: message
-      }
-    });
-  } else {
-    switch (type) {
-      case 'success':
-        toast.success(message, {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-        break;
-      case 'error':
-        toast.error(message, {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-        break;
-      case 'warn':
-        toast.warn(message, {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-        break;
-      case 'info':
-        toast.info(message, {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-        break;
-      default:
-        toast(message, {
-          position: toast.POSITION.BOTTOM_CENTER
-        });
-    }
+  switch (type) {
+    case 'success':
+      toast.success(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: 'custom-toast',
+        hideProgressBar: true,
+        closeButton: false
+      });
+      break;
+    case 'error':
+      toast.error(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: 'custom-toast',
+        hideProgressBar: true,
+        closeButton: false
+      });
+      break;
+    case 'warn':
+      toast.warn(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: 'custom-toast',
+        hideProgressBar: true,
+        closeButton: false
+      });
+      break;
+    case 'info':
+      toast.info(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: 'custom-toast',
+        hideProgressBar: true,
+        closeButton: false
+      });
+      break;
+    default:
+      toast(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        className: 'custom-toast',
+        hideProgressBar: true,
+        closeButton: false
+      });
   }
 }
