97c2de2773d6605e96ffc84c7560040ad47915ef
satendra_fisdom <satendra@fisdom.com>
2019-04-09 15:28:19 +0530

    show toast for web

--- react_webviews/src/common/ui/Toast.js
+++ react_webviews/src/common/ui/Toast.js
@@ -1,39 +1,43 @@
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.min.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { nativeCallback } from 'utils/native_callback';
+import { getConfig } from '../../utils/functions';
 
 export default function (message, type = 'default') {
 
-  nativeCallback({
-    action: 'show_toast',
-    message: {
-      message: message
+  if (getConfig().app === 'android' || getConfig().app === 'ios') {
+    nativeCallback({
+      action: 'show_toast',
+      message: {
+        message: message
+      }
+    });
+  } else {
+    switch (type) {
+      case 'success':
+        toast.success(message, {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+        break;
+      case 'error':
+        toast.error(message, {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+        break;
+      case 'warn':
+        toast.warn(message, {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+        break;
+      case 'info':
+        toast.info(message, {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+        break;
+      default:
+        toast(message, {
+          position: toast.POSITION.BOTTOM_CENTER
+        });
     }
-  });
-  // switch (type) {
-  //   case 'success':
-  //     toast.success(message, {
-  //       position: toast.POSITION.BOTTOM_CENTER
-  //     });
-  //     break;
-  //   case 'error':
-  //     toast.error(message, {
-  //       position: toast.POSITION.BOTTOM_CENTER
-  //     });
-  //     break;
-  //   case 'warn':
-  //     toast.warn(message, {
-  //       position: toast.POSITION.BOTTOM_CENTER
-  //     });
-  //     break;
-  //   case 'info':
-  //     toast.info(message, {
-  //       position: toast.POSITION.BOTTOM_CENTER
-  //     });
-  //     break;
-  //   default:
-  //     toast(message, {
-  //       position: toast.POSITION.BOTTOM_CENTER
-  //     });
-  // }
+  }
 }
