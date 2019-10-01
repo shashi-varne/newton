5279f3440b6a92cab3757ae3c5fb8db7bed35702
Vinod Jat <vinod@fisdom.com>
2019-10-01 12:57:14 +0530

    gold:removed intro page
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/gold/components/general/about.js
+++ react_webviews/src/gold/components/general/about.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import qs from 'qs';
+import React, { Component } from "react";
+import qs from "qs";
 
-import Container from '../../common/Container';
-import intro_gold from 'assets/intro_gold@2x.png';
-import safegold_logo from 'assets/safegold_logo@2x.png';
-import buy_gold_icon from 'assets/buy_gold_icon@2x.png';
-import track_gold_icon from 'assets/track_gold_icon@2x.png';
-import sell_gold_icon from 'assets/sell_gold_icon@2x.png';
-import deliver_gold_icon from 'assets/deliver_gold_icon@2x.png';
-import { ToastContainer } from 'react-toastify';
-import Api from 'utils/api';
-import toast from '../../../common/ui/Toast';
-import { inrFormatDecimal } from 'utils/validators';
-import { nativeCallback } from 'utils/native_callback';
-import { getConfig } from 'utils/functions';
+import Container from "../../common/Container";
+import intro_gold from "assets/intro_gold@2x.png";
+import safegold_logo from "assets/safegold_logo@2x.png";
+import buy_gold_icon from "assets/buy_gold_icon@2x.png";
+import track_gold_icon from "assets/track_gold_icon@2x.png";
+import sell_gold_icon from "assets/sell_gold_icon@2x.png";
+import deliver_gold_icon from "assets/deliver_gold_icon@2x.png";
+import { ToastContainer } from "react-toastify";
+import Api from "utils/api";
+import toast from "../../../common/ui/Toast";
+import { inrFormatDecimal } from "utils/validators";
+import { nativeCallback } from "utils/native_callback";
+import { getConfig } from "utils/functions";
 
 class About extends Component {
   constructor(props) {
@@ -21,33 +21,39 @@ class About extends Component {
     this.state = {
       show_loader: true,
       openResponseDialog: false,
-      minAmount: '',
+      minAmount: "",
       params: qs.parse(props.history.location.search.slice(1)),
-      isPrime: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("mypro.fisdom.com") >= 0,
-      ismyway: qs.parse(props.history.location.search.slice(1)).base_url.indexOf("api.mywaywealth.com") >= 0,
-      type: '',
-    }
+      isPrime:
+        qs
+          .parse(props.history.location.search.slice(1))
+          .base_url.indexOf("mypro.fisdom.com") >= 0,
+      ismyway:
+        qs
+          .parse(props.history.location.search.slice(1))
+          .base_url.indexOf("api.mywaywealth.com") >= 0,
+      type: ""
+    };
   }
 
   componentWillMount() {
     if (this.state.ismyway) {
       this.setState({
-        type: 'myway'
+        type: "myway"
       });
     } else if (this.state.isPrime) {
       this.setState({
-        type: 'Fisdom Prime'
+        type: "Fisdom Prime"
       });
     } else {
       this.setState({
-        type: 'fisdom'
+        type: "fisdom"
       });
     }
   }
 
   async componentDidMount() {
     try {
-      const res = await Api.get('/api/gold/buy/currentprice');
+      const res = await Api.get("/api/gold/buy/currentprice");
 
       if (res.pfwresponse.status_code === 200) {
         let result = res.pfwresponse.result;
@@ -55,42 +61,45 @@ class About extends Component {
           show_loader: false,
           minAmount: result.buy_info.minimum_buy_price
         });
-
       } else {
         this.setState({
           show_loader: false
         });
-        toast(res.pfwresponse.result.error || res.pfwresponse.result.message || 'Something went wrong', 'error');
+        toast(
+          res.pfwresponse.result.error ||
+            res.pfwresponse.result.message ||
+            "Something went wrong",
+          "error"
+        );
       }
     } catch (err) {
       this.setState({
         show_loader: true
       });
-      toast('Something went wrong', 'error');
+      toast("Something went wrong", "error");
     }
-
   }
 
-  navigate = (pathname) => {
-    if (pathname === 'details') {
-      this.sendEvents('know_more')
+  navigate = pathname => {
+    if (pathname === "details") {
+      this.sendEvents("know_more");
     }
     this.props.history.push({
       pathname: pathname,
       search: getConfig().searchParams
     });
-  }
+  };
 
   sendEvents(user_action) {
     let eventObj = {
-      "event_name": 'GOLD',
-      "properties": {
-        "user_action": user_action,
-        "screen_name": 'Intro'
+      event_name: "GOLD",
+      properties: {
+        user_action: user_action,
+        screen_name: "Intro"
       }
     };
 
-    if (user_action === 'just_set_events') {
+    if (user_action === "just_set_events") {
       return eventObj;
     } else {
       nativeCallback({ events: eventObj });
@@ -98,9 +107,9 @@ class About extends Component {
   }
 
   handleClick = () => {
-    this.sendEvents('next')
-    this.navigate('my-gold');
-  }
+    this.sendEvents("next");
+    this.navigate("my-gold");
+  };
 
   render() {
     return (
@@ -111,11 +120,11 @@ class About extends Component {
         handleClick={this.handleClick}
         buttonTitle="Proceed"
         type={this.state.type}
-        events={this.sendEvents('just_set_events')}
+        events={this.sendEvents("just_set_events")}
       >
         <div className="gold-about-card">
           <div className="Banner">
-            <img alt="Gold" style={{ width: '100%' }} src={intro_gold} />
+            <img alt="Gold" style={{ width: "100%" }} src={intro_gold} />
             <div className="BannerContent">
               <div className="BannerText">Introducing Gold</div>
               <div className="SafeGoldImage">
@@ -126,35 +135,44 @@ class About extends Component {
                 <div>|</div>
                 <div>99.5% Purity</div>
               </div>
-              <div className="helper" onClick={() => this.navigate('details')} >Know More</div>
+              <div className="helper" onClick={() => this.navigate("details")}>
+                Know More
+              </div>
             </div>
           </div>
           <div className="BannerBody">
             <div className="gold-about-text">
               <div className="gold-dot"></div>
               <div className="about-img-tile">
-                <span className="about-img-span2">Buy 24K gold & skip the responsibility of safe-keeping & traditional lockers.
+                <span className="about-img-span2">
+                  Buy 24K gold & skip the responsibility of safe-keeping &
+                  traditional lockers.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
               <div className="gold-dot"></div>
               <div className="about-img-tile">
-                <span className="about-img-span2">You get a free & secure locker from <span className="know-more-buy">BRINK's</span>, global leader in gold custodian services with 100% insurance cover.
+                <span className="about-img-span2">
+                  You get a free & secure locker from{" "}
+                  <span className="know-more-buy">BRINK's</span>, global leader
+                  in gold custodian services with 100% insurance cover.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
               <div className="gold-dot"></div>
               <div className="about-img-tile">
-                <span className="about-img-span2">Sell your gold with one click, anywhere and anytime.
+                <span className="about-img-span2">
+                  Sell your gold with one click, anywhere and anytime.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
               <div className="gold-dot"></div>
               <div className="about-img-tile">
-                <span className="about-img-span2">Get your gold delivered to your doorstep, hassle-free.
+                <span className="about-img-span2">
+                  Get your gold delivered to your doorstep, hassle-free.
                 </span>
               </div>
             </div>
@@ -164,33 +182,65 @@ class About extends Component {
           <div>
             <h3>How It Works?</h3>
           </div>
-          <div >
+          <div>
             <div className="gold-about-text">
-              <img alt="Gold" className="about-img" src={buy_gold_icon} width="54" />
+              <img
+                alt="Gold"
+                className="about-img"
+                src={buy_gold_icon}
+                width="54"
+              />
               <div className="about-img-tile">
-                <span className="about-img-span2"> <span className="know-more-buy">Buy</span> the desired quantity or amount of gold above {inrFormatDecimal(this.state.minAmount)} and purchase it at the live price quoted.
+                <span className="about-img-span2">
+                  {" "}
+                  <span className="know-more-buy">Buy</span> the desired
+                  quantity or amount of gold above{" "}
+                  {inrFormatDecimal(this.state.minAmount)} and purchase it at
+                  the live price quoted.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
-              <img alt="Gold" className="about-img" src={track_gold_icon} width="54" />
+              <img
+                alt="Gold"
+                className="about-img"
+                src={track_gold_icon}
+                width="54"
+              />
               <div className="about-img-tile">
-                <span className="about-img-span2"><span className="know-more-buy">Track</span> your gold value & transactions anytime from your gold locker.
+                <span className="about-img-span2">
+                  <span className="know-more-buy">Track</span> your gold value &
+                  transactions anytime from your gold locker.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
-              <img alt="Gold" className="about-img" src={sell_gold_icon} width="54" />
+              <img
+                alt="Gold"
+                className="about-img"
+                src={sell_gold_icon}
+                width="54"
+              />
               <div className="about-img-tile">
-                <span className="about-img-span2"><span className="know-more-buy">Sell</span> any amount of gold above Rs. 1 & get the amount credited in your bank account within 72 hours.
+                <span className="about-img-span2">
+                  <span className="know-more-buy">Sell</span> any amount of gold
+                  above Rs. 1 & get the amount credited in your bank account
+                  within 72 hours.
                 </span>
               </div>
             </div>
             <div className="gold-about-text">
-              <img alt="Gold" className="about-img" src={deliver_gold_icon} width="54" />
+              <img
+                alt="Gold"
+                className="about-img"
+                src={deliver_gold_icon}
+                width="54"
+              />
               <div className="about-img-tile">
-                <span className="about-img-span2">Select any gold product of your choice when you have more than 1 gm in
-                  your gold locker & track your <span className="know-more-buy">Delivery</span>
+                <span className="about-img-span2">
+                  Select any gold product of your choice when you have more than
+                  1 gm in your gold locker & track your{" "}
+                  <span className="know-more-buy">Delivery</span>
                 </span>
               </div>
             </div>
--- react_webviews/src/gold/index.js
+++ react_webviews/src/gold/index.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import {
-  Route,
-  Switch
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router";
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import '../common/theme/Style.css';
-import './common/Style.css';
-import { getConfig } from 'utils/functions';
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import "../common/theme/Style.css";
+import "./common/Style.css";
+import { getConfig } from "utils/functions";
 
-import './components/Style.css';
-import NotFound from '../common/components/NotFound';
-import About from './components/general/about';
-import KnowMore from './components/general/know_more';
-import GoldSummary from './components/mygold/gold_summary';
-import GoldLocker from './components/mygold/gold_locker';
-import Transactions from './components/transactions/index';
-import GoldRegister from './components/buy/register';
-import BuyOrder from './components/buy/order';
-import GoldBank from './components/sell/bank';
-import SellOrder from './components/sell/order';
-import DeliveryAddress from './components/delivery/address';
-import DeliveryOrder from './components/delivery/order';
-import DeliverySelectedProduct from './components/delivery/selected_product';
-import Payment from './components/payment/index';
-import Otp from './components/otp/index';
+import "./components/Style.css";
+import NotFound from "../common/components/NotFound";
+// import About from "./components/general/about";
+import KnowMore from "./components/general/know_more";
+import GoldSummary from "./components/mygold/gold_summary";
+import GoldLocker from "./components/mygold/gold_locker";
+import Transactions from "./components/transactions/index";
+import GoldRegister from "./components/buy/register";
+import BuyOrder from "./components/buy/order";
+import GoldBank from "./components/sell/bank";
+import SellOrder from "./components/sell/order";
+import DeliveryAddress from "./components/delivery/address";
+import DeliveryOrder from "./components/delivery/order";
+import DeliverySelectedProduct from "./components/delivery/selected_product";
+import Payment from "./components/payment/index";
+import Otp from "./components/otp/index";
 
-import { create } from 'jss';
-import JssProvider from 'react-jss/lib/JssProvider';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import { create } from "jss";
+import JssProvider from "react-jss/lib/JssProvider";
+import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
 
 const generateClassName = createGenerateClassName({
   dangerouslyUseGlobalCSS: true,
-  productionPrefix: 'f',
+  productionPrefix: "f"
 });
 const jss = create(jssPreset());
 // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
@@ -44,54 +41,54 @@ const theme = createMuiTheme({
       // light: will be calculated from palette.primary.main,
       main: getConfig().primary,
       // dark: will be calculated from palette.primary.main,
-      contrastText: '#ffffff',
+      contrastText: "#ffffff"
     },
     secondary: {
       // light: '#0066ff',
       main: getConfig().secondary,
       // dark: will be calculated from palette.secondary.main,
-      contrastText: '#ffffff',
+      contrastText: "#ffffff"
     },
     default: {
       // light: '#0066ff',
       main: getConfig().default,
       // dark: will be calculated from palette.secondary.main,
-      contrastText: '#ffffff',
+      contrastText: "#ffffff"
     }
     // error: will us the default color
   },
   overrides: {
     MuiFormControl: {
       root: {
-        width: '100%'
+        width: "100%"
       }
     },
     MuiInput: {
       input: {
-        padding: '11px 0 7px',
-        fontSize: '14px'
+        padding: "11px 0 7px",
+        fontSize: "14px"
       }
     },
     MuiInputLabel: {
       root: {
-        fontSize: '0.9rem',
+        fontSize: "0.9rem",
         color: getConfig().label,
-        fontWeight: 'normal'
+        fontWeight: "normal"
       },
       shrink: {
-        transform: 'translate(0, 1.5px) scale(0.85)'
+        transform: "translate(0, 1.5px) scale(0.85)"
       }
     },
     MuiButton: {
       raisedSecondary: {
-        '&:hover': {
+        "&:hover": {
           backgroundColor: getConfig().secondary
         }
       }
     },
     MuiIconButton: {
       root: {
-        height: '56px'
+        height: "56px"
       }
     },
     MuiTab: {
@@ -99,7 +96,7 @@ const theme = createMuiTheme({
         // width: '50%'
         flex: 1
       }
-    },
+    }
   }
 });
 
@@ -107,7 +104,7 @@ const ScrollToTop = withRouter(
   class ScrollToTopWithoutRouter extends Component {
     componentDidUpdate(prevProps) {
       if (this.props.location !== prevProps.location) {
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
       }
     }
 
@@ -117,7 +114,7 @@ const ScrollToTop = withRouter(
   }
 );
 
-const Gold = (props) => {
+const Gold = props => {
   const { url } = props.match;
 
   return (
@@ -125,8 +122,8 @@ const Gold = (props) => {
       <MuiThemeProvider theme={theme}>
         <ScrollToTop />
         <Switch>
-          <Route exact path={`${url}`} component={About} />
-          <Route path={`${url}/about`} component={About} />
+          <Route exact path={`${url}`} component={GoldSummary} />
+          <Route path={`${url}/about`} component={GoldSummary} />
           <Route path={`${url}/details`} component={KnowMore} />
           <Route path={`${url}/my-gold`} component={GoldSummary} />
           <Route path={`${url}/my-gold-locker`} component={GoldLocker} />
@@ -136,10 +133,22 @@ const Gold = (props) => {
           <Route path={`${url}/buy-gold-order`} component={BuyOrder} />
           <Route path={`${url}/bank-details`} component={GoldBank} />
           <Route path={`${url}/sell-gold-order`} component={SellOrder} />
-          <Route path={`${url}/gold-delivery-address`} component={DeliveryAddress} />
-          <Route path={`${url}/gold-delivery-order`} component={DeliveryOrder} />
-          <Route path={`${url}/select-gold-product`} component={DeliverySelectedProduct} />
-          <Route path={`${url}/:orderType/payment/:status`} component={Payment} />
+          <Route
+            path={`${url}/gold-delivery-address`}
+            component={DeliveryAddress}
+          />
+          <Route
+            path={`${url}/gold-delivery-order`}
+            component={DeliveryOrder}
+          />
+          <Route
+            path={`${url}/select-gold-product`}
+            component={DeliverySelectedProduct}
+          />
+          <Route
+            path={`${url}/:orderType/payment/:status`}
+            component={Payment}
+          />
           <Route path={`${url}/verify`} component={Otp} />
 
           {/* Edit paths */}
