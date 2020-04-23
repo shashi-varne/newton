3164bd441f56106636d0ba831e1671069c4423ee
Vinod Jat <vinod@fisdom.com>
2020-04-23 14:34:46 +0530

    onload crash handlling
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/utils/api.js
+++ react_webviews/src/utils/api.js
@@ -8,7 +8,7 @@ import { getConfig } from  'utils/functions'
 
 const myHistory = createBrowserHistory();
 let { base_url } = qs.parse(myHistory.location.search.slice(1));
-let redirect_url  = getConfig().redirect_url;
+let redirect_url  = getConfig ? getConfig().redirect_url : '';
 let is_secure = false;
 
 axios.defaults.baseURL = decodeURIComponent(base_url).replace(/\/$/, "");
