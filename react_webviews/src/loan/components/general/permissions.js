30f68a13b1c800cbb2a569cc16d36eb4527aabdf
Sanjay <sanjay@fisdom.com>
2020-07-22 20:56:06 +0530

    implemented Permission layout

--- react_webviews/src/loan/components/general/permissions.js
+++ react_webviews/src/loan/components/general/permissions.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Container from '../../common/Container';
 import { nativeCallback } from 'utils/native_callback';
 import { initialize } from '../../common/functions';
+import { getConfig } from 'utils/functions';
+import '../Style.scss';
 
 class Permissions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show_loader: false
+      show_loader: false,
+      productName: getConfig().productName
     }
 
     this.initialize = initialize.bind(this);
@@ -49,13 +52,53 @@ class Permissions extends Component {
     return (
       <Container
         showLoader={this.state.show_loader}
-        title="DUMMY_HEADER_TITLE"
+        title="Mandatory permissions"
         events={this.sendEvents('just_set_events')}
         handleClick={this.handleClick}
         buttonTitle="CONTINUE"
       >
-        <div className="loan-permissons">
-          {/* {code goes here} */}
+        <div className="loan-permissions">
+
+          <div style={{ display: 'flex'}}>
+            <img
+              src={ require(`assets/${this.state.productName}/ic_document_mobile.svg`)}
+              style={{marginBottom: '115px'}}
+              alt="" 
+            />
+            <div className="container">
+              <div className="head">Mobile</div>
+              <div className="content">
+                Our app collects and monitors specific <br />
+                information about your device including <br />
+                your SIM Network service provider of your <br />
+                SMI.Device id and IP address. This help us <br />
+                to prevent fraud by uniquely identifying the <br />
+                devices.
+              </div> 
+            </div>
+          </div>
+
+          <div style={{ display: 'flex'}}>
+            <img
+              src={ require(`assets/${this.state.productName}/ic_document_location.svg`)}
+              style={{marginBottom: '80px'}}
+              alt="" 
+            />
+            <div className="container">
+              <div className="head">Location</div>
+              <div className="content">
+                We collect and monitor the information about <br />
+                the location of your device for verifying the <br />
+                address, creating your risk profile, and make a <br />
+                better credit risk decision.
+              </div> 
+            </div>
+          </div>
+
+          <div className="content">
+            Please note that above information is needed by <b>DMI Finance Pvt Ltd</b>, it is mandatorily to perform your credit risk assessment and is securely shared with our registered third party service providers for generating loan offers for you.
+          </div>
+
         </div>
       </Container>
     );
