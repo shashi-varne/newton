19f0b8a7dbea67bf458ab6469b981322328d69ba
Sanjay <sanjay@fisdom.com>
2020-07-31 10:51:05 +0530

    .

--- react_webviews/src/wealth_report/desktop/MainPage.js
+++ react_webviews/src/wealth_report/desktop/MainPage.js
@@ -28,13 +28,6 @@ export default class MainPage extends Component {
     return (
       <div style={{ width: '100%', height: '100%', background: 'white', overflow: 'scroll' }}>
         <div id="wr-header-hero"></div>
-        {/* <div id="wr-header-bar">
-          <div id="wr-header-pan-select" className="wr-header-tab"></div>
-          <div className="wr-header-tab">Overview</div>
-          <div className="wr-header-tab">Analysis</div>
-          <div className="wr-header-tab">Holdings</div>
-          <div className="wr-header-tab">Taxation</div>
-        </div> */}
         <Header params={params.tab} />
         <div id="wr-body">
           {this.renderTab(params.tab)}
