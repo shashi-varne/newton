dd4e05f0d2172a4702904646e105a5155195062f
Sanjay <sanjay@fisdom.com>
2020-08-02 14:28:52 +0530

    added footer

--- react_webviews/src/wealth_report/mini-components/holdingFilter.js
+++ react_webviews/src/wealth_report/mini-components/holdingFilter.js
@@ -1,69 +1,66 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Grow from '@material-ui/core/Grow';
 
 const Filters = [
     {
-        'category': 'Fund Type',
+        'category': 'Fund Type:',
         'filter': ['Debt', 'Equity', 'Other']
     },
     {
-        'category': 'Current Value',
+        'category': 'Current Value:',
         'filter': ['<1L', '1.5L', '5-10L', '10L+']
     },
     {
-        'category': 'Fisdom Rating',
+        'category': 'Fisdom Rating:',
         'filter': ['3 & Below', '4 & above']
     }
 ]
 
 export default class HoldingFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      checked: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      checked: !this.state.checked
+    })
+  }
+
   render() {
+    let { clicked, checked } = this.state;
+    let time = 1500;
+
     return (
-      <div>
-        {/* <div className="wr-filter">
+      <React.Fragment>
+        <div className="wr-filter" style={{display: clicked ? 'none' : 'flex'}}>
           <Button
             variant="contained"
             disableRipple
             className="wr-button"
+            onClick={this.handleClick}
+            size="small"
           >
             Filter
             <img src={require(`assets/fisdom/ic-filter.svg`)} alt="" style={{marginLeft:'8px'}}/>
           </Button>
-        </div> */}
+        </div>
 
-        {/* <div className="wr-filter-content">
-          <div className="wr-head">
-            Fund Type:
-            <Button
-            variant="contained"
-            disableRipple
-            className="wr-button"
-            size="small"
-          >
-            Debt
-          </Button>
-          <Button
-            variant="contained"
-            disableRipple
-            className="wr-button"
-            size="small"
-          >
-            Equity
-          </Button>
-          <Button
-            variant="contained"
-            disableRipple
-            className="wr-button"
-            size="small"
-          >
-            Other
-          </Button>
-          </div>
-        </div> */}
-
-        <div className="wr-filter-content">
-            {Filters.map(item => (
-                <div className="wr-head">
+        <div className="wr-filter-content" style={{display: clicked ? 'flex' : 'none'}}>
+          <div className="wr-category">
+            {Filters.map(item => {
+              time = time - 400;
+              return (
+                <Grow in={checked}
+                 {...(checked ? { timeout: time } : {})}
+                 >
+                  <div className="wr-head">
                     {item.category}
                     {item.filter.map(item => (
                       <Button
@@ -75,11 +72,19 @@ export default class HoldingFilter extends Component {
                         {item}
                       </Button>
                     ))}
-                </div>
-            ))}
+                  </div>
+                </Grow>
+            )})}
+          </div>
+            
+          <div class="vl"></div>
+          <img src={require(`assets/fisdom/ic-clear-filter.svg`)} alt=""
+           style={{marginLeft:'16px', cursor: 'pointer' }}
+           onClick={this.handleClick}
+           />
         </div>
 
-      </div>
+      </React.Fragment>
     );
   }
 }
