09e76486e7ddb9e9828c8d21017cbf52f077af4f
Sanjay <sanjay@fisdom.com>
2020-08-03 15:09:30 +0530

    implemented modal

--- react_webviews/src/wealth_report/mobile/container.js
+++ react_webviews/src/wealth_report/mobile/container.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { getConfig, setHeights } from 'utils/functions';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogActions,
+  // DialogTitle,
+  DialogContent,
+  DialogContentText
+} from 'material-ui/Dialog';
 
 
 class Container extends Component {
@@ -8,11 +15,54 @@ class Container extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        openDialog: false,
+        openPopup: false,
     }
   }
 
   componentDidMount() {
 
+  }
+
+  handleClose = () => {
+    this.setState({
+      openPopup: false
+    });
+  }
+
+  handlePopup = () => {
+  
+    this.setState({
+      openPopup: false
+    });
+
+    
+
+  }
+
+
+  renderPopup = () => {
+      console.log('hi')
+    return (
+      <Dialog
+          fullScreen={false}
+          open={this.state.openPopup}
+          onClose={this.handleClose}
+          aria-labelledby="responsive-dialog-title"
+      >
+          <DialogContent>
+              <DialogContentText>gfhjvcghjbnvnhj</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+              <Button onClick={this.handleClose} color="default">
+                  No
+        </Button>
+              <Button onClick={this.handlePopup} color="default" autoFocus>
+                  Yes
+        </Button>
+          </DialogActions>
+      </Dialog>
+  );
   }
 
 
@@ -21,7 +71,7 @@ class Container extends Component {
 
     return (
       <div className={`ContainerWrapper`} >
-            <div>
+            <div onClick={this.renderPopup} >
                 huoijvjhiuhhgi
             </div>
       </div>
