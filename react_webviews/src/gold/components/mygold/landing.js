ae18591f6f7cc600bfb7b44762b282d11f23f488
Sanjay <sanjay@fisdom.com>
2020-08-04 14:44:08 +0530

    .

--- react_webviews/src/gold/components/mygold/landing.js
+++ react_webviews/src/gold/components/mygold/landing.js
@@ -327,11 +327,11 @@ class GoldSummary extends Component {
     return (
       <span className="gold-offer-terms" key={index}>
         {index !== 5 ? `${index + 1}. ${props}` : 
-          <ul>
-            {props.map(offers => (
-              <li><b>{offers}</b></li>
-            ))}
-          </ul>
+          // <ul>
+            props.map(offers => (
+              <div><b>&bull;{offers}</b></div>
+            ))
+          // </ul>
         }
       </span>
     )
