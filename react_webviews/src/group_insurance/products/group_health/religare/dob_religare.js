77e9750307531c3c33f17b60aeb5b97eb1cee7ac
Sanjay <sanjay@fisdom.com>
2020-09-10 15:47:56 +0530

    modified final_dob_data

--- react_webviews/src/group_insurance/products/group_health/religare/dob_religare.js
+++ react_webviews/src/group_insurance/products/group_health/religare/dob_religare.js
@@ -38,35 +38,30 @@ class GroupHealthPlanDobReligare extends Component {
                 'name': 'Self',
                 'label': groupHealthPlanData.account_type === 'self' ? 'Date of birth (DD/MM/YYYY)' : "Your date of birth (DD/MM/YYYY)",
                 'value': '',
-                'backend_key': 'self_account_key'
             },
             {
                 'key': 'wife',
                 'name': 'Wife',
                 'label': "Wife's date of birth (DD/MM/YYYY)",
                 'value': '',
-                'backend_key': 'spouse_account_key'
             },
             {
                 'key': 'husband',
                 'name': 'Husband',
                 'label': "Husband's date of birth (DD/MM/YYYY)",
                 'value': '',
-                'backend_key': 'spouse_account_key'
             },
             {
                 'key': 'father',
                 'name': 'Father',
                 'label': "Father's date of birth (DD/MM/YYYY)",
                 'value': '',
-                'backend_key': 'parent_account1_key'
             },
             {
                 'key': 'mother',
                 'name': 'Mother',
                 'label': "Mother's date of birth (DD/MM/YYYY)",
                 'value': '',
-                'backend_key': 'parent_account2_key'
             }
         ];
 
@@ -76,21 +71,20 @@ class GroupHealthPlanDobReligare extends Component {
 
         dob_data.forEach(item => {
             if (ui_members[item.key]) {
-                
-                item.value = ui_members[item.key + '_dob'] || '';
-
-                if(!ui_members.father && item.key === 'mother') {
-                    item.backend_key = 'parent_account1_key';
-                }
-
                 final_dob_data.push(item)
             }
         });
 
+        let {eldest_member, dob} = groupHealthPlanData.final_dob_data
+
+        let obj = final_dob_data.find(item => item.key === eldest_member)
+        let index = final_dob_data.indexOf(obj);
+        final_dob_data[index].value = dob;
+
         this.setState({
             final_dob_data: final_dob_data,
             dob_data: dob_data,
-            selectedValue: groupHealthPlanData.post_body.eldest_member
+            selectedValue: eldest_member
         })
 
     }
@@ -148,27 +142,12 @@ class GroupHealthPlanDobReligare extends Component {
 
         if(canProceed) {
             groupHealthPlanData.ui_members = ui_members;
-            groupHealthPlanData.final_dob_data = final_dob_data;
-
-            final_dob_data.forEach(item => {
-                let backend_key = item.backend_key;
-                let relation = item.key;
-
-                post_body[backend_key] = {
-                    dob: item.value,
-                    relation: relation
-                }
-
-                post_body.eldest_member = selectedValue;
-            })
-
-            if(final_dob_data.length === 1 && groupHealthPlanData.account_type === 'parent') {
-                final_dob_data[0].backend_key = 'parent_account1_key';
+            groupHealthPlanData.final_dob_data = {
+                eldest_member: selectedValue,
+                dob: dob
             }
 
-            if(ui_members.self_gender && post_body.self_account_key) {
-                post_body.self_account_key.gender = ui_members.self_gender;
-            }
+            post_body.eldest_member_dob = dob;
 
             groupHealthPlanData.post_body = post_body;
 
