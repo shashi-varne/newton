9bdff238a8dc8686a39a8cc408d46f0f3c4f3c30
Sanjay <sanjay@fisdom.com>
2020-09-11 13:41:42 +0530

    integreted request body for self

--- react_webviews/src/group_insurance/products/group_health/religare/lifeStyle_details.js
+++ react_webviews/src/group_insurance/products/group_health/religare/lifeStyle_details.js
@@ -1,150 +1,285 @@
-import React, { Component } from 'react';
-import Container from '../../../common/Container';
-import { nativeCallback } from 'utils/native_callback';
-import { getConfig } from 'utils/functions';
-import { initialize, updateBottomPremium } from '../common_data';
-import RadioAndCheckboxList from './radioAndCheckboxList';
-import { isValidMonthYear } from 'utils/validators';
+import React, { Component } from "react";
+import Container from "../../../common/Container";
+import { nativeCallback } from "utils/native_callback";
+import { getConfig } from "utils/functions";
+import { initialize, updateBottomPremium, updateLead } from "../common_data";
+import RadioAndCheckboxList from "./radioAndCheckboxList";
+import { isValidMonthYear } from "utils/validators";
+import { formatMonthandYear, dobFormatTest } from "utils/validators";
 
 class GroupHealthPlanLifestyleDetail extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            ctaWithProvider: true,
+  constructor(props) {
+    super(props);
+    this.state = {
+      ctaWithProvider: true,
+      life_style_question: {},
+    };
+
+    this.initialize = initialize.bind(this);
+    this.updateLead = updateLead.bind(this);
+    this.updateBottomPremium = updateBottomPremium.bind(this);
+  }
+
+  componentWillMount() {
+    this.initialize();
+  }
+
+  async componentDidMount() {
+    let {account_type, ui_members} = this.state.groupHealthPlanData;
+    let mem_options = [];
+
+    for (var key in ui_members) { 
+      if (key !== '' && ui_members[key] === true) {
+        mem_options.push(key)
+      }
+    }
+    mem_options.push('None');
+
+    let list = [];
+
+    if (account_type === "self") {
+      list = [
+        {
+          label:
+            "Do you smoke, consume alcohol, or chew tobacco, ghutka or paan or use any recreational drugs? If ‘Yes’ then please provide the frequency & amount consumed.",
+          options: [
+            {
+              name: "Yes",
+              value: "Yes",
+            },
+            {
+              name: "No",
+              value: "No",
+            },
+          ],
+          key: 'self',
+          input_type: "radio",
+        },
+      ];
+    } else {
+      list = [
+        {
+          label:
+            "Does any of the insured members smoke, consume alcohol, or chew tobacco, ghutka or paan or use any recreational drugs? If ‘Yes’ then please provide the frequency & amount consumed.",
+          options: mem_options,
+          input_type: "checkbox",
+        },
+      ];
+    }
+
+    this.setState({
+      account_type: this.state.groupHealthPlanData.account_type,
+      list: list,
+    });
+  }
+
+  navigate = (pathname) => {
+    this.props.history.push({
+      pathname: pathname,
+      search: getConfig().searchParams,
+    });
+  };
+
+  sendEvents(user_action) {
+    let eventObj = {
+      event_name: "health_insurance",
+      properties: {
+        user_action: user_action,
+        screen_name: "lifestyle_details",
+      },
+    };
+
+    if (user_action === "just_set_events") {
+      return eventObj;
+    } else {
+      nativeCallback({ events: eventObj });
+    }
+  }
+
+  handleChangeRadio = (key, event) => {
+    let { life_style_question } = this.state;
+
+    life_style_question = {
+      'answer': event === 0 ? true : false
+    }
+
+    this.setState({
+      life_style_question: life_style_question
+    })
+  };
+
+  handleChange = (event) => {
+    let {name, value, id} = event.target;
+    let { life_style_question } = this.state;
+
+    if (id === "description") {
+      
+      if (!name) {
+        life_style_question = {
+          ...life_style_question,
+          answer_description: value
+        }
+      } else {
+        life_style_question[name] = {
+          ...this.state.life_style_question[name],
+          answer_description: value,
+        };
+      }
+
+      
+    } else {
+      if (!dobFormatTest(value)) {
+        return;
+      }
+
+      let input;
+      if (!name) {
+        input = document.getElementById('date')
+        input.onkeyup = formatMonthandYear;
+
+        life_style_question = {
+          ...life_style_question,
+          start_date: value
         }
 
-        this.initialize = initialize.bind(this);
-        this.updateBottomPremium = updateBottomPremium.bind(this);
+      } else {
+        input = document.getElementById(name + "_date");
+        input.onkeyup = formatMonthandYear;
+
+        life_style_question[name] = {
+          ...this.state.life_style_question[name],
+          start_date: value,
+        };
+      }
     }
 
-    componentWillMount() {
-        this.initialize();
-    }
+    this.setState({
+      life_style_question: life_style_question,
+    });
 
-    async componentDidMount() {
-        
-        let account_type = this.state.groupHealthPlanData.account_type;
+  };
 
-        let list = []
+  handleCheckbox = (event) => {
+    let name = event.target.name;
+    let { life_style_question } = this.state;
 
-        if (account_type === 'self') {
-            
-            list = [
-                {
-                    'label': 'Do you smoke, consume alcohol, or chew tobacco, ghutka or paan or use any recreational drugs? If ‘Yes’ then please provide the frequency & amount consumed.',
-                    'options': [
-                        {
-                            'name': 'Yes',
-                            'value': 'Yes'
-                        },
-                        {
-                            'name': 'No',
-                            'value': 'No'
-                        }
-                    ],
-                    'input_type': 'radio'
-                }
-            ]
-        } else {
-            list = [
-                {
-                    'label': 'Does any of the insured members smoke, consume alcohol, or chew tobacco, ghutka or paan or use any recreational drugs? If ‘Yes’ then please provide the frequency & amount consumed.',
-                    'options': {
-                        'family': ['Wife', 'Son', 'None'],
-                        'selfandfamily': ['Self', 'Wife', 'Son', 'None'],
-                        'parents': ['Father', 'Mother', 'None']
-                    },
-                    'input_type': 'checkbox'
-                }
-            ]
+    if (name === "None") {
+      let options = this.state.list[0].options;
+
+      options.forEach((name) => {
+        if (name !== "None") {
+          life_style_question[name] = {
+            ...this.state.life_style_question[name],
+            checked: false,
+          };
         }
-
-        this.setState({
-            account_type: this.state.groupHealthPlanData.account_type,
-            list: list
-        })
+      });
+    } else {
+      life_style_question["None"] = {
+        ...life_style_question[name],
+        checked: false,
+      };
     }
 
-    navigate = (pathname) => {
-        this.props.history.push({
-            pathname: pathname,
-            search: getConfig().searchParams
-        });
-    }
+    life_style_question[name] = {
+      ...life_style_question[name],
+      checked: event.target.checked,
+    };
 
-    sendEvents(user_action) {
-        let eventObj = {
-            "event_name": 'health_insurance',
-            "properties": {
-                "user_action": user_action,
-                "screen_name": 'lifestyle_details'
+    this.setState({
+      life_style_question: life_style_question,
+    });
+  };
+
+  handleClick = () => {
+    this.sendEvents("next");
+    let { account_type, list, life_style_question } = this.state;
+
+    let error = "";
+    if (account_type === 'self') {
+      let date = life_style_question.start_date;
+
+      if (!isValidMonthYear(date)) {
+        error = "please enter valid month or year";
+      }
+
+      this.setState({
+        error: error
+      })
+    } else {
+      // list[0].options.forEach((item) => {
+      //   if (!isValidMonthYear(this.state[item])) {
+      //     error = "please enter valid month or year";
+
+      //     life_style_question = {
+      //       ...life_style_question,
+      //       [item+'_error']: error
+      //     }
+      //   }
+      // });
+      for (var key in life_style_question) {
+        console.log(key.checked)
+        if (key !== 'None' && key.checked === true) {
+          console.log(key.start_date)
+          if (!isValidMonthYear(key.start_date)) {
+            error = "please enter valid month or year"
+
+            life_style_question = {
+              ...life_style_question,
+              [key+'_error']: error
             }
+          }
         }
+      }
 
-        if (user_action === 'just_set_events') {
-            return eventObj;
-        } else {
-            nativeCallback({ events: eventObj });
-        }
     }
 
-    handleChangeRadio = (value) => {
-        this.setState({
-            selectedValue: value
-        })
-    }
-
-    handleChange = (data) => {
-        this.setState({
-            ...data
-        })
-    }
-
-    handleClick = () => {
-        this.sendEvents('next');
-        let { account_type, list} = this.state;
-
-        let error = '';
-        list[0].options[account_type].forEach(item => {
-            if (!isValidMonthYear(this.state[item])) {
-                error = 'please enter valid month or year'
-
-                this.setState({
-                    [item+'_error']: error
-                })
-            };
-        })
-    }
+    this.setState ({
+      life_style_question: life_style_question
+    })
     
-    render() { 
-        let { account_type, list } = this.state;
-        console.log(this.state)
+    if (life_style_question.answer || !error) { 
+      let body = {
+        'life_style_question_exists': true,
+        'life_style_question': life_style_question
+      }
 
-        return ( 
-            <Container
-                events={this.sendEvents('just_set_events')}
-                show_loader={this.state.show_loader}
-                title='Lifestyle detail'
-                buttonTitle='CONTINUE'
-                withProvider={true}
-                buttonData={this.state.bottomButtonData}
-                handleClick={() => this.handleClick()}
-            >
-                <div className="common-top-page-subtitle">
-                    This is important to avoid claims rejection later
-                </div>
-                {account_type && 
-                    <RadioAndCheckboxList 
-                        account_type={account_type}
-                        name='lifeStyle details'
-                        list={list}
-                        handleChange={this.handleChange}
-                        error={this.state}
-                        handleChangeRadio={this.handleChangeRadio} />}
-            </Container>
-         );
+      // this.updateLead(body);
     }
+
+  };
+
+  render() {
+    let { account_type, list, life_style_question } = this.state;
+
+    return (
+      <Container
+        events={this.sendEvents("just_set_events")}
+        show_loader={this.state.show_loader}
+        title="Lifestyle detail"
+        buttonTitle="CONTINUE"
+        withProvider={true}
+        buttonData={this.state.bottomButtonData}
+        handleClick={() => this.handleClick()}
+      >
+        <div className="common-top-page-subtitle">
+          This is important to avoid claims rejection later
+        </div>
+        {account_type && (
+          <RadioAndCheckboxList
+            account_type={account_type}
+            name="lifeStyle details"
+            list={list}
+            handleChange={this.handleChange}
+            error={this.state.error}
+            life_style_question={life_style_question}
+            handleCheckbox={this.handleCheckbox}
+            handleChangeRadio={this.handleChangeRadio}
+          />
+        )}
+      </Container>
+    );
+  }
 }
- 
+
 export default GroupHealthPlanLifestyleDetail;
--- react_webviews/src/group_insurance/products/group_health/religare/medical_history.js
+++ react_webviews/src/group_insurance/products/group_health/religare/medical_history.js
@@ -1,167 +1,205 @@
-import React, { Component } from 'react';
-import Container from '../../../common/Container';
-import { nativeCallback } from 'utils/native_callback';
-import { getConfig } from '../../../../utils/functions';
-import { initialize, updateBottomPremium } from '../common_data';
-import RadioAndCheckboxList from './radioAndCheckboxList';
+import React, { Component } from "react";
+import Container from "../../../common/Container";
+import { nativeCallback } from "utils/native_callback";
+import { getConfig } from "../../../../utils/functions";
+import { initialize, updateBottomPremium, updateLead } from "../common_data";
+import RadioAndCheckboxList from "./radioAndCheckboxList";
 
 class GroupHealthPlanMedicalHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ctaWithProvider: true,
+      medical_questions: {},
+    };
+
+    this.initialize = initialize.bind(this);
+    this.updateBottomPremium = updateBottomPremium.bind(this);
+  }
+
+  componentWillMount() {
+    this.initialize();
+  }
+
+  async componentDidMount() {
+    let account_type = this.state.groupHealthPlanData.account_type;
+
+    let list = [];
+
+    let radioOptions = [
+      {
+        name: "yes",
+        value: "Yes",
+      },
+      {
+        name: "No",
+        value: "No",
+      },
+    ];
+
+    if (account_type === "self") {
+      list = [
+        {
+          label:
+            "Have you been diagnosed / hospitalized for any illness / injury during the last 48 months?",
+          options: radioOptions,
+          key: "mand_1",
+          input_type: "radio",
+        },
+        {
+          label:
+            "Have you ever filed a claim with your current / previous insurer?",
+          options: radioOptions,
+          key: "mand_2",
+          input_type: "radio",
+        },
+        {
+          label:
+            "Has your Health insurance been declined, cancelled or charged a higher premium?",
+          options: radioOptions,
+          key: "mand_3",
+          input_type: "radio",
+        },
+        {
+          label:
+            "Are you already covered under any other health insurance policy of Religare Health Insurance?",
+          options: radioOptions,
+          key: "mand_4",
+          input_type: "radio",
+        },
+      ];
+    } else {
+      list = [
+        {
+          label:
+            "Have any of the person(s) to be insured been diagnosed / hospitalized for any illness / injury during the last 48 months?",
+          options: radioOptions,
+          key: "mand_1",
+          input_type: "radio",
+        },
+        {
+          label:
+            "Have any of the person(s) to be insured ever filed a claim with their current / previous insurer?",
+          options: radioOptions,
+          key: "mand_2",
+          input_type: "radio",
+        },
+        {
+          label: "Who is the member?",
+          options: {
+            family: ["Wife", "Son"],
+            selfandfamily: ["Self", "Wife", "Son"],
+            parents: ["Father", "Mother"],
+          },
+          key: "mand_3",
+          input_type: "checkbox",
+        },
+        {
+          label:
+            "Has any proposal for Health insurance been declined, cancelled or charged a higher premium?",
+          options: radioOptions,
+          key: "mand_4",
+          input_type: "radio",
+        },
+        {
+          label:
+            "Is any of the person(s) to be insured, already covered under any other health insurance policy of Religare Health Insurance?",
+          options: radioOptions,
+          key: "mand_5",
+          input_type: "radio",
+        },
+      ];
+    }
+
+    this.setState({
+      account_type: account_type,
+      list: list,
+    });
+  }
+
+  navigate = (pathname) => {
+    this.props.history.push({
+      pathname: pathname,
+      search: getConfig().searchParams,
+    });
+  };
+
+  sendEvents(user_action) {
+    let eventObj = {
+      event_name: "health_insurance",
+      properties: {
+        user_action: user_action,
+        screen_name: "medical_history",
+      },
+    };
+
+    if (user_action === "just_set_events") {
+      return eventObj;
+    } else {
+      nativeCallback({ events: eventObj });
+    }
+  }
+
+  handleCheckbox = (event) => {
+    let { name, value } = event.target;
+    let { medical_questions } = this.state;
+
+    medical_questions[name] = {
+      ...medical_questions[name],
+      [value.toLowerCase()]: event.target.checked,
+    };
+
+    this.setState({
+      medical_questions: medical_questions,
+    });
+  };
+
+  handleClick = () => {
+    this.sendEvents("next");
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            ctaWithProvider: true,
-        }
 
-        this.initialize = initialize.bind(this);
-        this.updateBottomPremium = updateBottomPremium.bind(this);
-    }
+  };
 
-    componentWillMount() {
-        this.initialize();
-    }
+  handleChangeRadio = (key, event) => {
+    let { medical_questions } = this.state;
 
-    async componentDidMount() {
+    medical_questions[key] = {
+      answer: event === 0 ? true : false,
+    };
 
-        let account_type = this.state.groupHealthPlanData.account_type;
+    this.setState({
+      medical_questions: medical_questions,
+    });
+  };
 
-        let list = [];
+  render() {
+    let { account_type, list } = this.state;
 
-        let radioOptions = [
-            {
-                'name': 'yes',
-                'value': 'Yes'
-            },
-            {
-                'name': 'No',
-                'value': 'No'
-            }
-        ]
-
-        if (account_type === 'self') {
-
-            list = [
-                {
-                    'label': 'Have you been diagnosed / hospitalized for any illness / injury during the last 48 months?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Have you ever filed a claim with your current / previous insurer?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Has your Health insurance been declined, cancelled or charged a higher premium?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Are you already covered under any other health insurance policy of Religare Health Insurance?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-            ]
-
-        } else {
-
-            list = [
-                {
-                    'label': 'Have any of the person(s) to be insured been diagnosed / hospitalized for any illness / injury during the last 48 months?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Have any of the person(s) to be insured ever filed a claim with their current / previous insurer?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Who is the member?',
-                    'options': {
-                        'family': ['Wife', 'Son'],
-                        'selfandfamily': ['Self', 'Wife', 'Son'],
-                        'parents': ['Father', 'Mother']
-                    },
-                    'input_type': 'checkbox'
-                },
-                {
-                    'label': 'Has any proposal for Health insurance been declined, cancelled or charged a higher premium?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-                {
-                    'label': 'Is any of the person(s) to be insured, already covered under any other health insurance policy of Religare Health Insurance?',
-                    'options': radioOptions,
-                    'input_type': 'radio'
-                },
-            ]
-        }
-
-        this.setState({
-            account_type: account_type,
-            list: list
-        })
-    }
-
-    navigate = (pathname) => {
-        this.props.history.push({
-            pathname: pathname,
-            search: getConfig().searchParams
-        });
-    }
-
-    sendEvents(user_action) {
-        let eventObj = {
-            "event_name": 'health_insurance',
-            "properties": {
-                "user_action": user_action,
-                "screen_name": 'medical_history'
-            }
-        }
-
-        if (user_action === 'just_set_events') {
-            return eventObj;
-        } else {
-            nativeCallback({ events: eventObj });
-        }
-    }
-
-    handleClick = () => {
-        this.sendEvents('next');
-    }
-
-    handleChangeRadio = (value) => {
-        this.setState({
-            selectedValue: value
-        })
-    }
-    
-    render() {
-        let { account_type, list } = this.state;
-
-        return ( 
-            <Container
-                events={this.sendEvents('just_set_events')}
-                show_loader={this.state.show_loader}
-                title='Medical History'
-                buttonTitle='CONTINUE'
-                withProvider={true}
-                buttonData={this.state.bottomButtonData}
-                handleClick={() => this.handleClick()}
-            >
-                <div className="common-top-page-subtitle">
-                    This is important to avoid claims rejection later
-                </div>
-                {account_type && 
-                    <RadioAndCheckboxList
-                        account_type={account_type}
-                        name="medical history"
-                        list={list}
-                        handleChangeRadio={this.handleChangeRadio} />}
-            </Container>
-         );
-    }
+    return (
+      <Container
+        events={this.sendEvents("just_set_events")}
+        show_loader={this.state.show_loader}
+        title="Medical History"
+        buttonTitle="CONTINUE"
+        withProvider={true}
+        buttonData={this.state.bottomButtonData}
+        handleClick={() => this.handleClick()}
+      >
+        <div className="common-top-page-subtitle">
+          This is important to avoid claims rejection later
+        </div>
+        {account_type && (
+          <RadioAndCheckboxList
+            account_type={account_type}
+            name="medical history"
+            list={list}
+            handleCheckbox={this.handleCheckbox}
+            handleChangeRadio={this.handleChangeRadio}
+          />
+        )}
+      </Container>
+    );
+  }
 }
- 
+
 export default GroupHealthPlanMedicalHistory;
