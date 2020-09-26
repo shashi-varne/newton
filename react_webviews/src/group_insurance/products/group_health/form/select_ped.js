ab67524879706e4e87016f6481e7eb084458a009
Sanjay <sanjay@fisdom.com>
2020-09-26 12:43:12 +0530

    ped prefill bug

--- react_webviews/src/group_insurance/products/group_health/form/select_ped.js
+++ react_webviews/src/group_insurance/products/group_health/form/select_ped.js
@@ -58,17 +58,11 @@ class GroupHealthPlanSelectPed extends Component {
         ped_diseases_name = (ped_diseases_name || '').split(',');
         let duration = member_info.duration
 
-        this.state.screenData.ped_list.forEach((item, index) => {
-            item.checked = false;
-            item.start_date = '';
-            item.answer_description = '';
-            item.description = index === this.state.screenData.ped_list.lenght-1 ? '' : item.description;
-        });
-
         let options = this.state.screenData.ped_list;
 
         if(this.state.provider === 'RELIGARE') {
             let ped_data = member_info.ped_diseases || [];
+            console.log(ped_data)
             ped_data.forEach(item => {
                 options.forEach((opt, index) => {
 
@@ -76,6 +70,10 @@ class GroupHealthPlanSelectPed extends Component {
                         options[index].checked = true;
                         options[index].start_date = item.start_date || '';
                         options[index].description = item.answer_description || '';
+                    } else {
+                        options[index].checked = false;
+                        options[index].start_date = '';
+                        options[index].description = '';
                     }
                 })
 
