3aca4ae62ad1f5d724c2661d597ea600c213b920
Kiran IC <kirani.c@fisdom.com>
2020-09-28 19:08:19 +0530

    minor bug fixed

--- react_webviews/src/group_insurance/products/group_health/landing.js
+++ react_webviews/src/group_insurance/products/group_health/landing.js
@@ -274,7 +274,7 @@ class GroupHealthLanding extends Component {
             Covers all age groups
           </div>
           <div className="generic-page-subtitle">
-            Buy health insurance for yourself, spouse, kids or parents also.
+           {this.state.provider==="STAR"?'Option to cover your entire family (spouse, kids and parents)':'Buy health insurance for yourself, spouse, kids or parents also.'} 
           </div>
 
           <div className='family-images'>
