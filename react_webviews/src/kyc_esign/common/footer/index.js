148134d3dd56df1d1553ba191013fd55d80f7a7d
Sharique Asghar <sharique@fisdom.com>
2020-09-30 11:17:08 +0530

    removed withProviderLayout code from footer

--- react_webviews/src/kyc_esign/common/footer/index.js
+++ react_webviews/src/kyc_esign/common/footer/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 
-import { DefaultLayout, WithProviderLayout } from './layout';
+import { DefaultLayout } from './layout';
 
 const Footer = (props) => {
-  if(props.withProvider) {
-    return (
-   
-        <div className="Footer" style={{border: '1px solid rgb(235, 235, 226)'}}>
-          <WithProviderLayout type="default" {...props} />
-        </div>
-    );   
-  }else {
-    return (
-   
-      <div className="Footer">
-        <DefaultLayout type="default" {...props} />
-      </div>
-    );
-  }
-  
+  return (
+    <div className="Footer">
+      <DefaultLayout type="default" {...props} />
+    </div>
+  );
 };
 
 export default Footer;
