5487387c08a32cb47c31e88864c499006c3c7f84
shashidhar <shashidhar@fisdom.com>
2020-10-15 13:32:07 +0530

    removing unwanted files from the previoius commit

--- react_webviews (copy)/src/external_portfolio/constants.js
+++
@@ -1,3 +0,0 @@
-export const regenTimeLimit = 30;
-
-export const genericErrMsg = 'Something went wrong. Please try again';
--- react_webviews (copy)/src/referral/common/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = (props) => (
-  <div className={`Card ${(props.nopadding) ? 'nopadding' : ''}`}>
-    {props.children}
-  </div>
-);
-
-export default Card;
