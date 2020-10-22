c6e7478bda9632de1d0dac304bcb092d76182d10
shashidhar <shashidhar@fisdom.com>
2020-10-22 19:57:36 +0530

    WVFIS-392 and WVFIS-372

--- react_webviews/src/common/ui/RadioWithoutIcon.js
+++ react_webviews/src/common/ui/RadioWithoutIcon.js
@@ -75,7 +75,7 @@ class RadioGrp extends Component {
 
     return (
       <div>
-        <div className="RadioWithIcon">
+        <div className="RadioWithIcon" style={{fontSize: '13px'}}>
             {this.props.label}
           {/* <span className={(this.props.error) ? 'error' : ''}>{(this.props.error) ? 'Mandatory' : ''}</span> */}
         </div>
