d04612f208f95e69d1d0acc4e3444c6ddef85bfb
Vinod Jat <vinod@fisdom.com>
2020-10-28 13:42:34 +0530

    changes
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/group_insurance/products/group_health/form/address.js
+++ react_webviews/src/group_insurance/products/group_health/form/address.js
@@ -224,15 +224,15 @@ class GroupHealthPlanAddressDetails extends Component {
             form_data['p_pincode_error'] = 'Please enter valid pincode';
         }
 
-        for(var key in form_data){
+        for(let key in form_data){
             if(key === 'addressline' || key ==="addressline2" || key ==="p_addressline" || key === "p_addressline2"){
-                if(validateLengthDynamic(form_data[key], 3)){
+                if(validateLengthDynamic(form_data[key], 4)){
                     form_data[key+'_error'] = "Please enter at least 4 characters";
                 }
             }
         }
         
-        for (var key in form_data) {
+        for (let key in form_data) {
             if (key.indexOf('error') >= 0) {
                 if (form_data[key]) {
                     canSubmitForm = false;
