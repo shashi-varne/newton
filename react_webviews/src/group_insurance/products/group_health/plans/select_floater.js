675a8c5e2756fc4bfa262f34b9f942ce12289e5c
shashidhar <shashidhar@fisdom.com>
2020-11-06 15:33:18 +0530

    Floater change for religare

--- react_webviews/src/group_insurance/products/group_health/plans/select_floater.js
+++ react_webviews/src/group_insurance/products/group_health/plans/select_floater.js
@@ -56,7 +56,7 @@ class GroupHealthPlanSelectFloater extends Component {
 
         if(post_body.mem_info.child > 0){
             for(var key in post_body){
-                    if(key.includes('child') && calculateAge(post_body[key].dob, false) < 5){
+                    if(key.includes('child') && calculateAge(post_body[key].dob, false) < 5 && this.state.providerConfig.key == "RELIGARE"){
                         this.setState({disableFloaterOption: true });
                     }
             }
@@ -179,7 +179,7 @@ class GroupHealthPlanSelectFloater extends Component {
     }
 
     choosePlan = (index, props) => {
-        if(props.key === "NF" && this.state.disableFloaterOption){
+        if(props.key === "NF" && this.state.disableFloaterOption && this.state.providerConfig.key == "RELIGARE"){
             return;
         }
         this.setState({
@@ -203,7 +203,7 @@ class GroupHealthPlanSelectFloater extends Component {
         return (
 
             <div onClick={() => this.choosePlan(index, props)}
-                className={`tile ${index === this.state.selectedIndex ? 'tile-selected' : ''} ${this.state.disableFloaterOption && props.key === "NF" ? 'tile-disabled': ''}`} key={index}>
+                className={`tile ${index === this.state.selectedIndex ? 'tile-selected' : ''} ${this.state.disableFloaterOption && props.key === "NF" && this.state.providerConfig.key == "RELIGARE" ? 'tile-disabled': ''}`} key={index}>
                 <div className="select-tile">
                     <div className="flex-column">
                         <div className="name">
