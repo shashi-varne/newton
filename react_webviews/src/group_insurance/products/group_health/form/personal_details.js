c66617661c81db4d71e91ac8d35ea69cb3a17a7f
srikantagowda <srikanta@fisdom.com>
2020-11-25 20:10:08 +0530

    error retrive

--- react_webviews/src/group_insurance/products/group_health/form/personal_details.js
+++ react_webviews/src/group_insurance/products/group_health/form/personal_details.js
@@ -362,11 +362,11 @@ class GroupHealthPlanPersonalDetails extends Component {
       form_data.name_error = 'Invalid name';
     }
 
-    let weightlimit = form_data.weight.toString();
+    // let weightlimit = form_data.weight.toString();
 
-    if(weightlimit.length > 3){
-      form_data.weight_error = "Invalid weight";
-    }
+    // if(weightlimit.length > 3){
+    //   form_data.weight_error = "Invalid weight";
+    // }
 
     let canSubmitForm = true;
     for (var key in form_data) {
