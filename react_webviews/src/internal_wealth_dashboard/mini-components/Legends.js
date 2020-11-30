2761df47374eac09bd07ec915a5f68724424c93c
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2020-11-30 13:45:43 +0530

    added percentage to legend

--- react_webviews/src/internal_wealth_dashboard/mini-components/Legends.js
+++ react_webviews/src/internal_wealth_dashboard/mini-components/Legends.js
@@ -1,6 +1,6 @@
 import React from 'react' 
 
-function Legends({ data = {}, columns = 2, classes = {} }) {
+function Legends({ data = {}, columns = 2, classes = {}, percentage = true }) {
   const {
     container: containerClass,
     child: childClass,
@@ -26,7 +26,7 @@ function Legends({ data = {}, columns = 2, classes = {} }) {
             ></div>
             <div className={`iwd-lc-title ${labelClass}`}>{key}</div>
           </header>
-          <div className={`iwd-lc-value ${valueClass}`}>{share}</div>
+          <div className={`iwd-lc-value ${valueClass}`}>{share}%</div>
         </li>
       ))}
     </ul>
