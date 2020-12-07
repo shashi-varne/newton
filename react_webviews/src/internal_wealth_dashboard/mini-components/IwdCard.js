09870770a017c5b4243f166cf101abce44d2eeb2
karan uday <karan.uday@fisdom.com>
2020-12-07 18:47:13 +0530

    Added CSSTransition Module and other animation changes

--- react_webviews/src/internal_wealth_dashboard/mini-components/IwdCard.js
+++ react_webviews/src/internal_wealth_dashboard/mini-components/IwdCard.js
@@ -4,6 +4,7 @@ import IlsError from 'assets/fisdom/ils_error.svg';
 import React from 'react';
 import ErrorScreen from '../../common/responsive-components/ErrorScreen';
 import IwdCardLoader from './IwdCardLoader';
+import { CSSTransition } from 'react-transition-group';
 
 export default function IwdCard({
   headerText = '',
@@ -34,9 +35,17 @@ export default function IwdCard({
   };
 
   return (
-    <div className={`iwd-card ${className}`} id={id} style={style}>
-      {headerText && <div className="iwd-card-header">{headerText}</div>}
-      {renderChild()}
-    </div>
+    // <CSSTransition
+    //   appear={true}
+    //   in={true}
+    //   classNames="example"
+    //   timeout={1000}
+    // >
+      <div className={`iwd-card ${className} iwd-animatedFade`} id={id} style={style}>
+        {headerText && <div className="iwd-card-header">{headerText}</div>}
+        {renderChild()}
+      </div>
+    // </CSSTransition>
   );
 }
+
