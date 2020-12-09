3e4666505415fb454e0db96c0720f996d7fb4031
Vinod Jat <vinod@fisdom.com>
2020-12-09 19:56:09 +0530

    cached mechanism
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/common/ui/Imgc.js
+++ react_webviews/src/common/ui/Imgc.js
@@ -5,29 +5,30 @@ import './style.scss';
 import { SkeltonRect } from './Skelton';
 // import SVG from 'react-inlinesvg';
 
-
 class ImgcClass extends Component {
     constructor(props) {
         super(props);
         this.state = {
             fetchFailed: false,
-            loaded: false
+            loaded: this.isCached(props.src)
         };
     }
 
-    onLoad = () => {
 
-    }
 
-    onError = () => {
-
+    isCached = (src) => {
+        var img = new Image();
+        img.src = src;
+        var complete = img.complete;
+        // img.src = "";
+        return complete;
     }
 
     render() {
 
         let props = this.props;
         let skeltonFlag = this.state.fetchFailed || !this.state.loaded;
-        console.log(skeltonFlag)
+
         return (
 
             // <SVG
@@ -37,15 +38,21 @@ class ImgcClass extends Component {
             // style={props.style}
             // loader={<SkeltonRect style={props.style} className={props.className} />}
             // />
+
+
             // <SkeltonRect style={props.style} className={props.className} />
 
+
+
             <Fragment>
-                <img src={props.src} className={props.className}
+
+                <img id="image" src={props.src} className={props.className}
                     onLoad={() => {
                         console.log("loaded")
                         this.setState({
                             loaded: true
                         })
+
                     }}
                     onError={() => {
                         console.log("error")
@@ -53,15 +60,19 @@ class ImgcClass extends Component {
                             fetchFailed: true
                         })
                     }}
-                    style={{ ...props.style, display: skeltonFlag ? 'none' : '',
-                    flexShrink: 0 }} className={props.className}
+                    alt={props.alt}
+                    style={{
+                        ...props.style,
+                        flexShrink: 0,
+                        display: skeltonFlag ? 'none' : ''
+                    }}
+                    className={`${props.className}  ${skeltonFlag ? '' : ''}`}
                 />
-
 
 
                 <SkeltonRect
                     hide={!skeltonFlag}
-                    style={{...props.style,flexShrink: 0}} className={props.className} />
+                    style={{ ...props.style, flexShrink: 0 }} className={props.className} />
             </Fragment>
         );
 
