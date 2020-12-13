dac363c35d33fc66c7d967affbe5d9c577453126
Sanjay <sanjay@fisdom.com>
2020-12-13 20:45:13 +0530

    copy fix

--- react_webviews/src/loan_idfc/components/status/status.js
+++ react_webviews/src/loan_idfc/components/status/status.js
@@ -263,7 +263,7 @@ class LoanStatus extends Component {
     } = this.state;
 
     if (vendor_application_status === "idfc_0.5_accepted") {
-      commonMapper.top_title = `Congratulations, ${first_name}`;
+      commonMapper.top_title = `Congratulations, ${first_name}!`;
     }
 
     return (
