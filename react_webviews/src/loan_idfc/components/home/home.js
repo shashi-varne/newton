6b23bfcc3f585f135d4c35ee3b49f8e73fce80ca
AlekhyaBathula <alekhya@fisdom.com>
2020-12-19 21:24:10 +0530

    Fixed path of know more screen

--- react_webviews/src/loan_idfc/components/home/home.js
+++ react_webviews/src/loan_idfc/components/home/home.js
@@ -51,6 +51,7 @@ class Home extends Component {
         subtitle: "Quick disbursal",
         loan_amount: " ₹40 lac",
         logo: "idfc_logo",
+        provider_name: 'idfc',
         cta_title: "RESUME",
       },
       dmi: {
@@ -59,6 +60,7 @@ class Home extends Component {
         subtitle: "Quick money transfer",
         loan_amount: "₹1 lac",
         logo: "dmi-finance",
+        provider_name: 'dmi',
         cta_title: "RESUME",
       },
     };
@@ -79,9 +81,9 @@ class Home extends Component {
     }
   };
 
-  handleResume = () => {
+  handleResume = (provider_name) => {
     this.sendEvents("resume");
-    this.navigate("loan-know-more");
+    this.navigate(`${provider_name}/loan-know-more`);
   };
 
   sendEvents(user_action) {
