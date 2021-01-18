5ca65ab8198f5a3be36c90b126ba5f6b2ba8e63c
Sanjay <sanjay@fisdom.com>
2021-01-18 23:06:02 +0530

    pdf changes

--- react_webviews/src/loan/components/loan/loan_eligible.js
+++ react_webviews/src/loan/components/loan/loan_eligible.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import Container from '../../common/Container';
 import { nativeCallback } from 'utils/native_callback';
 import { initialize } from '../../common/functions';
-import {  formatAmountInr } from "../../../utils/validators";
+import { formatAmountInr } from "../../../utils/validators";
 import Api from 'utils/api';
 import toast from '../../../common/ui/Toast';
 import Grid from 'material-ui/Grid';
 import Checkbox from 'material-ui/Checkbox';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 class LoanEligible extends Component {
   constructor(props) {
@@ -15,9 +18,13 @@ class LoanEligible extends Component {
       show_loader: false,
       get_lead: true,
       getLeadBodyKeys: ['vendor_info'],
-      checkbox: false,
-      checkbox1: false,
-      checkbox2: false,
+      checkbox: {
+        one: true,
+        two: true,
+        three: true
+      },
+      tncClicked: false,
+      numPages: null,
     }
 
     this.initialize = initialize.bind(this);
@@ -56,7 +63,7 @@ class LoanEligible extends Component {
     }
   }
 
-  triggerConversion= async () => {
+  triggerConversion = async () => {
 
     this.setState({
       show_loader: true
@@ -92,47 +99,75 @@ class LoanEligible extends Component {
   }
 
   handleChange = (name) => {
-    let {checkbox1, checkbox2, checkbox} = this.state;
-    
-    if (name === 'checkbox') {
-      this.setState({
-        checkbox: !checkbox,
-        checkbox1: !checkbox,
-        checkbox2: !checkbox,
-      })
-    } else {
-      this.setState({
-        [name]: !this.state[name],
-      })
+    let {checkbox} = this.state;
+
+    checkbox[name] = !checkbox[name];
+
+    if (name !== 'one' && (checkbox.two && checkbox.three)) {
+      checkbox.one = true;
+    } else if (name !== 'one' && (!checkbox.two || !checkbox.three)) {
+      checkbox.one = false;
     }
     
-    if (checkbox1 && checkbox2) {
+    if (name === 'one') {
+      checkbox.two = checkbox.one;
+      checkbox.three = checkbox.one;
+    } 
+
+    this.setState({
+      checkbox: checkbox
+    })
+  }
+
+  handleTnC = () => {
+    this.setState({
+      tncClicked: true
+    })
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({
+      numPages: numPages
+    })
+  }
+
+  goBack = () => {
+    this.sendEvents('back')
+
+    if (this.state.tncClicked) {
       this.setState({
-        checked: true
+        tncClicked: false
       })
+    } else {
+      this.navigate('loan/dmi/journey')
     }
   }
 
   render() {
-
+    let {checkbox} = this.state;
     let vendor_info = this.state.vendor_info || {};
     return (
       <Container
         showLoader={this.state.show_loader}
-        title=''
+        title={this.state.tncClicked ? 'Terms and Conditions' : ''}
         events={this.sendEvents('just_set_events')}
         handleClick={this.handleClick}
         buttonTitle="CONTINUE"
-        hidePageTitle={true}
+        hidePageTitle={!this.state.tncClicked}
+        noFooter={this.state.tncClicked}
         headerData={{
-          icon: 'close'
+          icon: 'close',
+          goBack: this.goBack
+        }}
+        styleContainer={{
+          overflow: 'hidden'
         }}
       >
-        <div className="loan-status">
+        {!this.state.tncClicked && <div className="loan-status">
           <img
-            src={ require(`assets/${this.state.productName}/ils_loan_status.svg`)}
-            style={{width:"100%"}}
-            alt="" 
+            src={require(`assets/${this.state.productName}/ils_loan_status.svg`)}
+            style={{ width: "100%" }}
+            alt=""
           />
 
           <div className="loan-eligible">
@@ -147,12 +182,12 @@ class LoanEligible extends Component {
             <Grid id="agreeScroll" ref={this.agreeRef} container spacing={16} alignItems="center">
               <Grid item xs={1} className="TextCenter">
                 <Checkbox
-                  defaultChecked
-                  checked={this.state.checkbox}
+                  // defaultChecked
+                  checked={checkbox.one}
                   color="primary"
                   value="confirm_details_check"
                   name="confirm_details_check"
-                  onChange={() => this.handleChange('checkbox')}
+                  onChange={() => this.handleChange('one')}
                   className="Checkbox" />
               </Grid>
               <Grid item xs={11}>
@@ -163,64 +198,64 @@ class LoanEligible extends Component {
             <div id="agreement" className="agreement-block" style={{
             }} onScroll={this.onScroll}>
               <div className='inner-checkbox'>
-              <Grid container spacing={16}>
-                <Grid item xs={1}>
-                  <Checkbox
-                    checked={this.state.checkbox1}
-                    color="primary"
-                    id="checkbox"
-                    name="checkbox"
-                    disableRipple
-                    className="Checkbox"
-                    onChange={() => this.handleChange('checkbox1')}
-                  />
-                </Grid>
+                <Grid container spacing={16}>
+                  <Grid item xs={1}>
+                    <Checkbox
+                      checked={checkbox.two}
+                      color="primary"
+                      id="checkbox"
+                      name="checkbox"
+                      disableRipple
+                      className="Checkbox"
+                      onChange={() => this.handleChange('two')}
+                    />
+                  </Grid>
 
-                <Grid item xs={11}>
-                  <div className="label">In order to secure the interest of my legal heir(s). I hereby declare, request and authorise Edelweiss General Insurance Company for the following:</div>
-                  <div className="label">1. that I authorized DMI Finance to submit requests on my behalf, in relation to the benefits available under my Policy. May
-                    accept and act in accordance with the request submitted, under my Policy, through DMI Finance, without sending any notice or seeking any further confirmation from me/ my legal heirs. 
+                  <Grid item xs={11}>
+                    <div className="label">In order to secure the interest of my legal heir(s). I hereby declare, request and authorise Edelweiss General Insurance Company for the following:</div>
+                    <div className="label">1. that I authorized DMI Finance to submit requests on my behalf, in relation to the benefits available under my Policy. May
+                    accept and act in accordance with the request submitted, under my Policy, through DMI Finance, without sending any notice or seeking any further confirmation from me/ my legal heirs.
                   </div>
-                  <div className="label">2. To make any payout that is due and payable under my Policy, including claim payout/ refund, in the following manner: by crediting such amount, as is equivalent to the outstanding loan amount with the Financer, in my Loan Account Number held with
+                    <div className="label">2. To make any payout that is due and payable under my Policy, including claim payout/ refund, in the following manner: by crediting such amount, as is equivalent to the outstanding loan amount with the Financer, in my Loan Account Number held with
                     the Financer, or the remaining balance, i.e differential amount between the amount payable under my Policy and the outstanding loan amount, to myself or to my nominee, as may be applicable.
                   </div>
+                  </Grid>
                 </Grid>
-              </Grid>
 
-              <div className="label">By acting on my request in accordance with this Autorization, I hereby agree to indemnify Edelweiss General Insurance in case it incurs/ suffers any loss. Further, I relinquish my rights towards any and all claims and shall discharge from any and all liabilites under
+                <div className="label">By acting on my request in accordance with this Autorization, I hereby agree to indemnify Edelweiss General Insurance in case it incurs/ suffers any loss. Further, I relinquish my rights towards any and all claims and shall discharge from any and all liabilites under
                 my Policy and the same shall be binding on me and my heirs, executors, administrators, successors or legal representatives, as the case may be.
               </div>
 
-              <div className="label"><b>Good Health Declaration</b></div>
+                <div className="label"><b>Good Health Declaration</b></div>
 
-              <Grid container spacing={16}>
-                <Grid item xs={1}>
-                  <Checkbox
-                    checked={this.state.checkbox2}
-                    color="primary"
-                    id="checkbox"
-                    name="checkbox"
-                    disableRipple
-                    className="Checkbox"
-                    onChange={() => this.handleChange('checkbox2')}
-                  />
-                </Grid>
+                <Grid container spacing={16}>
+                  <Grid item xs={1}>
+                    <Checkbox
+                      checked={checkbox.three}
+                      color="primary"
+                      id="checkbox"
+                      name="checkbox"
+                      disableRipple
+                      className="Checkbox"
+                      onChange={() => this.handleChange('three')}
+                    />
+                  </Grid>
 
-                <Grid item xs={11}>
-                  <div className="label">I declare that I am of good health and i do not have any physical defect, deformity or disability. I further declare that I perform all my routine activities independently, that I do not have any history of, have never suffered from, am not currently suffering from,
+                  <Grid item xs={11}>
+                    <div className="label">I declare that I am of good health and i do not have any physical defect, deformity or disability. I further declare that I perform all my routine activities independently, that I do not have any history of, have never suffered from, am not currently suffering from,
                    nor have I received, nor am I currently receiving, nor do I expect to receive amy treatment, nor have been hospitalized, nor do I expect to be hospitalized for any ailment or disease or critical illness of any nature.</div>
+                  </Grid>
                 </Grid>
-              </Grid>
 
-              <div className="tnc label">I hereby declare the I have read policy 
-                <b style={{color:'var(--primary', cursor:'pointer'}} onClick={() => this.navigate('/loan/dmi/tnc')}> Terms and Conditions </b>
+                <div className="tnc label">I hereby declare the I have read policy
+                <b style={{ color: 'var(--primary', cursor: 'pointer' }} onClick={() => this.handleTnC()}> Terms and Conditions </b>
                carefully.</div>
+              </div>
             </div>
           </div>
-          </div>
 
-            
-          
+
+
 
           <div className="loan-value">
             <div>
@@ -233,13 +268,13 @@ class LoanEligible extends Component {
             </div>
             <div>
               <div>Annual interest rate</div>
-                <div className="values">{vendor_info.loan_rate}%</div>
+              <div className="values">{vendor_info.loan_rate}%</div>
             </div>
           </div>
 
           <div className="container">
-            <div style={{padding:'20px 20px 19px 20px'}}>
-              <div className="head" style={{paddingBottom: '22px'}}>
+            <div style={{ padding: '20px 20px 19px 20px' }}>
+              <div className="head" style={{ paddingBottom: '22px' }}>
                 Loan details
               </div>
               <div className="items">
@@ -248,17 +283,17 @@ class LoanEligible extends Component {
               </div>
               <div className="items">
                 <div>Credit insurance (@1%)</div>
-                <div>{this.state.checked ? '- '+formatAmountInr(vendor_info.insurance_premium_decision) : '- ₹0'}</div>
+                <div>{this.state.checkbox ? '- ' + formatAmountInr(vendor_info.insurance_premium_decision) : '- ₹0'}</div>
               </div>
               <div className="items">
                 <div>Processing fee</div>
-                <div>{'- '+formatAmountInr(vendor_info.processing_fee_decision)}</div>
+                <div>{'- ' + formatAmountInr(vendor_info.processing_fee_decision)}</div>
               </div>
               <div className="items">
                 <div>GST (@18%)</div>
-                <div>{'- '+formatAmountInr(vendor_info.gst_decision)}</div>
+                <div>{'- ' + formatAmountInr(vendor_info.gst_decision)}</div>
               </div>
-              <hr style={{background:"#ccd3db"}} />
+              <hr style={{ background: "#ccd3db" }} />
               <div className="credit">
                 <div>Amount credited to bank a/c</div>
                 <div>{formatAmountInr(vendor_info.net_amount_decision)}</div>
@@ -266,7 +301,27 @@ class LoanEligible extends Component {
             </div>
           </div>
 
-        </div>
+        </div>}
+
+        {this.state.tncClicked && <div className="tnc">
+          <Document
+            file="https://dmi-idfc-dot-plutus-staging.appspot.com/api/res/download/AMIfv95R_sFMFEfgAWjeiM1xjquEE5d7rNNMP_Oetbqv77ff8-31mQnqm08xaBNbgeE6xz4Wp0tVh2MkNYUzio_k3NzqTuluyE2THsT6XwiK03l_K-qwnoHeJvRT70Oge66c437VyKwDjC_ih1aBo5LKB16EDQlXsvBWeuG9mUFVbGfUdMaeYMg?filename=AMIfv95R_sFMFEfgAWjeiM1xjquEE5d7rNNMP_Oetbqv77ff8-31mQnqm08xaBNbgeE6xz4Wp0tVh2MkNYUzio_k3NzqTuluyE2THsT6XwiK03l_K-qwnoHeJvRT70Oge66c437VyKwDjC_ih1aBo5LKB16EDQlXsvBWeuG9mUFVbGfUdMaeYMg"
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            {Array.from(
+              new Array(this.state.numPages),
+              (el, index) => (
+                <div style={{ marginBottom: '20px' }} key={`page_${index + 1}`}>
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                    loading=''
+                  />
+                </div>
+              ),
+            )}
+          </Document>
+        </div>}
       </Container>
     );
   }
