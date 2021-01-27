f75833112788eb9f2260ee319b6ed3cc4ad2e9d8
Sanjay <sanjay@fisdom.com>
2021-01-27 13:41:18 +0530

    .

--- react_webviews/src/dashboard/nps/components/pan.js
+++ react_webviews/src/dashboard/nps/components/pan.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Container from "fund_details/common/Container";
 import "../style.scss";
+import InputWithIcon from "../../../common/ui/InputWithIcon";
+import name from "../../../assets/name_present_employer_dark_icn.png";
 
 const PanDetails = (props) => {
   const [value, setValue] = useState(0);
@@ -12,9 +14,16 @@ const PanDetails = (props) => {
       title="Why NPS?"
       classOverRideContainer="pr-container"
     >
-        <div className="pan-details">
-
+      <div className="pan-details">
+        <div className="InputField">
+          <InputWithIcon
+            icon={name}
+            width="40"
+            id="input-with-icon-grid"
+            label="With a grid"
+          />
         </div>
+      </div>
     </Container>
   );
 };
