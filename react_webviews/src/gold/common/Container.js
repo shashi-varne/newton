0cb50c658b7767d1022ae89bb3e700312a526097
Vinod Jat <vinod@fisdom.com>
2021-02-17 14:21:27 +0530

    common header/footer/container in gold
    Signed-off-by: Vinod Jat <vinod@fisdom.com>

--- react_webviews/src/gold/common/Container.js
+++ react_webviews/src/gold/common/Container.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router";
 
-import Header from '../../common/components/Header';
-import {didmount} from '../../common/components/container_functions';
+import {didmount ,commonRender} from '../../common/components/container_functions';
 
-import Footer from "./footer";
 
 import { nativeCallback } from "utils/native_callback";
 import '../../utils/native_listner';
-import { getConfig } from 'utils/functions';
 import {checkStringInString, storageService} from 'utils/validators';
 import {forceBackState, goBackMap} from '../constants';
-
-import UiSkelton from '../../common/ui/Skelton';
 
 class Container extends Component {
   constructor(props) {
@@ -29,6 +24,7 @@ class Container extends Component {
     };
 
     this.didmount = didmount.bind(this);
+    this.commonRender =  commonRender.bind(this);
   }
 
   componentDidMount() {
@@ -133,99 +129,11 @@ class Container extends Component {
   };
 
   render() {
-    let steps = [];
-    for (var i = 0; i < this.props.total; i++) {
-      if (this.props.current > i) {
-        steps.push(<span className="active" key={i}></span>);
-      } else {
-        steps.push(<span key={i}></span>);
-      }
-    }
-
-    if (this.state.mounted) { 
-      return (
-        <div className={`ContainerWrapper ${this.props.classOverRide}  ${(getConfig().productName !== 'fisdom') ? 'blue' : ''}`}  >
-          {/* Header Block */}
-          {(!this.props.noHeader && !getConfig().hide_header) &&<Header
-            disableBack={this.props.disableBack}
-            title={this.props.title}
-            smallTitle={this.props.smallTitle}
-            provider={this.props.provider}
-            count={this.props.count}
-            total={this.props.total}
-            current={this.props.current}
-            goBack={this.headerGoBack}
-            edit={this.props.edit}
-            type={getConfig().productName}
-            resetpage={this.props.resetpage}
-            handleReset={this.props.handleReset}
-            inPageTitle={this.state.inPageTitle}
-            force_hide_inpage_title={this.state.force_hide_inpage_title}
-            style={this.props.styleHeader}
-            className={this.props.classHeader}
-            headerData={this.props.headerData}
-          />}
-  
-          {/* Below Header Block */}
-          <div id="HeaderHeight" style={{ top: 56 }}>
-            {/* Loader Block */}
-            {this.renderPageLoader()}
-          </div>
-  
-
-          {!this.state.force_hide_inpage_title && 
-            this.new_header_scroll() 
-          }
-
-          { this.props.skelton && 
-            <UiSkelton 
-            type={this.props.skelton}
-            />
-          }
-  
-          {/* Children Block */}
-            <div
-            style={{...this.props.styleContainer, backgroundColor: this.props.skelton ? '#fff': 'initial'}}
-            className={`Container ${this.props.classOverRideContainer} ${this.props.noPadding ? "no-padding" : ""}`}
-            >
-            <div 
-            className={`${!this.props.skelton ? 'fadein-animation' : ''}`}
-            style={{display: this.props.skelton ? 'none': ''}}
-            > {this.props.children}
-             </div>
-          </div>
-  
-          {/* Footer Block */}
-          {!this.props.noFooter && !this.props.skelton &&  (
-            <Footer
-              noFooter={this.props.noFooter}
-              fullWidthButton={this.props.fullWidthButton}
-              logo={this.props.logo}
-              buttonTitle={this.props.buttonTitle}
-              provider={this.props.provider}
-              premium={this.props.premium}
-              paymentFrequency={this.props.paymentFrequency}
-              edit={this.props.edit}
-              resetpage={this.props.resetpage}
-              handleClick={this.props.handleClick}
-              handleClick2={this.props.handleClick2}
-              handleReset={this.props.handleReset}
-              onlyButton={this.props.onlyButton}
-              disable={this.props.disable}
-              withProvider={this.props.withProvider}
-              buttonData={this.props.buttonData}
-            />
-          )}
-          {/* No Internet */}
-          {this.renderDialog()}
-          {this.renderPopup()}
-        </div>
-      );
-    }
-
-    return null;
-
-    
+    return(
+      <Fragment>
+      {this.commonRender()}
+      </Fragment>
+    )
   }
 }
 
