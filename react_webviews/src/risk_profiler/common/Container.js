534ca7ae6d6b209fb96a756a519568a7204d418e
Srikanta <srikanta@fisdom.com>
2021-02-19 16:03:24 +0530

    risk_profiler container update

--- react_webviews/src/risk_profiler/common/Container.js
+++ react_webviews/src/risk_profiler/common/Container.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component , Fragment } from 'react';
 import { withRouter } from 'react-router';
 
-import Header from './Header';
-import Footer from './footer';
-import Banner from '../../common/ui/Banner';
-
 import { nativeCallback } from 'utils/native_callback';
-import Button from 'material-ui/Button';
-import Dialog, {
-  DialogActions,
-  DialogTitle,
-  DialogContent,
-  DialogContentText
-} from 'material-ui/Dialog';
 import '../../utils/native_listner';
-import { getConfig, setHeights } from 'utils/functions';
+import { getConfig } from 'utils/functions';
+
+
+import {didmount ,commonRender} from '../../common/components/container_functions';
+
 
 
 class Container extends Component {
@@ -30,34 +23,19 @@ class Container extends Component {
     }
     this.handleTopIcon = this.handleTopIcon.bind(this);
     this.handlePopup = this.handlePopup.bind(this);
+
+    this.historyGoBack = this.historyGoBack.bind(this);
+
+    this.didmount = didmount.bind(this);
+    this.commonRender = commonRender.bind(this);
   }
 
   componentDidMount() {
-    setHeights({ 'header': true, 'container': false });
-    let that = this;
-    if (getConfig().generic_callback) {
-      window.callbackWeb.add_listener({
-        type: 'back_pressed',
-        go_back: function () {
-          that.historyGoBack();
-        }
-      });
-    } else {
-      window.PlutusSdk.add_listener({
-        type: 'back_pressed',
-        go_back: function () {
-          that.historyGoBack();
-        }
-      });
-    }
+    this.didmount();
   }
 
   componentWillUnmount() {
-    if (getConfig().generic_callback) {
-      window.callbackWeb.remove_listener({});
-    } else {
-      window.PlutusSdk.remove_listener({});
-    }
+    this.unmount();
   }
 
   navigate = (pathname) => {
@@ -65,15 +43,6 @@ class Container extends Component {
       pathname: pathname,
       search: getConfig().searchParams
     });
-  }
-
-  getEvents(user_action) {
-    if (!this || !this.props || !this.props.events) {
-      return;
-    }
-    let events = this.props.events;
-    events.properties.user_action = user_action;
-    return events;
   }
 
   historyGoBack = () => {
@@ -134,28 +103,6 @@ class Container extends Component {
 
   }
 
-  renderDialog = () => {
-    return (
-      <Dialog
-        fullScreen={false}
-        open={this.state.openDialog}
-        onClose={this.handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">No Internet Found</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Check your connection and try again.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button className="DialogButtonFullWidth" onClick={this.handleClose} color="secondary" autoFocus>
-            OK
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
 
   handlePopup = () => {
     this.setState({
@@ -166,31 +113,6 @@ class Container extends Component {
 
   }
 
-  renderPopup = () => {
-    return (
-      <Dialog
-        fullScreen={false}
-        open={this.state.openPopup}
-        onClose={this.handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        {/* <DialogTitle id="form-dialog-title">No Internet Found</DialogTitle> */}
-        <DialogContent>
-          <DialogContentText>
-            {this.state.popupText}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleClose} color="default">
-            No
-          </Button>
-          <Button onClick={this.handlePopup} color="default" autoFocus>
-            Yes
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
 
   handleTopIcon() {
     this.setState({
@@ -200,98 +122,18 @@ class Container extends Component {
     })
   }
 
-  renderPageLoader = () => {
-    if (this.props.showLoader) {
-      return (
-        <div className="Loader">
-          <div className="LoaderOverlay">
-            <img src={require(`assets/${this.state.productName}/loader_gif.gif`)} alt="" />
-          </div>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-
   componentDidUpdate(prevProps) {
-    setHeights({ 'header': true, 'container': false });
+    this.didupdate();
   }
 
   render() {
-    let steps = [];
-    for (var i = 0; i < this.props.total; i++) {
-      if (this.props.current > i) {
-        steps.push(<span className='active'
-          style={{ background: getConfig().primary, marginRight: 0 }} key={i}></span>);
-      } else {
-        steps.push(<span key={i} style={{ marginRight: 0 }}></span>);
-      }
-    }
-
-    return (
-      <div className={`ContainerWrapper ${this.props.classOverRide}  ${(getConfig().productName !== 'fisdom') ? 'blue' : ''}`} >
-        {/* Header Block */}
-        <Header
-          disableBack={this.props.disableBack}
-          title={this.props.title}
-          smallTitle={this.props.smallTitle}
-          provider={this.props.provider}
-          count={this.props.count}
-          total={this.props.total}
-          current={this.props.current}
-          goBack={this.historyGoBack}
-          edit={this.props.edit}
-          type={getConfig().productName}
-          resetpage={this.props.resetpage}
-          handleReset={this.props.handleReset}
-          topIcon={this.props.topIcon}
-          handleTopIcon={this.handleTopIcon} />
-
-        {/* Below Header Block */}
-        <div id="HeaderHeight" style={{ top: 56 }}>
-
-          {/* Loader Block */}
-          {this.renderPageLoader()}
-
-          {steps && <div className="Step">
-            {steps}
-          </div>}
-
-          {/* Banner Block */}
-          {this.props.banner && <Banner text={this.props.bannerText} />}
-
-        </div>
-
-
-        {/* Children Block */}
-        <div className={`Container ${this.props.classOverRideContainer}`}>
-          {this.props.children}
-        </div>
-
-        {/* Footer Block */}
-        {!this.props.noFooter &&
-          <Footer
-            fullWidthButton={this.props.fullWidthButton}
-            logo={this.props.logo}
-            buttonTitle={this.props.buttonTitle}
-            provider={this.props.provider}
-            premium={this.props.premium}
-            paymentFrequency={this.props.paymentFrequency}
-            edit={this.props.edit}
-            resetpage={this.props.resetpage}
-            handleClick={this.props.handleClick}
-            handleReset={this.props.handleReset}
-            onlyButton={this.props.onlyButton}
-            noFooter={this.props.noFooter}
-            isDisabled={this.props.isDisabled} />
-        }
-        {/* No Internet */}
-        {this.renderDialog()}
-        {this.renderPopup()}
-      </div>
-    );
+    return(
+      <Fragment>
+      {this.commonRender()}
+      </Fragment>
+    )
   }
+  
 };
 
 export default withRouter(Container);
