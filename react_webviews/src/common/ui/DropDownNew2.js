3e91c6474038469c2616b260400f76c6fc45229f
Srikanta <srikanta@fisdom.com>
2021-03-17 00:05:58 +0530

    Label added for drop-down

--- react_webviews/src/common/ui/DropDownNew2.js
+++ react_webviews/src/common/ui/DropDownNew2.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Select from 'react-select';
 import { FormControl } from 'material-ui/Form';
-// import { InputLabel } from 'material-ui/Input';
+import { InputLabel } from 'material-ui/Input';
 import './style.scss';
 
 class SelectDropDown2 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedOption:this.props.value,
-            options: this.props.options
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: this.props.value,
+      options: this.props.options
+    };
+  }
 
-    
+
   handleChange = selectedOption => {
     // if (!selectedOption.length) {
     //   return (
@@ -21,8 +21,8 @@ class SelectDropDown2 extends React.Component {
     //   )
     // }
 
-    this.setState({ selectedOption })
 
+    this.setState({ selectedOption })
     if (selectedOption) {
       if (selectedOption.value && (selectedOption.value + "").length) {
         this.props.onChange(selectedOption.value);
@@ -36,10 +36,10 @@ class SelectDropDown2 extends React.Component {
 
 
 
-    componentDidUpdate(prevState) {
-        if (prevState.value !== this.props.value && this.props.value) { this.setState({ selectedOption: this.props.value }) }
-        if (prevState.options !== this.props.options && this.props.options) { this.setState({ options: this.props.options }) }
-    }
+  componentDidUpdate(prevState) {
+    if (prevState.value !== this.props.value && this.props.value) { this.setState({ selectedOption: this.props.value }) }
+    if (prevState.options !== this.props.options && this.props.options) { this.setState({ options: this.props.options }) }
+  }
 
   componentWillMount() {
     const value = this.props.options.find(opt => opt.value === this.props.value || opt.name === this.props.value);
@@ -48,31 +48,32 @@ class SelectDropDown2 extends React.Component {
 
   render() {
 
-  //  const Option = props => { return ( <div> <components.Option {...props}> <input type="checkbox" checked={props.isSelected} onChange={() => null} /> <label>{props.value}</label> </components.Option> </div> ); };
+    //  const Option = props => { return ( <div> <components.Option {...props}> <input type="checkbox" checked={props.isSelected} onChange={() => null} /> <label>{props.value}</label> </components.Option> </div> ); };
 
     const options = this.props.options.map((ele, index) => {
-        return ({
-           'value': ele.value, 'label': ele.name
-        })
+      return ({
+        'value': ele.value, 'label': ele.name
+      })
     })
 
-    const value = options.find(opt => opt.value === this.state.selectedOption || opt.name === this.state.selectedOption );        
+    const value = options.find(opt => opt.value === this.state.selectedOption || opt.name === this.state.selectedOption);
     return (
       <FormControl className="Dropdown label" disabled={this.props.disabled} style={{ margin: '2px 0px' }}>
-        {/* <InputLabel htmlFor={this.props.id}>{this.props.label}</InputLabel> */}
-
-      {(value && <span className="label2">{this.props.label || 'label'}</span> )} {console.log(value)}
+        {(<InputLabel shrink={!!this.state.selectedOption || this.state.shrink } htmlFor={this.props.id}><span
+        style={{ marginLeft: '10px', position: 'absolute' , marginTop: '1px'}} >{this.props.label}</span></InputLabel>)}
+        {/* {(!value && <span className="label2">{this.props.label || 'label'}</span> )} */}
         <Select
-          components={{ Option }}
+          // components={{ Option }}
           defaultValue={value}
-          placeholder={this.props.label}
+          onMenuOpen={() =>  this.setState({shrink: true})}
+          onMenuClose={() => this.setState({shrink: false})}
+          placeholder={''}
           isClearable
           isSearchable={this.props.options.length <= 6 ? false : true}
-          value={value}
+          value={this.state.selectedOption}
           onChange={this.handleChange}
-          options={Option}
+          options={options}
           menuPlacement={'top'}
-
           textFieldProps={{
             label: 'Label',
             InputLabelProps: {
@@ -83,7 +84,7 @@ class SelectDropDown2 extends React.Component {
           components={{
             IndicatorSeparator: () => null
           }}
-          // defaultMenuIsOpen={true}
+        // defaultMenuIsOpen={true}
         />
         {(this.props.error || this.state.error) ? <span className='error-radiogrp'> {this.props.helperText || this.state.helperText || 'Please select an option'} </span> :
           <span className='error-radiogrp'> {this.props.helperText || this.state.helperText || ''} </span>}
@@ -93,7 +94,7 @@ class SelectDropDown2 extends React.Component {
 }
 
 const DropDownNew2 = (props) => {
-    return (<SelectDropDown2 {...props} />)
+  return (<SelectDropDown2 {...props} />)
 }
 
 export default DropDownNew2;
