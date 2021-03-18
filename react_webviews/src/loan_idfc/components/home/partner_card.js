56bca7ef3b90aceb2ba4299c9de78a39a8ead555
Sanjay <sanjay@fisdom.com>
2021-03-18 13:47:41 +0530

    css change

--- react_webviews/src/loan_idfc/components/home/partner_card.js
+++ react_webviews/src/loan_idfc/components/home/partner_card.js
@@ -3,12 +3,15 @@ import Button from "material-ui/Button";
 import Card from "../../../common/ui/Card";
 import SVG from "react-inlinesvg";
 import { getConfig } from "utils/functions";
+import Collapse from "@material-ui/core/Collapse";
 
 class PartnerCard extends Component {
   renderBenefits = (data, index) => {
     return (
       <div key={index} className="benefits-points">
-        <div><div className="dot"></div></div>
+        <div>
+          <div className="dot"></div>
+        </div>
         <div>
           <div>{data.data ? data.data : data}</div>
           {data.sub_data &&
@@ -30,13 +33,14 @@ class PartnerCard extends Component {
         {this.props.baseData.card_tag && this.props.baseData.displayTag && (
           <div className="card-tag">{this.props.baseData.card_tag}</div>
         )}
-        <div className="flex partner" 
-        onClick={() => 
-          this.props.handleClick(this.props.baseData.provider_name)
-        }
+        <div
+          className="flex partner"
+          onClick={() =>
+            this.props.handleClick(this.props.baseData.provider_name)
+          }
         >
           <div>
-            <div style={{fontWeight: '600'}}>{this.props.baseData.title}</div>
+            <div style={{ fontWeight: "600" }}>{this.props.baseData.title}</div>
             <div>{this.props.baseData.subtitle}</div>
           </div>
           <img
@@ -44,28 +48,22 @@ class PartnerCard extends Component {
             alt={this.props.baseData.logo}
           />
         </div>
-        <div className="flex" 
-        onClick={() => 
-          this.props.handleClick(this.props.baseData.provider_name)
-        }
+        <div
+          className="flex"
+          onClick={() =>
+            this.props.handleClick(this.props.baseData.provider_name)
+          }
         >
           <div>
             <div className="sub-text">Loan up to</div>
             <div className="sub-text-2">{this.props.baseData.loan_amount}</div>
           </div>
-          <Button
-            variant="raised"
-            size="large"
-            autoFocus
-          >
+          <Button variant="raised" size="large" autoFocus>
             {this.props.otp_verified ? "RESUME" : "APPLY NOW"}
           </Button>
         </div>
         {this.props.baseData.benefits && (
-          <div
-            className="benefits"
-            onClick={() => this.props.handleBenefits()}
-          >
+          <div className="benefits" onClick={() => this.props.handleBenefits()}>
             <div className="benefits-header">
               Benefits
               <SVG
@@ -77,14 +75,14 @@ class PartnerCard extends Component {
                 }.svg`)}
               />
             </div>
-            {this.props.isSelected && (
+            <Collapse in={this.props.isSelected}>
               <div className="benefits-content">
                 {this.props.baseData.benefits.benefits_title && (
                   <div>{this.props.baseData.benefits.benefits_title}</div>
                 )}
                 {this.props.baseData.benefits.options.map(this.renderBenefits)}
               </div>
-            )}
+            </Collapse>
           </div>
         )}
       </Card>
