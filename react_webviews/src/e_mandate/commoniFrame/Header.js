c6b12c709ae04a583e840f200dea9acfb7301cd2
fdamitsingh <amit.singh@fisdom.com>
2021-03-19 18:49:19 +0530

    added the header for mobile view moneycontrol

--- react_webviews/src/e_mandate/commoniFrame/Header.js
+++ react_webviews/src/e_mandate/commoniFrame/Header.js
@@ -3,19 +3,54 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import restart from 'assets/restart_nav_icn.svg';
-import back_arrow from 'assets/back_moneycontrol.svg';
-import close from 'assets/close_moneycontrol.svg';
+import back_arrow from 'assets/back_white.svg';
+import close from 'assets/close_white.svg';
+import close_blue from 'assets/close_blue.svg';
+import back_blue from 'assets/back_blue.svg';
+import { getConfig } from 'utils/functions';
 
-const Header = ({ classes, title, count, total, current, width,goBack, edit, type, resetpage, handleReset, smallTitle, disableBack, provider, topIcon, handleTopIcon, noBack }) => (
-   <AppBar position="fixed" color="primary" style={{width: width,}} className={classes.topBar} >
-    <Toolbar>
-      <div style={{display: 'flex', paddingLeft:"26px",paddingRight:"10px", justifyContent: 'space-between' , direction: 'row',alignItems:"center",width:"100%"}}>  
-      <div onClick={goBack}>
-
-        {!disableBack && <img src={back_arrow} alt="back" style={{cursor:"pointer"}}/>}
-        {(disableBack === true || disableBack === 'summary') && <img src={close} alt="close" style={{cursor:"pointer"}}/>}
-      </div>
-      {/* {!noBack &&   
+const Header = ({
+  classes,
+  title,
+  count,
+  total,
+  current,
+  width,
+  goBack,
+  edit,
+  type,
+  resetpage,
+  handleReset,
+  smallTitle,
+  disableBack,
+  provider,
+  topIcon,
+  handleTopIcon,
+  noBack,
+}) => {
+  const isMobile = getConfig().isMobileDevice;
+  return (
+    <AppBar position='fixed' color='primary' style={{ width: width }} className={classes.topBar}>
+      {!isMobile ? (
+        <Toolbar>
+          <div
+            style={{
+              display: 'flex',
+              paddingLeft: '26px',
+              paddingRight: '10px',
+              justifyContent: 'space-between',
+              direction: 'row',
+              alignItems: 'center',
+              width: '100%',
+            }}
+          >
+            <div onClick={goBack}>
+              {!disableBack && <img src={back_arrow} alt='back' style={{ cursor: 'pointer' }} />}
+              {(disableBack === true || disableBack === 'summary') && (
+                <img src={close} alt='close' style={{ cursor: 'pointer' }} />
+              )}
+            </div>
+            {/* {!noBack &&   
       <IconButton 
       className={classes.menuButton} 
       color="inherit" aria-label="Menu" onClick={goBack}
@@ -23,25 +58,38 @@ const Header = ({ classes, title, count, total, current, width,goBack, edit, typ
         {!disableBack && <NavigateBeforeIcon className={classes.NavigateBeforeIcon} />}
         {(disableBack === true || disableBack === 'summary') && <Close className={classes.NavigateBeforeIcon} />}
       </IconButton>} */}
-    <img src={ require(`assets/finity/moneycontrol_logo.svg`)} className={classes.img} style={{ marginTop: '20px', marginBottom: '20px'}} alt=""/> 
-    {<img src={ require(`assets/finity_white_logo_2.png`)} style={{paddingRight: '40px', marginTop: '32px', marginBottom: '32px'}} alt=""/> } 
-      {resetpage &&
-        <img onClick={handleReset}
-          alt=""
-          width={20}
-          src={restart}
-        />
-      }
-      </div>
-    </Toolbar>
-  </AppBar>
-);
+            <img
+              src={require(`assets/finity/moneycontrol_logo.svg`)}
+              className={classes.img}
+              style={{ marginTop: '20px', marginBottom: '20px' }}
+              alt=''
+            />
+            {
+              <img
+                src={require(`assets/finity_white_logo_2.png`)}
+                style={{ paddingRight: '40px', marginTop: '32px', marginBottom: '32px' }}
+                alt=''
+              />
+            }
+            {resetpage && <img onClick={handleReset} alt='' width={20} src={restart} />}
+          </div>
+        </Toolbar>
+      ) : (
+        <div onClick={goBack} style={{ marginLeft: '-7px' }}>
+          {!disableBack && <img src={back_blue} alt='back' style={{ cursor: 'pointer' }} />}
+          {(disableBack === true || disableBack === 'summary') && (
+            <img src={close_blue} alt='close' style={{ cursor: 'pointer' }} />
+          )}
+        </div>
+      )}
+    </AppBar>
+  );
+};
 
-
-const styles = function() { 
+const styles = function () {
   var windowWidth = window.innerWidth;
 
-  if (windowWidth > 425){
+  if (windowWidth > 425) {
     return {
       root: {
         flexGrow: 1,
@@ -55,50 +103,49 @@ const styles = function() {
         height: '80px',
       },
       topBar: {
-        backgroundColor: "#3792FC",
-        width: "100%",
+        backgroundColor: '#3792FC',
+        width: '100%',
         height: '80px',
         display: 'flex',
         justifyContent: 'space-between',
-       paddingLeft: '3%'
+        paddingLeft: '3%',
       },
       NavigateBeforeIcon: {
         color: 'white',
-        paddingLeft:'1%',
-        marginLeft: '-5px'
-      }
-    }
-  }else return {
-    root: {
-      flexGrow: 1,
-    },
-    flex: {
-      flex: 1,
-    },
-    menuButton: {
-      marginLeft: '-25px !important',
-      marginRight: '17px !important',
-      height: '80px',
-    },
-    topBar: {
-      backgroundColor: 'white',
-      width: "100%",
-      height: '80px',
-      border: '1px solid white',
-      paddingLeft: '3%'
-    },
-    NavigateBeforeIcon: {
-      color : "#3792FC",
-      paddingLeft:'1%',
-      marginLeft: '-5px'
-    },
+        paddingLeft: '1%',
+        marginLeft: '-5px',
+      },
+    };
+  } else
+    return {
+      root: {
+        flexGrow: 1,
+      },
+      flex: {
+        flex: 1,
+      },
+      menuButton: {
+        marginLeft: '-25px !important',
+        marginRight: '17px !important',
+        height: '80px',
+      },
+      topBar: {
+        backgroundColor: 'white',
+        width: '100%',
+        height: '60px',
+        border: '1px solid white',
+        paddingLeft: '3%',
+      },
+      NavigateBeforeIcon: {
+        color: '#3792FC',
+        paddingLeft: '1%',
+        marginLeft: '-5px',
+      },
 
-    img: {
-      display: 'none'
-    }
-
-  }
-
-}
+      img: {
+        display: 'none',
+      },
+    };
+};
 
 export default withStyles(styles)(Header);
