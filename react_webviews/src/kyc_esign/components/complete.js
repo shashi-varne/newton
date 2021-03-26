443227c6454ef0a21f92b2aa7b2d0f879327a116
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2021-03-26 15:55:14 +0530

    in progress esign complete screen

---
+++ react_webviews/src/kyc_esign/components/complete.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getConfig } from 'utils/functions'
+
+const Complete = () => {
+  const productName = getConfig().productName
+  return (
+    <div className="nsdl-status">
+      <section className="nsdl-success">
+        <img
+          src={require(`assets/${productName}/ic_process_done.svg`)}
+          className="done-image"
+          alt=""
+        />
+      </section>
+    </div>
+  )
+}
+
+export default Complete
--- react_webviews/src/kyc_esign/components/nsdl.js
+++ react_webviews/src/kyc_esign/components/nsdl.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import Container from '../common/Container';
-import { nativeCallback } from 'utils/native_callback';
-import { getConfig } from 'utils/functions';
-import { getUrlParams } from 'utils/validators';
-import ContactUs from '../../common/components/contact_us';
+import React, { Fragment, Component } from 'react'
+import Container from '../common/Container'
+import { nativeCallback } from 'utils/native_callback'
+import { getConfig } from 'utils/functions'
+import { getUrlParams } from 'utils/validators'
+import ContactUs from '../../common/components/contact_us'
+import Failed from './failed'
+import Complete from './complete'
+import toast from '../../common/ui/Toast'
+import Api from '../../utils/api'
 
 class DigiStatus extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       show_loader: false,
       productName: getConfig().productName,
-      params: getUrlParams()
+      params: getUrlParams(),
     }
   }
 
@@ -19,27 +23,73 @@ class DigiStatus extends Component {
     // nativeCallback({ action: 'exit_web' });
     this.props.history.push({
       pathname: '/invest',
-      search:  getConfig().searchParams
+      search: getConfig().searchParams,
     })
   }
 
+  retry = async () => {
+    const redirectUrl = encodeURIComponent(
+      window.location.origin + '/kyc-esign/nsdl' + getConfig().searchParams
+    )
+
+    this.setState({ show_loader: 'button' })
+
+    try {
+      let res = await Api.get(
+        `/api/kyc/formfiller2/kraformfiller/upload_n_esignlink?kyc_platform=app&redirect_url=${redirectUrl}`
+      )
+      let resultData = res.pfwresponse.result
+      if (resultData && !resultData.error) {
+        if (getConfig().app === 'ios') {
+          nativeCallback({
+            action: 'show_top_bar',
+            message: {
+              title: 'eSign KYC',
+            },
+          })
+        }
+        nativeCallback({
+          action: 'take_control',
+          message: {
+            back_text: 'You are almost there, do you really want to go back?',
+          },
+        })
+        window.location.href = resultData.esign_link
+      } else {
+        toast(
+          resultData.error || resultData.message || 'Something went wrong',
+          'error'
+        )
+      }
+
+      this.setState({ show_loader: false })
+    } catch (err) {
+      this.setState({
+        show_loader: false,
+      })
+      toast('Something went wrong')
+    }
+  }
+
   render() {
-    const { show_loader, productName } = this.state;
-    const { status = "failed" } = this.state.params;
+    const { show_loader, productName } = this.state
+    const { status = 'failed' } = this.state.params
     const headerData = {
-      icon: "close",
-      goBack: this.handleClick
+      icon: 'close',
+      goBack: this.handleClick,
     }
 
     return (
       <Container
         showLoader={show_loader}
-        title={status === "success" ? 'eSign KYC completed' : 'eSign KYC failed'}
-        handleClick={this.handleClick}
-        buttonTitle='OKAY'
+        title={
+          status === 'success' ? 'eSign KYC completed' : 'eSign KYC failed'
+        }
+        handleClick={status === 'success' ? this.handleClick : this.retry}
+        buttonTitle="OKAY"
         headerData={headerData}
       >
-        <div className="nsdl-status">
+        {/* <div className="nsdl-status">
           <img
             src={require(`assets/${productName}/ils_esign_${status}.svg`)}
             style={{ width: "100%" }}
@@ -54,11 +104,19 @@ class DigiStatus extends Component {
               Sorry! the eSign verification is failed. Please try again.
             </div>
           }
-        </div>
-        <ContactUs />
+        </div> */}
+        {status === 'success' ? (
+          <Fragment>
+            <Failed />
+            <ContactUs />
+          </Fragment>
+        ) : (
+          <Complete />
+        )}
+        {/* <ContactUs /> */}
       </Container>
-    );
+    )
   }
 }
 
-export default DigiStatus;
+export default DigiStatus
