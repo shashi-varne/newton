38c51a1bf16b4ad94e4cb57b64f7dc3621d5cea2
AlekhyaBathula <alekhya@fisdom.com>
2021-03-31 20:17:48 +0530

    Added mf flow in fund details & WIP : new container changes of fund details

--- react_webviews/src/dashboard/invest/functions.js
+++ react_webviews/src/dashboard/invest/functions.js
@@ -555,9 +555,8 @@ export function showFundInfo(data) {
   dataCopy.diy_type = "recommendation";
   dataCopy.invest_type_from = "instaredeem";
   storageService().setObject("diystore_fundInfo", dataCopy);
-  this.props.history.push({
-    pathname: "/fund-details",
-    search: `${getConfig().searchParams}&isins=${data.isin}`,
+  this.navigate("/fund-details", {
+    searchParams: `${getConfig().searchParams}&isins=${data.isin}`,
   });
 }
 
@@ -719,7 +718,7 @@ export async function getNfoRecommendation() {
         cartCount: cartCount,
         showFunds: showFunds,
       });
-    }  else {
+    } else {
       this.setState({ show_loader: false });
       toast(result.message || result.error || errorMessage);
     }
@@ -772,7 +771,9 @@ export async function getDiyPurchaseLimit(data) {
     let { fundsData, purchaseLimitData } = this.state;
     if (status === 200) {
       purchaseLimitData[data.investType] = result.funds_data;
-      purchaseLimitData[data.investType] = purchaseLimitData[data.investType].map((dict) => {
+      purchaseLimitData[data.investType] = purchaseLimitData[
+        data.investType
+      ].map((dict) => {
         var results = fundsData.filter((obj) => {
           if (obj.isin === dict["isin"]) {
             obj["allow_purchase"] = dict["ot_sip_flag"];
@@ -964,7 +965,7 @@ export async function makeInvestment(investment, isReferralGiven) {
     type === "diy"
   ) {
     this.navigate("/invest-journey");
-    return
+    return;
   }
 
   if (isReferralGiven && investReferralData.code) {
@@ -1044,7 +1045,11 @@ export async function proceedInvestmentChild(data) {
         storageService().setObject("mf_invest_data", investmentEventData);
 
         if (isSipDatesScreen) {
-          this.setState({ openSuccessDialog: true, investResponse: result, isApiRunning: false });
+          this.setState({
+            openSuccessDialog: true,
+            investResponse: result,
+            isApiRunning: false,
+          });
           return;
         }
         if (getConfig().Web) {
@@ -1230,8 +1235,11 @@ export function openKyc() {
     if (kycJourneyStatus === "ground") {
       this.navigate("/kyc/home");
     } else if (kycJourneyStatus === "ground_pan") {
-      this.navigate('/kyc/journey', { state: {
-        show_aadhaar: true, fromState: "invest" }
+      this.navigate("/kyc/journey", {
+        state: {
+          show_aadhaar: true,
+          fromState: "invest",
+        },
       });
     } else {
       this.navigate(kycStatusData.next_state, {
--- react_webviews/src/fund_details/common/Container.js
+++ react_webviews/src/fund_details/common/Container.js
@@ -226,3 +226,145 @@ const Container = (props) => {
 };
 
 export default withRouter(Container);
+
+//////////////   NEW CONTAINER      ////////////////////////
+
+// import React, { Component, Fragment } from "react";
+// import { withRouter } from "react-router";
+// import {
+//   didMount,
+//   commonRender,
+// } from "../../common/components/container_functions";
+// import { nativeCallback } from "utils/native_callback";
+// import "../../utils/native_listner";
+// import "./Style.scss"
+// import { getConfig, setHeights } from "../../utils/functions";
+// import { isFunction } from "../../utils/validators";
+
+// class Container extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       openDialog: false,
+//       openPopup: false,
+//       popupText: "",
+//       callbackType: "",
+//       inPageTitle: true,
+//       force_hide_inpage_title: this.props.hidePageTitle,
+//       new_header: true,
+//       project: "fund-details", //to use in common functions
+//       x: React.createRef(true),
+//     };
+//     this.historyGoBack = this.historyGoBack.bind(this);
+//     this.handleTopIcon = this.handleTopIcon.bind(this);
+
+//     this.didMount = didMount.bind(this);
+//     this.commonRender = commonRender.bind(this);
+//   }
+
+
+//   componentDidMount() {
+//     this.didMount();
+//     this.initialize();
+//   }
+
+//   initialize = () => {
+//     let x = this.state.x;
+//     setHeights({ header: true, container: false });
+//     console.log(window)
+//     if (x.current) {
+//       x.current = false;
+//     } else {
+//       if (getConfig().generic_callback) {
+//         // window.callbackWeb.addEventListener({
+//         //   type: 'back_pressed',
+//         //   go_back: () => {this.historyGoBack()},
+//         // });
+//       } else {
+//         // window.PlutusSdk.addEventListener({
+//         //   type: 'back_pressed',
+//         //   go_back: () => {this.historyGoBack()},
+//         // });
+//       }
+//     }
+//   }
+
+//   historyGoBack = (backData) => {
+//     // let fromHeader = backData ? backData.fromHeader : false;
+//     // let pathname = this.props.history.location.pathname;
+//     let { params } = this.props.location;
+
+//     if (params && params.disableBack) {
+//       nativeCallback({ action: 'exit' });
+//       return;
+//     }
+
+//     if (isFunction(this.props.goBack)) {
+//       return this.props.goBack(params);
+//     }
+//     nativeCallback({ events: this.getEvents('back') });
+//     this.props.history.goBack();
+//   };
+
+//   getEvents = (user_action) => {
+//     if (!this || !this.props || !this.props.events) {
+//       return;
+//     }
+//     let events = this.props.events;
+//     events.properties.user_action = user_action;
+//     return events;
+//   };
+
+//   // useEffect(() => {
+//   //   setHeights({ header: true, container: false });
+//   //   if (x.current) {
+//   //     x.current = false;
+//   //   } else {
+//   //     if (getConfig().generic_callback) {
+//   //       window.callbackWeb.addEventListener({
+//   //         type: 'back_pressed',
+//   //         go_back: () => historyGoBack(),
+//   //       });
+//   //     } else {
+//   //       window.PlutusSdk.addEventListener({
+//   //         type: 'back_pressed',
+//   //         go_back: () => historyGoBack(),
+//   //       });
+//   //     }
+//   //   }
+//   // }, []);
+
+//   componentWillUnmount() {
+//     this.unmount();
+//   }
+
+//   handleTopIcon = () => {
+//     this.props.handleTopIcon();
+//   };
+
+//   historyGoBack = (backData) => {
+//     if (this.getEvents("back")) {
+//       nativeCallback({ events: this.getEvents("back") });
+//     }
+
+//     if (this.props.headerData && this.props.headerData.goBack) {
+//       this.props.headerData.goBack();
+//       return;
+//     }
+
+//     this.props.history.goBack();
+//   };
+
+//   componentDidUpdate(prevProps) {
+//     this.didupdate();
+//   }
+
+//   render() {
+//     let props_base = {
+//       classOverRide: "loanMainContainer",
+//     };
+//     return <Fragment>{this.commonRender(props_base)}</Fragment>;
+//   }
+// }
+
+// export default withRouter(Container);
