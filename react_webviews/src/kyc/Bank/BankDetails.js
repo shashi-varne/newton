9d0a66f6a4dcbb71d307c96a4e94d60076e5b91f
Srikanta <srikanta@fisdom.com>
2021-05-07 16:42:35 +0530

    WVFIS-980-data-aid-added

--- react_webviews/src/kyc/Bank/BankDetails.js
+++ react_webviews/src/kyc/Bank/BankDetails.js
@@ -75,15 +75,16 @@ const BankDetails = (props) => {
       handleClick={handleClick}
       noFooter={bank.bank_status !== "rejected"}
       title="Bank accounts"
+      data-aid='kyc-bankDetails-page'
     >
-      <div className="bank-details">
+      <div className="bank-details" data-aid='bank-details-container'>
         {!showLoader && (
           <>
-            <div className="bank-info">
+            <div className="bank-info" data-aid='bank-info'>
               <img src={bank.bank_image} className="left-icon" alt="" />
               <div className="content">
                 <div className="bank-name">
-                  <div className="name" id='bank_name'>
+                  <div className="name">
                     {bank.bank_name}
                     {bank.bank_status === "rejected" && (
                       <img
@@ -95,62 +96,62 @@ const BankDetails = (props) => {
                   </div>
                   {bank.status === "default" &&
                     bank.bank_status !== "rejected" && (
-                      <div className="tag" id='tag'>PRIMARY</div>
+                      <div className="tag">PRIMARY</div>
                     )}
                 </div>
-                <div className="branch-name" id='branch_name'>{bank.branch_name}</div>
+                <div className="branch-name">{bank.branch_name}</div>
               </div>
             </div>
-            <div className="item">
-              <div className="left" id='account_number_title'>Account number</div>
-              <div className="right" id='account_number'> {bank.account_number} </div>
+            <div className="item" data-aid='account_number'>
+              <div className="left">Account number</div>
+              <div className="right"> {bank.account_number} </div>
             </div>
-            <div className="item">
-              <div className="left" id='ifsc_code_title'>IFSC code</div>
-              <div className="right" id='ifsc_code'>{bank.ifsc_code} </div>
+            <div className="item" data-aid='ifsc_code'>
+              <div className="left">IFSC code</div>
+              <div className="right">{bank.ifsc_code} </div>
             </div>
-            <div className="item">
-              <div className="left" id='account_type_text'>Account type</div>
-              <div className="right" id='account_type'> {bank.account_type} </div>
+            <div className="item" data-aid='account_type'>
+              <div className="left">Account type</div>
+              <div className="right"> {bank.account_type} </div>
             </div>
-            <div className="item">
-              <div className="left" id='Status-text'>Status</div>
+            <div className="item" data-aid='status'>
+              <div className="left">Status</div>
               <div
                 className={`status ${
                   bank.bank_status === "rejected" && "failed"
                 } ${bank.bank_status === "verified" && "verified"}`}
-                id={`mapped_bank_status`}
+                data-aid={`mapped_bank_status`}
               >
                 {bank.mapped_bank_status}
               </div>
             </div>
             {bank.bank_status !== "rejected" && (
-              <div className="mandate-section">
-                <div className="title" id='Mandates-text'>Mandates</div>
+              <div className="mandate-section" data-aid='mandate-section'>
+                <div className="title">Mandates</div>
                 {bank.mandates && bank.mandates.length > 0 ? (
                   bank.mandates.map((mandate, index) => {
                     return (
                       <div key={index} className="content">
-                        <div className="item">
-                          <div className="left" id='ID-text'>ID</div>
-                          <div className="right" id='ID'>{mandate.id} </div>
+                        <div className="item" data-aid='ID'>
+                          <div className="left">ID</div>
+                          <div className="right">{mandate.id} </div>
                         </div>
-                        <div className="item">
-                          <div className="left" id='account_type_text'>Account type</div>
-                          <div className="right" id='amount'>
+                        <div className="item" data-aid='account-type'>
+                          <div className="left">Account type</div>
+                          <div className="right">
                             {" "}
                             {formatAmountInr(mandate.amount)}{" "}
                           </div>
                         </div>
-                        <div className="item">
-                          <div className="left" id='Status-text'>Status</div>
+                        <div className="item" data-aid='Status'>
+                          <div className="left">Status</div>
                           <div
                             className={`status ${
                               mandate.status === "rejected" && "failed"
                             } ${mandate.status === "verified" && "verified"} ${
                               mandate.status === "init" && "underprocess"
                             }`}
-                            id='mapped_mandate_status'
+                            data-aid='mapped_mandate_status'
                           >
                             {mandate.mapped_mandate_status}
                           </div>
@@ -159,7 +160,7 @@ const BankDetails = (props) => {
                     );
                   })
                 ) : (
-                  <div className="info-text" id='info-text'>
+                  <div className="info-text" data-aid='info-text'>
                     You do not have any mandates associated with this bank
                     account
                   </div>
