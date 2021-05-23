d2e65b7b9b164f4af43aae202ec9544e67a26325
Sharique Asghar <sharique@fisdom.com>
2021-05-23 16:05:43 +0530

    modified manual signature and document verification screen

--- react_webviews/src/kyc/Equity/components/ManualSignature.js
+++ react_webviews/src/kyc/Equity/components/ManualSignature.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
 import { nativeCallback } from '../../../utils/native_callback';
 import Container from '../../common/Container'
 import { navigate as navigateFunc } from '../../common/functions';
 import useUserKycHook from "../../common/hooks/userKycHook";
-import Button from 'material-ui/Button';
 import WVInfoBubble from "../../../common/ui/InfoBubble/WVInfoBubble";
+import WVButton from "../../../common/ui/Button/WVButton"
+import WVSteps from "../../../common/ui/Steps/WVSteps"
 import ContactUs from "../../../common/components/contact_us";
 import { getConfig } from '../../../utils/functions';
 import { companyDetails } from "../../constants";
@@ -14,6 +15,50 @@ const ManualSignature = (props) => {
   const navigate = navigateFunc.bind(props);
   const config = getConfig();
   const {kyc} = useUserKycHook();
+
+  const renderStep1Content = useCallback(() => {
+    return (
+      <>
+        <WVButton
+          variant='outlined'
+          size='large'
+          color="secondary"
+          fullWidth
+          classes={{ label: 'form-download-btn' }}
+        >
+          <div className="download-text">DOWNLOAD FORMS</div>
+          <img
+            alt="Download button"
+            src={require("assets/download.svg")}
+          />
+        </WVButton>
+        <div className="step-note">
+          Form with instructions is emailed at <b style={{color: "#161A2E"}}>{kyc?.identification?.meta_data.email || "sharique@fisdom.com"}</b>
+        </div>
+      </>
+    )
+  }, []);
+
+  const renderStep2Content = useCallback(() => {
+    return (
+      <WVInfoBubble
+        type="info"
+        isDismissable
+        isOpen={true}
+      >
+        Signature should be on “all marked boxes”
+      </WVInfoBubble>
+    )
+  }, []);
+
+  const renderStep3Content = useCallback(() => {
+    return (
+      <div className="step-note">
+        <div><b>{companyDetails.NAME}</b></div>
+        <div>{companyDetails.ADDRESS}</div>
+      </div>
+    )
+  }, []);
 
   const handleDownloadFormsClick = () => {
 
@@ -27,6 +72,12 @@ const ManualSignature = (props) => {
     // navigate('path')
   }
 
+  const stepsToRender = [
+    { "id": 1, "title": "Get all the forms", render: renderStep1Content },
+    { "id": 2, "title": "Print all the forms & sign it", render: renderStep2Content },
+    { "id": 3, "title": "Courier the signed documents at our given address", render: renderStep3Content }
+  ]
+
   return (
     <Container
       title="Manual Signature"
@@ -39,56 +90,18 @@ const ManualSignature = (props) => {
           Send us your signed documents through courier by following the steps below
         </div>
         <div class="page-content">
-          <div className="step">
-            <div className="step-header">
-              {/* Todo: import component to show step */}
-              <div style={{width: "20px", height: "20px", textAlign: "center", padding: "3px 0", color: "white", fontSize: "12px", backgroundColor: `${config.styles.primaryColor}`, borderRadius: "50%"}}>1</div>
-              <div className="step-title">Get all the forms</div>
-            </div>
-            <div className="step-content">
-              {/* Todo: Import custom button */}
-              <Button
-                fullWidth={true}
-                variant='outlined'
-                size='large'
-                color='secondary'
-              >
-                Download Forms
-              </Button>
-              <div className="step-note">
-                Form with instructions is emailed at <b style={{color: "#161A2E"}}>{kyc?.identification?.meta_data.email || "sharique@fisdom.com"}</b>
+          {stepsToRender?.length && stepsToRender.map((step, index) => (
+            <WVSteps
+              stepNum={step.id}
+              title={step.title}
+              classes={{ stepContent: 'step' }}
+              key={step.id}
+            >
+              <div className="step-content">
+                {step.render()}
               </div>
-            </div>
-          </div>
-          <div className="step">
-            <div className="step-header">
-              {/* Todo: import component to show step */}
-              <div style={{width: "20px", height: "20px", textAlign: "center", padding: "3px 0", color: "white", fontSize: "12px", backgroundColor: `${config.styles.primaryColor}`, borderRadius: "50%"}}>2</div>
-              <div className="step-title">Print all the forms & sign it</div>
-            </div>
-            <div className="step-content">
-              <WVInfoBubble
-              type="info"
-              isDismissable
-              isOpen={true}
-              >
-                Signature should be on “all marked boxes”
-              </WVInfoBubble>
-            </div>
-          </div>
-          <div className="step">
-            <div className="step-header">
-              {/* Todo: import component to show step */}
-              <div style={{width: "20px", height: "20px", textAlign: "center", padding: "3px 0", color: "white", fontSize: "12px", backgroundColor: `${config.styles.primaryColor}`, borderRadius: "50%"}}>3</div>
-              <div className="step-title">Courier the signed documents at our given address</div>
-            </div>
-            <div className="step-content">
-              <div className="step-note">
-                <div><b>{companyDetails.NAME}</b></div>
-                <div>{companyDetails.ADDRESS}</div>
-              </div>
-            </div>
-          </div>
+            </WVSteps>
+          ))}
           <div className="page-note">
             We will verify the documents. You will receive an email from us once you are successfully onboarded
           </div>
