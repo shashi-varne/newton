6a63077361f485bb8e589daccee9c10c35471a80
Lakshya Poddar <lakshyapoddar1604@gmail.com>
2021-05-29 18:11:40 +0530

    fund details screen fix

--- react_webviews/src/dashboard/PassiveIndexFunds/mini-components/Accordian.js
+++ react_webviews/src/dashboard/PassiveIndexFunds/mini-components/Accordian.js
@@ -1,11 +1,11 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import Collapse from "@material-ui/core/Collapse";
 import ExpandMoreIcon from "../../../assets/expand_more.svg";
 import ExpandLessIcon from "../../../assets/expand_less.svg";
-import './commonStyles.scss'
+import "./commonStyles.scss";
 
 const PassiveFundAccordian = ({ title, children }) => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <>
       <div
@@ -14,19 +14,27 @@ const PassiveFundAccordian = ({ title, children }) => {
           justifyContent: "space-between",
           alignItems: "center",
           cursor: "pointer",
-          padding: "0 20px 0 20px"
+          padding: "0 20px",
         }}
         onClick={() => setOpen(!open)}
       >
-        <p style={{ fontSize: "15px", fontWeight: "700", lineHeight: "24px", letterSpacing: "0px"
-}}>{title}</p>
+        <p
+          style={{
+            fontSize: "15px",
+            fontWeight: "700",
+            lineHeight: "24px",
+            letterSpacing: "0px",
+          }}
+        >
+          {title}
+        </p>
         <img src={open ? ExpandLessIcon : ExpandMoreIcon} alt="" />
       </div>
-      <div style={{padding: "0 5px 0 5px"}}>
-      <Collapse in={open}>{children}</Collapse>
+      <div style={{ padding: "0 5px 0 5px" }}>
+        <Collapse in={open}>{children}</Collapse>
       </div>
     </>
   );
 };
 
-export default (memo(PassiveFundAccordian));
+export default memo(PassiveFundAccordian);
