d4e2ecbee7d163ce6abcd79cfc5f43c9cd4a1376
Karan Uday <karan.uday@fisdom.com>
2021-06-02 14:34:24 +0530

    Added timestamp for HV token + replaced geocoder api with Google Maps JS SDK

--- react_webviews/src/kyc/Upload/Selfie.js
+++ react_webviews/src/kyc/Upload/Selfie.js
@@ -1,5 +1,5 @@
 import "./commonStyles.scss";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../common/Container'
 import { storageService, isEmpty } from '../../utils/validators'
 import { storageConstants, SUPPORTED_IMAGE_TYPES } from '../constants'
@@ -25,6 +25,7 @@ const Selfie = (props) => {
   const [isCamInitialised, setIsCamInitialised] = useState(false);
   const [isCamLoading, setIsCamLoading] = useState(true);
   const [isLocnPermOpen, setIsLocnPermOpen] = useState(false);
+  const [isLocInitialised, setIsLocInitialised] = useState(true);
   const [locationData, setLocationData] = useState({});
   const [selfieLiveScore, setSelfieLiveScore] = useState('');
   // const [showLoader, setShowLoader] = useState(false);
@@ -98,8 +99,15 @@ const Selfie = (props) => {
     navigate('/kyc/upload/selfie-steps');
   }
 
-  const closeLocnPermDialog = () => {
+  const closeLocnPermDialog = (locationCloseType) => {
+    if (locationCloseType === 'invalid-region') {
+      navigate('/kyc/journey');
+    }
     setIsLocnPermOpen(false);
+  }
+
+  const onLocationInit = () => {
+    setIsLocInitialised(true);
   }
 
   const onCameraInit = (init) => {
@@ -110,6 +118,12 @@ const Selfie = (props) => {
       Toast('Something went wrong! Please try again in some time');
     }
   }
+
+  useEffect(() => {
+    if (isCamInitialised && isLocInitialised) {
+      setIsCamLoading(false);
+    }
+  }, [isCamInitialised, isLocInitialised])
 
   return (
     <Container
@@ -170,6 +184,7 @@ const Selfie = (props) => {
               />
               <LocationPermission
                 isOpen={isLocnPermOpen}
+                onInit={onLocationInit}
                 onClose={closeLocnPermDialog}
                 onLocationFetchSuccess={onLocationFetchSuccess}
                 parentProps={props}
