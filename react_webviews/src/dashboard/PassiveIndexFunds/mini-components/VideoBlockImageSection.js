e3a5592c5f8ff247a5d2e42d27eb56da7b0edfdc
Srikanta <srikanta@fisdom.com>
2021-06-09 16:12:26 +0530

    css fix

--- react_webviews/src/dashboard/PassiveIndexFunds/mini-components/VideoBlockImageSection.js
+++ react_webviews/src/dashboard/PassiveIndexFunds/mini-components/VideoBlockImageSection.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from 'react';
+import "./commonStyles.scss";
 
 function VideoBlockImageSection() {
     return (
-        <div className="info-container" data-aid="info-container" style={{ color: "#94C5FF" }}>
+        <div className="info-container" data-aid="info-container">
             <div className="info-block" data-aid="info-block-1">
                 <div>
                     <img src={require("../../../assets/passive_info1.svg")} alt="" />
                 </div>
-                <p style={{ color: "#94C5FF" }}>Lower cost</p>
+                <p>Lower cost</p>
             </div>
             <p>|</p>
             <div className="info-block" data-aid="info-block-2">
                 <div>
                     <img src={require("../../../assets/passive_info2.svg")} alt="" />
                 </div>
-                <p style={{ color: "#94C5FF" }}>Broad diversification</p>
+                <p>Broad diversification</p>
             </div>
             <p>|</p>
             <div className="info-block" data-aid="info-block-3">
                 <div>
                     <img src={require("../../../assets/passive_info3.svg")} alt="" />
                 </div>
-                <p style={{ color: "#94C5FF" }}>Mirror the market</p>
+                <p>Mirror the market</p>
             </div>
         </div>
     )
