5513954aa2f92544894c17e1120622cc2cfbf1d7
Srikanta <srikanta@fisdom.com>
2021-06-09 16:07:06 +0530

    css fix

--- react_webviews/src/dashboard/PassiveIndexFunds/mini-components/VideoBlockImageSection.js
+++ react_webviews/src/dashboard/PassiveIndexFunds/mini-components/VideoBlockImageSection.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 function VideoBlockImageSection() {
     return (
-        <div className="info-container" data-aid="info-container">
+        <div className="info-container" data-aid="info-container" style={{ color: "#94C5FF" }}>
             <div className="info-block" data-aid="info-block-1">
                 <div>
                     <img src={require("../../../assets/passive_info1.svg")} alt="" />
                 </div>
-                <p>Lower cost</p>
+                <p style={{ color: "#94C5FF" }}>Lower cost</p>
             </div>
             <p>|</p>
             <div className="info-block" data-aid="info-block-2">
                 <div>
                     <img src={require("../../../assets/passive_info2.svg")} alt="" />
                 </div>
-                <p>Broad diversification</p>
+                <p style={{ color: "#94C5FF" }}>Broad diversification</p>
             </div>
             <p>|</p>
             <div className="info-block" data-aid="info-block-3">
                 <div>
                     <img src={require("../../../assets/passive_info3.svg")} alt="" />
                 </div>
-                <p>Mirror the market</p>
+                <p style={{ color: "#94C5FF" }}>Mirror the market</p>
             </div>
         </div>
     )
