46679e2077369f816d767c1d102dd6dde37652d1
AlekhyaBathula <alekhya@fisdom.com>
2021-06-14 02:41:36 +0530

    Added rejected check for DL popup

--- react_webviews/src/kyc/Journey/Journey.js
+++ react_webviews/src/kyc/Journey/Journey.js
@@ -575,7 +575,8 @@ const Journey = (props) => {
       !show_aadhaar &&
       user.kyc_registration_v2 !== 'submitted' &&
       user.kyc_registration_v2 !== 'complete' &&
-      fromState !== "/kyc/digilocker/failed"
+      fromState !== "/kyc/digilocker/failed" &&
+      journeyStatus !== "rejected"
     ) {
       if (
         !storageService().get('show_aadhaar') &&
