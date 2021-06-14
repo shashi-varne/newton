f4b9bf2aa1056f435ef7d35e6217aba15ada99e4
AlekhyaBathula <alekhya@fisdom.com>
2021-06-14 15:39:17 +0530

    Fixed terms and privacy link in login and registration

--- react_webviews/src/login_and_registration/Login.js
+++ react_webviews/src/login_and_registration/Login.js
@@ -242,9 +242,9 @@ class Login extends Component {
               </div>
             )}
             <div className="agree-terms" data-aid='agree-terms'>
-              By signing in, you agree to fisdom's{" "}
+              By signing in, you agree to {config.productName}'s{" "}
               <a
-                href="https://www.fisdom.com/terms/"
+                href={config.termsLink}
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -252,7 +252,7 @@ class Login extends Component {
               </a>{" "}
               and{" "}
               <a
-                href="https://www.fisdom.com/privacy/"
+                href={config.privacyLink}
                 target="_blank"
                 rel="noopener noreferrer"
               >
