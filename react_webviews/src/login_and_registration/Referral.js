6f58b747178aae03c08194efd54c8ff65a61019b
Srikanta <srikanta@fisdom.com>
2021-06-15 15:53:44 +0530

    referral update

--- react_webviews/src/login_and_registration/Referral.js
+++ react_webviews/src/login_and_registration/Referral.js
@@ -1,6 +1,8 @@
-
+import "./Style.scss";
 import React, { Component } from 'react';
 import Container from "../dashboard/common/Container";
+import WVInPageSubtitle from "../common/ui/InPageHeader/WVInPageSubtitle"
+import Input from "common/ui/Input";
 import { getConfig } from 'utils/functions';
 
 
@@ -11,32 +13,66 @@ class Referral extends Component {
     this.state = {
       insurance_details: {},
       productName: getConfig().productName,
+      form_data: {},
+      isPromoApiRunning: false,
     }
   }
 
   componentWillMount() {
 
-
   }
 
 
+  
+
+
+  handleChange = (name) => (event) => {
+    let value = event.target ? event.target.value : event;
+    let { form_data } = this.state;
+    form_data[name] = value;
+    form_data[`${name}_error`] = "";
+    this.setState({ form_data: form_data });
+  };
+
   render() {
+
+    const { form_data, isPromoApiRunning } = this.state;
+
     return (
       <Container
-        events={this.sendEvents('just_set_events')}
         fullWidthButton={true}
-        onlyButton={true}
-        // handleClick={() => this.handleClick()}
-        buttonTitle="GIVE ME A CALL"
+        twoButtonVertical={true}
+        dualbuttonwithouticon={true}
+        button1Props={{
+          type: 'primary',
+          title: "CONTINUE",
+          onClick: () => console.log("H"),
+        }}
+        button2Props={{
+          type: 'secondary',
+          title: "SKIP",
+          onClick: () => console.log("A"),
+          showLoader: false,
+        }}
         showLoader={this.state.show_loader}
-        title="Let's talk &#38; help you out">
+        title="Do you have a referral code?">
 
 
+        <WVInPageSubtitle children={"This will map your account with our partner"} />
 
-        <h>hello man!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h>
-
-
-
+        <div className="form-field">
+          <Input
+            error={form_data.confirm_password_error ? true : false}
+            type="text"
+            value={form_data.confirm_password}
+            helperText={form_data.confirm_password_error || ""}
+            class="input"
+            id="referral_code"
+            label="Referral code"
+            name="referral_code"
+            onChange={this.handleChange("referral_code")}
+          />
+        </div>
 
       </Container>
     )
