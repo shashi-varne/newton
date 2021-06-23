4b6ff1cfaf216cb5bf444075e818e9478ee4da0c
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2021-06-23 07:37:49 +0530

    added tax2win feature list compoment

---
+++ react_webviews/src/tax_filing/mini-components/FeatureListItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './FeatureList.scss'
+
+function FeatureListItem({ imgSrc, bgImgSrc, title, subtitle }) {
+  return (
+    <div className="flex align-start">
+      <div
+        className="tax-filing-feature-icn-container flex align-center justify-center"
+        style={{ backgroundImage: `url(${bgImgSrc})` }}
+      >
+        <img src={imgSrc} alt={title} subtitle={subtitle} />
+      </div>
+      <div className="tax-filing-feature-content m-left-2x">
+        <div className="heading3-medium">{title}</div>
+        <div className="body-text2 text-secondary">{subtitle}</div>
+      </div>
+    </div>
+  )
+}
+
+export default FeatureListItem
--- react_webviews/src/tax_filing/pages/Landing.js
+++ react_webviews/src/tax_filing/pages/Landing.js
@@ -7,36 +7,41 @@ import HowToSteps from 'common/ui/HowToSteps'
 import StepsToFollow from '../../common/ui/stepsToFollow'
 import Carousal from '../mini-components/Carousal'
 import WVMenuListDropdownItem from '../../common/ui/MenuListDropdown/WVMenuListDropdownItem'
+import FeatureListItem from '../mini-components/FeatureListItem'
 
 // import { gettingStartedSteps } from '../constants'
 
-// function GetStarted({ title = 'Why eFile with us?', steps }) {
-//   const gettingStartedSteps = [
-//     {
-//       icon: 'icn_secure_safe',
-//       title: 'Secure & Safe ',
-//       subtitle: 'Income tax department authorized platform',
-//     },
-//     {
-//       icon: 'icn_tax_savings',
-//       title: 'Maximum tax savings',
-//       subtitle: 'Get every tax deduction you are eligible for',
-//     },
-//     {
-//       icon: 'icn_calculator',
-//       title: '100% accuracy',
-//       subtitle: 'Precise calculations to avoid overpaying of taxes',
-//     },
-//   ]
-//   return (
-//     <HowToSteps
-//       style={{ margin: '0px 0px 0px 0px', paddingTop: 0 }}
-//       baseData={{ title, options: gettingStartedSteps }}
-//       classNameIcon="steps-icon"
-//       showSkelton={true}
-//     />
-//   )
-// }
+function GetStarted({ title = 'Why eFile with us?', steps }) {
+  const productName = getConfig().productName
+  const gettingStartedSteps = [
+    {
+      frontImage: require('assets/icn_secure_safe.svg'),
+      bgImage: require(`assets/${productName}/bg_why_icons.svg`),
+      title: 'Secure & Safe ',
+      subtitle: 'Income tax department authorized platform',
+    },
+    {
+      frontImage: require('assets/icn_tax_savings.svg'),
+      bgImage: require(`assets/${productName}/bg_why_icons.svg`),
+      title: 'Maximum tax savings',
+      subtitle: 'Get every tax deduction you are eligible for',
+    },
+    {
+      frontImage: require('assets/icn_calculator.svg'),
+      bgImage: require(`assets/${productName}/bg_why_icons.svg`),
+      title: '100% accuracy',
+      subtitle: 'Precise calculations to avoid overpaying of taxes',
+    },
+  ]
+  return (
+    <FeatureListItem
+      imgSrc={require('assets/icn_secure_safe.svg')}
+      bgImgSrc={require(`assets/${productName}/bg_why_icons.svg`)}
+      title={`Secure & Safe`}
+      subtitle={`Income tax department authorized platform`}
+    />
+  )
+}
 
 const LandingPage = () => {
   const productName = getConfig().productName
@@ -56,7 +61,7 @@ const LandingPage = () => {
         // }}
       />
       <WVTag variant="info" label="free" dataAidSuffix="tax2win-free-tag" />
-      {/* <GetStarted /> */}
+      <GetStarted />
       <Carousal
         title="Taxation made simple"
         subtitle="File ITR easily, quickly and with maximum tax savings"
@@ -79,7 +84,7 @@ const LandingPage = () => {
 const Title = ({ title }) => {
   return (
     <div className="flex">
-      <div className="heading3-medium">Do it yourself</div> 
+      <div className="heading3-medium">Do it yourself</div>
       <WVTag
         variant="attention"
         label="free"
