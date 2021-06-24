e356d18ce52161adbc287842f5c25b8c091cd693
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2021-06-25 02:34:27 +0530

    added input validation and error handling

--- react_webviews/src/tax_filing/pages/PersonalDetails.js
+++ react_webviews/src/tax_filing/pages/PersonalDetails.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Container from '../common/Container'
 import Input from 'common/ui/Input'
+import { validateEmail, validateNumber } from 'utils/validators'
 
 import { navigate as navigateFunc } from '../common/functions'
 
@@ -12,6 +13,36 @@ function PersonalDetails(props) {
   const [name, setName] = useState('Ashish Thakur')
   const [email, setEmail] = useState('ashish.thakur@fisdom.com')
   const [mobileNumber, setMobileNumber] = useState('9999999999')
+
+  const [errors, setErrors] = useState({
+    email: false,
+    name: false,
+    mobileNumber: false,
+  })
+
+  const handleFocus = (type) => () => {
+    if (errors[type]) {
+      setErrors({ ...errors, [type]: false })
+    }
+  }
+
+  const handleBlur = (type) => () => {
+    switch (type) {
+      case 'email':
+        if (!validateEmail(email)) {
+          setErrors({ ...errors, email: true })
+        }
+        break
+      case 'mobileNumber':
+        if (!validateEmail(email)) {
+          setErrors({ ...errors, mobileNumber: true })
+        }
+      case 'name':
+        if (name.length === 0) {
+          setErrors({ ...errors, name: true })
+        }
+    }
+  }
 
   const handleChange = (type) => (event) => {
     const value = event.target.value.trim()
@@ -48,28 +79,42 @@ function PersonalDetails(props) {
           type="text"
           value={name}
           label="Name"
+          onFocus={handleFocus('name')}
+          onBlur={handleBlur('name')}
           onChange={handleChange('name')}
           class="block m-top-3x"
           variant="outlined"
-          error={true}
-          helperText="Valid name"
+          error={errors?.name}
+          helperText={errors?.name ? 'Please enter a valid name' : ''}
+          required
         />
         <Input
           type="email"
           value={email}
           label="Email"
+          onFocus={handleFocus('email')}
+          onBlur={handleBlur('email')}
           onChange={handleChange('email')}
           class="block m-top-3x"
           variant="outlined"
-          disabled
+          error={errors?.email}
+          helperText={errors?.email ? 'Please enter a valid email address' : ''}
+          required
         />
         <Input
           type="text"
           value={mobileNumber}
           label="Mobile Number"
+          onFocus={handleFocus('mobileNumber')}
+          onBlur={handleBlur('mobileNumber')}
           onChange={handleChange('mobileNumber')}
           class="block m-top-3x"
           variant="outlined"
+          error={errors?.mobileNumber}
+          helperText={
+            errors?.mobileNumber ? 'Please enter a correct mobile number' : ''
+          }
+          required
         />
       </form>
     </Container>
