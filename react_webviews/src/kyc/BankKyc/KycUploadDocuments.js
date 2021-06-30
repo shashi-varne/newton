10df8da5c9bcf6eb86521afbf199b08a551bad77
Sharique Asghar <sharique@fisdom.com>
2021-06-30 17:19:32 +0530

    added check on additional flag to go back

--- react_webviews/src/kyc/BankKyc/KycUploadDocuments.js
+++ react_webviews/src/kyc/BankKyc/KycUploadDocuments.js
@@ -200,8 +200,8 @@ const KycUploadDocuments = (props) => {
   };
 
   const goBackToPath = () => {
-    if (fromState.indexOf("/kyc/add-bank/details") !== -1) {
-      props.history.goBack();
+    if (additional) {
+      navigate(PATHNAME_MAPPER.bankList);
       return;
     } else {
       if (kyc?.kyc_status === "non-compliant" && (kyc?.kyc_type === "manual" || kyc?.address?.meta_data?.is_nri)) {
