54afa47219aed28c5bd0ad890f0102de4923541b
Srikanta <srikanta@fisdom.com>
2021-06-30 12:28:35 +0530

    removed lodash empty

--- react_webviews/src/gold/components/buy/home.js
+++ react_webviews/src/gold/components/buy/home.js
@@ -21,7 +21,6 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import GoldBottomSecureInfo from '../ui_components/gold_bottom_secure_info';
 import {Imgc} from '../../../common/ui/Imgc';
-import { isEmpty } from "lodash"
 
 const plusOptionsAmount = [
   500, 1000, 2000, 5000
