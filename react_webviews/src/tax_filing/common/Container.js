551c06c4bf6ae5f64cf3162cefabe8ea6e90ab0d
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2021-07-07 23:01:03 +0530

    ios hiding top bar on reaching fisdom

--- react_webviews/src/tax_filing/common/Container.js
+++ react_webviews/src/tax_filing/common/Container.js
@@ -5,6 +5,7 @@ import { didMount, commonRender } from 'common/components/container_functions'
 import { nativeCallback } from 'utils/native_callback'
 import '../../utils/native_listner'
 import { trackBackButtonPress, untrackBackButtonPress } from './functions'
+import { getConfig  } from 'utils/functions'
 
 class Container extends Component {
   constructor(props) {
