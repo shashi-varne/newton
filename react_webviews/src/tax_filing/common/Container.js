9049c9d49c31aa31f2f31bdd6184fe897c56d2a0
siddharthkrishnan <siddharthkrishnan@fisdom.com>
2021-07-08 16:22:23 +0530

    removed unused import

--- react_webviews/src/tax_filing/common/Container.js
+++ react_webviews/src/tax_filing/common/Container.js
@@ -5,7 +5,6 @@ import { didMount, commonRender } from 'common/components/container_functions'
 import { nativeCallback } from 'utils/native_callback'
 import '../../utils/native_listner'
 import { trackBackButtonPress, untrackBackButtonPress } from './functions'
-import { getConfig } from 'utils/functions'
 
 class Container extends Component {
   constructor(props) {
