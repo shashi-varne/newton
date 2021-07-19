ad22287acf2499e8965afad6e083f6c5efd88ac4
AlekhyaBathula <alekhya@fisdom.com>
2021-07-19 13:46:26 +0530

    Added finity completed step asset

--- react_webviews/src/reports/components/mini-components/ProgressStep.js
+++ react_webviews/src/reports/components/mini-components/ProgressStep.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { getConfig } from "../../../utils/functions";
 
+const productName = getConfig().productName;
 const ProgressStep = ({ isCompleted, text, subtext }) => {
   return (
     <div className="progress" data-aid='progress-step'>
       <div className="content">
         <hr className="left"></hr>
         {isCompleted ? (
-          <img src={require(`assets/completed_step.svg`)} alt="" />
+          <img src={require(`assets/${productName}/completed_step.svg`)} alt="" />
         ) : (
           <span className="dot"></span>
         )}
