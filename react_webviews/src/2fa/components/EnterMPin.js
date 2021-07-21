8ca97ba3e586d88b4d6c586562bde90fc6ad8db1
Karan Uday <karan.uday@fisdom.com>
2021-07-21 20:02:43 +0530

    FIxed styling and renamed VerifyMPin to EnterMPin

---
+++ react_webviews/src/2fa/components/EnterMPin.js
@@ -0,0 +1,46 @@
+import './commonStyles.scss';
+import React from 'react';
+import WVOtp from '../../common/ui/Otp/WVOtp';
+
+const EnterMPin = ({
+    otpProps = {},
+    title = '',
+    subtitle = '',
+    children
+}) => {
+    return (
+        <div className="verify-mpin">
+            {title &&
+                <Title className="vm-title">
+                    {title || 'Enter your current fisdom PIN'}
+                </Title>
+            }
+            {subtitle &&
+                <Subtitle className="vm-subtitle">
+                    {subtitle}
+                </Subtitle>
+            }
+            {children}
+            <WVOtp
+                handleOtp={otpProps.handleOtp}
+                value={otpProps.otp}
+                isDisabled={otpProps.isDisabled}
+                hasError={otpProps.hasError}
+                bottomText={otpProps.bottomText}
+                classes={{ container: 'vm-otp-container' }}
+            />
+        </div>
+    );
+}
+
+const Title = ({ children, ...props }) => {
+    return <div {...props}>{children}</div>
+}
+EnterMPin.Title = Title;
+
+const Subtitle = ({ children, ...props }) => {
+    return <div {...props}>{children}</div>
+}
+EnterMPin.Subtitle = Subtitle;
+
+export default EnterMPin;
