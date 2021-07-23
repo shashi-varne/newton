1d51e665b3fd2613256b9ba5d88fbebceffffb0d
Srikanta <srikanta@fisdom.com>
2021-07-23 20:09:47 +0530

    VerifyPin Added children prop

--- react_webviews/src/dashboard/MyAccount/2fa/VerifyPin.js
+++ react_webviews/src/dashboard/MyAccount/2fa/VerifyPin.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import WVClickableTextElement from "common/ui/ClickableTextElement/WVClickableTextElement"
 import Container from "../../common/Container";
 import EnterMPin from "../../../2fa/components/EnterMPin";
+import { Imgc } from "../../../common/ui/Imgc";
 import { navigate as navigateFunc } from "../../../utils/functions";
 
 const VerifyPin = (props) => {
@@ -25,8 +26,10 @@ const VerifyPin = (props) => {
       data-aid='my-account-screen'
       showLoader={showLoader}
       handleClick={handleClick}
-      buttonTitle="Continue"
-    > 
+      noFooter={true}
+      hideInPageTitle
+      hidePageTitle
+    >
       <EnterMPin
         otpProps={{
           handleChange: handleOtp,
@@ -35,7 +38,16 @@ const VerifyPin = (props) => {
           // hasError:,
           bottomText: "Enter Fisdom PIN"
         }}
-      />
+      >
+        <Imgc
+          src={require(`assets/padlock1.svg`)}
+          alt=""
+          style={{ height: '20px', width: '20px', marginBottom: '20px' }}
+        />
+        <EnterMPin.Title style={{ marginBottom: '75px' }}>
+          Enter your current fisdom PIN
+        </EnterMPin.Title>
+      </EnterMPin>
       <WVClickableTextElement onClick={() => navigate("/forgot-fisdom-pin")}>
         <p className="clickable-text-ele">FORGOT PIN?</p>
       </WVClickableTextElement>
