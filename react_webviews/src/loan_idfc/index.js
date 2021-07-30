29f05f8f52ffa588e1f65d02eac8fd8a4fd0b146
AlekhyaBathula <alekhya@fisdom.com>
2021-07-30 12:16:25 +0530

    Removed JssProvider & MuiThemeProvider from idfc_loan, insurance, help_support_v2

--- react_webviews/src/loan_idfc/index.js
+++ react_webviews/src/loan_idfc/index.js
@@ -1,10 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { withRouter } from "react-router";
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
 import "../common/theme/Style.scss";
-import { themeConfig } from 'utils/constants';
-import { ToastContainer } from 'react-toastify';
 
 import "./components/Style.scss";
 import NotFound from "../common/components/NotFound";
@@ -77,42 +73,10 @@ import SelectLoan from "./components/home/select_loan";
 import Recommended from "./components/home/recommended";
 import LoanKnowMore from "./components/home/know_more";
 
-import { create } from "jss";
-import JssProvider from "react-jss/lib/JssProvider";
-import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
-
-const generateClassName = createGenerateClassName({
-  dangerouslyUseGlobalCSS: true,
-  productionPrefix: "f"
-});
-const jss = create(jssPreset());
-// We define a custom insertion point that JSS will look for injecting the styles in the DOM.
-// jss.options.insertionPoint = 'jss-insertion-point';
-
-const theme = createMuiTheme(themeConfig);
-
-const ScrollToTop = withRouter(
-  class ScrollToTopWithoutRouter extends Component {
-    componentDidUpdate(prevProps) {
-      if (this.props.location !== prevProps.location) {
-        window.scrollTo(0, 0);
-      }
-    }
-
-    render() {
-      return null;
-    }
-  }
-);
-
 const Lending = props => {
   const { url } = props.match;
 
   return (
-    <JssProvider jss={jss} generateClassName={generateClassName}>
-      <MuiThemeProvider theme={theme}>
-        <ScrollToTop />
-        <ToastContainer autoClose={3000} />
         <Switch>
 
           {/* // DMI */}
@@ -221,8 +185,6 @@ const Lending = props => {
           <Route component={NotFound} />
 
         </Switch>
-      </MuiThemeProvider>
-    </JssProvider>
   );
 };
 
