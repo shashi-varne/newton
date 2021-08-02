2e006afc1101df8fe20a09d43baf8c51bc2f32c9
Satendra <satendra@fisdom.com>
2021-08-02 15:36:07 +0530

    back_url and message removed

--- react_webviews/src/tax_filing/pages/Redirection.js
+++ react_webviews/src/tax_filing/pages/Redirection.js
@@ -41,10 +41,6 @@ function Redirection(props) {
       }
       nativeCallback({
         action: 'take_control',
-        message: {
-          back_url: backUrl,
-          back_text: 'You are almost there, do you really want to go back?',
-        },
         enable_history_back: true,
       })
       window.location.href =
