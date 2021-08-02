d7b9aafeace981e93245dcababb98482bfd77a7b
AlekhyaBathula <alekhya@fisdom.com>
2021-08-02 21:33:36 +0530

    Fixed image & skelton loading of kyc card

--- react_webviews/src/dashboard/Invest/components/Landing/Landing.js
+++ react_webviews/src/dashboard/Invest/components/Landing/Landing.js
@@ -219,7 +219,7 @@ class Landing extends Component {
             <SkeltonRect
               style={{
                 width: '100%',
-                height: '270px',
+                height: '90px',
                 marginBottom: "15px",
               }}
             />
