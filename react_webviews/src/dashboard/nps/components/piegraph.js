9ab1283b348e52c75060bee09a7dc2ca80ea35bb
fdamitsingh <amit.singh@fisdom.com>
2021-08-11 22:04:25 +0530

    refactoed the code

--- react_webviews/src/dashboard/nps/components/piegraph.js
+++ react_webviews/src/dashboard/nps/components/piegraph.js
@@ -1,26 +1,5 @@
 import React from 'react';
-// import { ResponsivePie } from '@nivo/pie';
 import ReactHighcharts from "react-highcharts";
-// export default function PieChart (props) {
-//   const { width = 0, height = 0, ...otherProps } = props;
-  
-//   return (
-//     // Require a container div since ResponsivePieChart fit to size of its contianer
-//     <div style={{ width: `${width}px`, height: `${height}px` }}>
-//       <ResponsivePie
-//         data={props.data}
-//         innerRadius={0.6}
-//         enableRadialLabels={false}
-//         enableSliceLabels={false}
-//         padAngle={1}
-//         animate={true}
-//         colors={{ datum: 'data.color' }}
-//         {...otherProps}
-//       />
-//     </div>
-//   );
-// }
-
 
 const PieChart = (props) => {
   const { width = 120, height = 120, ...otherProps } = props;
