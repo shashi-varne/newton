e2fb1ed364a78f354c6333b5bb4f2a6d191c2ada
srikantagowda <srikanta@fisdom.com>
2021-08-13 23:14:38 +0530

    pr fix

--- react_webviews/src/login_and_registration/pages/SecondaryVerification/SecondaryVerification.js
+++ react_webviews/src/login_and_registration/pages/SecondaryVerification/SecondaryVerification.js
@@ -19,7 +19,7 @@ class SecondaryVerification extends Component {
         super(props);
         this.state = {
             productName: getConfig().productName,
-            form_data: { whatsapp_consent: true, code: "+91" },
+            form_data: { whatsapp_consent: true, code: "91" },
             accountAlreadyExists: false,
             isEdit: false
         }
@@ -39,7 +39,7 @@ class SecondaryVerification extends Component {
         }
         this.setState({ loginType, form_data })
         countries.map((item) => {
-            return item.name = "+" + item.value;
+            return item.name = `+${item.value}`
         })
     }
 
@@ -56,11 +56,11 @@ class SecondaryVerification extends Component {
         let value = event.target ? event.target.value : event;
         let { form_data } = this.state;
         if(name === "mobile" && value && !validateNumber(value)) return;
-        if(name === "mobile" && form_data.code === "+91" & value.length > 10) return;
+        if(name === "mobile" && form_data.code === "91" & value.length > 10) return;
         form_data[name] = value;
         if(name === "whatsapp_consent") form_data[name] = !form_data?.whatsapp_consent;
         form_data[`${name}_error`] = "";
-        this.setState({ form_data: form_data });
+        this.setState({ form_data });
     };
 
     handleClick = async () => {
@@ -70,25 +70,9 @@ class SecondaryVerification extends Component {
         } = this.state;
         let keys_to_check = ["mobile", "code"];
         if (loginType === "email") keys_to_check = ["email"];
-        this.checkValidation(loginType, form_data)
-        let result = await this.authCheckApi(loginType, {
-            "contact_value": form_data[loginType]
-        });
-        if (result && result.is_user) {
-            this.setState({
-                accountAlreadyExists: true,
-                accountAlreadyExistsData: result.user,
-                verifyDetailsType: loginType,
-            })
-        } else if (result && !result.is_user) {
-            this.formCheckFields(keys_to_check, form_data, "LOGIN", loginType, true);
-        }
-    }
-    
-    checkValidation = (loginType, form_data) => {
         if (loginType === "mobile" &&
-            (!validateNumber(form_data["mobile"] && isEmpty(form_data["code"])) ||
-                (form_data["code"] === "+91" && form_data["mobile"].length < 10))) {
+            (!validateNumber(form_data["mobile"]) || isEmpty(form_data["code"]) ||
+                (form_data["code"] === "91" && form_data["mobile"].length < 10))) {
             this.setState({
                 form_data: {
                     ...form_data,
@@ -107,6 +91,18 @@ class SecondaryVerification extends Component {
             });
             toast("Invalid email");
             return;
+        }
+        let result = await this.authCheckApi(loginType, {
+            "contact_value": form_data[loginType]
+        });
+        if (result && result.is_user) {
+            this.setState({
+                accountAlreadyExists: true,
+                accountAlreadyExistsData: result.user,
+                verifyDetailsType: loginType,
+            })
+        } else if (result && !result.is_user) {
+            this.formCheckFields(keys_to_check, form_data, "LOGIN", loginType, true);
         }
     }
 
@@ -144,7 +140,7 @@ class SecondaryVerification extends Component {
         if (type === "email") {
             body.email = form_data?.email;
         } else {
-            body.mobile = form_data?.mobile;
+            body.mobile = `${form_data["code"]}|${form_data["mobile"]}`;
             body.whatsapp_consent = true;
         };
         await this.triggerOtpApi(body, type);
@@ -191,7 +187,7 @@ class SecondaryVerification extends Component {
                                         error={form_data.code_error ? true : false}
                                         helperText={form_data.code_error || ""}
                                         options={countries}
-                                        value={form_data.code || "+91"}
+                                        value={form_data.code || "91"}
                                         width={20}
                                         id="code"
                                         name="code"
@@ -214,7 +210,7 @@ class SecondaryVerification extends Component {
                                     />
                                 </span>
                             </div>
-                            <WVInPageSubtitle style={{ marginTop : form_data?.mobile_error ? "12px" : ""}} children={"We'll send an OTP to verify your mobile number"} />
+                            <div style={{ marginTop : form_data?.mobile_error ? "12px" : "2px"}} className="input-subtitle">We'll send an OTP to verify your mobile number</div>
                             <div className="declaration-container whatsapp-consent">
                                 <Checkbox
                                     defaultChecked
@@ -246,7 +242,7 @@ class SecondaryVerification extends Component {
                                     autoFocus
                                 />
                             </div>
-                            <WVInPageSubtitle children={"we'll keep you updated on your Investments"} className="input-subtitle" />
+                            <div className="input-subtitle">we'll keep you updated on your Investments</div>
                         </>
                     }
                 </div>
