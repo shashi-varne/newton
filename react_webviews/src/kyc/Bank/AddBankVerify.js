c9e35d81ed3b3aeef17dc1d11bafe08bf4f23c83
Sharique Asghar <sharique@fisdom.com>
2021-08-23 12:47:11 +0530

    added some conditons to show different content on different sceanrios

--- react_webviews/src/kyc/Bank/AddBankVerify.js
+++ react_webviews/src/kyc/Bank/AddBankVerify.js
@@ -287,7 +287,7 @@ const AddBankVerify = (props) => {
           uploadDocuments={uploadDocuments}
           checkBankDetails={checkBankDetails}
         />
-        <PennySuccessDialog isOpen={isPennySuccess} redirect={goTobankLists} />
+        <PennySuccessDialog isOpen={isPennySuccess} kyc={userKyc} redirect={goTobankLists} />
         <PennyExhaustedDialog
           isOpen={isPennyExhausted}
           redirect={goTobankLists}
