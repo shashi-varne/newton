4177adc9c27ff93c7c40680fa7fc16c6ec1c40a5
fdamitsingh <amit.singh@fisdom.com>
2021-08-25 19:29:41 +0530

    added the check for IFSC code through BANK_IFSC_CODES in kycbankdetails file

--- react_webviews/src/kyc/constants.js
+++ react_webviews/src/kyc/constants.js
@@ -230,6 +230,8 @@ export const getIfscCodeError = (code) => {
       return "Please enter a valid ifsc code of City Union Bank";
     case "ippb":
       return "Please enter a valid ifsc code of India Post Payments Bank";
+    case "ucomb":
+      return "Please enter a valid ifsc code of United Commercial Bank";
     default:
       return "Please enter a valid ifsc code";
   }
