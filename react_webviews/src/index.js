342d4a6fef8a5e052212747c9b93905ebd1e10e1
srikantagowda <srikanta@fisdom.com>
2021-08-31 15:29:14 +0530

    added check for status 400 to prevent Sentry Validation Error being reported

--- react_webviews/src/index.js
+++ react_webviews/src/index.js
@@ -105,33 +105,24 @@ if(getConfig().productName === 'finity') {
   favicon.href = './images/fisdom_icon.svg';
 }
 
-var tracesSampleRate = 1.0;
 if(getConfig().productName === "fisdom" && getConfig().isProdEnv)
 {
   Sentry.init({
     dsn: "https://38815adc8fd842e78c2145a583d26351@o60572.ingest.sentry.io/5726998",
     beforeSend(event) {
-      if (event?.level === "warning") {
-        return tracesSampleRate = 0;
-      }
       event.tags = event.tags || {};
       event.tags["partner_code"] = getConfig().code;
       event.tags["user_id"] = storageService()?.getObject('user')?.user_id;
       return event;
     },
     integrations: [new Integrations.BrowserTracing()],
-    allowUrls: ["app.fisdom.com", "wv.fisdom.com"],
-    tracesSampler: samplingContext => {
-      return tracesSampleRate;
-    }
+    allowUrls: ["app.fisdom.com", "wv.fisdom.com", "http://localhost:3000/"],
+    tracesSampleRate: 1.0,
   });
 } else if (getConfig().productName === "finity" && getConfig().isProdEnv) {
   Sentry.init({
     dsn: "https://84e342a0046748bab6860aafcf7e86da@o60572.ingest.sentry.io/5727007",
     beforeSend(event) {
-      if (event?.level === "warning") {
-        return tracesSampleRate = 0;
-      }
       event.tags = event.tags || {};
       event.tags["partner_code"] = getConfig().code;
       event.tags["user_id"] = storageService()?.getObject('user')?.user_id;
@@ -139,9 +130,7 @@ if(getConfig().productName === "fisdom" && getConfig().isProdEnv)
     },
     integrations: [new Integrations.BrowserTracing()],
     allowUrls: ["app.mywaywealth.com", "app.finity.in", "wv.mywaywealth.com", "wv.finity.in"],
-    tracesSampler: samplingContext => {
-      return tracesSampleRate;
-    }
+    tracesSampleRate: 1.0,
   });
 }
 
