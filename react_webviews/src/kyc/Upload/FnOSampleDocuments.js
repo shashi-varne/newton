21eac9a2a8f6c0a402717d029caabdbd4eac57a9
Sharique Asghar <sharique@fisdom.com>
2021-09-09 19:03:20 +0530

    added assets for itr and salary_slip

--- react_webviews/src/kyc/Upload/FnOSampleDocuments.js
+++ react_webviews/src/kyc/Upload/FnOSampleDocuments.js
@@ -5,9 +5,9 @@ import SampleDocuments from '../mini-components/SampleDocuments'
 const description = "Please ensure the documents are as per the details required. If you have multiple documents please merge them into one.";
 const documents = [
   // Todo: update doc after getting asset
-  { title: 'Bank Statement (Last 6 months)', name: "(BANK SAMPLE DOCUMENT)", doc: 'cancelled_cheque' },
-  { title: 'ITR (Any from last 2 years)', name: "(ITR SAMPLE DOCUMENT)", doc: 'passbook_first_page' },
-  { title: 'Salary Slip (Last 3 months) ', name: "(PAYSLIP SAMPLE DOCUMENT)", doc: 'bank_statement' },
+  { title: 'Bank Statement (Last 6 months)', name: "(BANK SAMPLE DOCUMENT)", doc: 'bank_statement' },
+  { title: 'ITR (Any from last 2 years)', name: "(ITR SAMPLE DOCUMENT)", doc: 'itr' },
+  { title: 'Salary Slip (Last 3 months) ', name: "(PAYSLIP SAMPLE DOCUMENT)", doc: 'salary_slip' },
 ]
 
 const FnOSampleDocuments = (props) => {
