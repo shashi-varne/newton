c729dedff254a9d50ebbf8e9c0d659c8c85215bc
karan.uday <karan.uday@fisdom.com>
2021-09-22 17:19:46 +0530

    Added a custom require function for assets import
    - handles dynamic image paths
    - adds fallback to check assets folder for cases when asset is not found in partner folder
    - graceful error handling for unreachable/unfound resources

--- react_webviews/src/utils/functions.js
+++ react_webviews/src/utils/functions.js
@@ -683,4 +683,16 @@ export function getGuestUserRoute(apiUrl){
       return apiUrl + `${url_char}guest_lead_id=${guest_id}`
   }
   return apiUrl
-}  
+}
+
+export function requireAsset(assetName, partner) {
+  try {
+    return require(`assets/${partner ? (partner + '/' + assetName) : assetName}`);
+  } catch (err) {
+    try {
+      return require(`assets/${assetName}`);
+    } catch (err) {
+      console.log('Could not find the asset you are looking for!', err);
+    }
+  }
+}
