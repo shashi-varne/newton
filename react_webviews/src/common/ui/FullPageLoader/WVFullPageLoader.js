92171d55b8f67122ffe7385b6e7fa18a24c02ec7
karan <karan.uday@fisdom.com>
2021-10-13 19:50:10 +0530

    Changes to FullPageLoader to support loading quotes

--- react_webviews/src/common/ui/FullPageLoader/WVFullPageLoader.js
+++ react_webviews/src/common/ui/FullPageLoader/WVFullPageLoader.js
@@ -11,29 +11,15 @@
 import './WVFullPageLoader.scss';
 import React from 'react';
 
-const QUOTES = [
-  'Risk comes from not knowing what you are doing.',
-  'Money is a terrible master but an excellent servant.',
-  'Wealth is the ability to fully experience life.',
-  'I’d like to live as a poor man with lots of money.',
-  'Don’t stay in bed, unless you can make money in bed.',
-  'It takes as much energy to wish as it does to plan.',
-  'The best thing money can buy is financial freedom.'
-];
-
 const WVFullPageLoader = ({
   classes,
-  loadingText,
-  showQuote = true,
+  loadingText
 }) => {
-  const quote = QUOTES[Math.floor(Math.random() * QUOTES.length)];
   return (
     <div className={`wv-full-page-loader ${classes?.container}`}>
       <div className="wv-fpl-overlay">
         <div className="wv-fpl-loading-circle"></div>
-        <div className="wv-fpl-loading-text">
-          {loadingText || (showQuote && quote)}
-        </div>
+        <div className="wv-fpl-loading-text">{loadingText}</div>
       </div>
     </div>
   );
