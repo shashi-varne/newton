142c4b2d53c182a0b9755b487ae362b20e811cdb
karan <karan.uday@fisdom.com>
2021-10-20 13:00:59 +0530

    Added quotes support for FullPageLoader

--- react_webviews/src/common/ui/FullPageLoader/WVFullPageLoader.js
+++ react_webviews/src/common/ui/FullPageLoader/WVFullPageLoader.js
@@ -9,20 +9,36 @@
     />
 */
 import './WVFullPageLoader.scss';
-import React from 'react';
+import React, { memo } from 'react';
+
+const QUOTES = [
+  // 'Risk comes from not knowing what you are doing.',
+  // 'Money is a terrible master but an excellent servant.',
+  // 'Wealth is the ability to fully experience life.',
+  // 'I’d like to live as a poor man with lots of money.',
+  // 'Don’t stay in bed, unless you can make money in bed.',
+  // 'It takes as much energy to wish as it does to plan.',
+  // 'The best thing money can buy is financial freedom.',
+  'The best thing money can buy is financial freedom.'
+];
 
 const WVFullPageLoader = ({
   classes,
-  loadingText
+  loadingText,
+  showQuote = true,
 }) => {
+  const quote = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+
   return (
     <div className={`wv-full-page-loader ${classes?.container}`}>
       <div className="wv-fpl-overlay">
         <div className="wv-fpl-loading-circle"></div>
-        <div className="wv-fpl-loading-text">{loadingText}</div>
+        <div className="wv-fpl-loading-text">
+          {loadingText || (showQuote && quote)}
+        </div>
       </div>
     </div>
   );
 }
 
-export default WVFullPageLoader;
+export default memo(WVFullPageLoader);
