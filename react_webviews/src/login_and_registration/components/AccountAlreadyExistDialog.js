9fd13cb98649031de478e841753b0d2a7510c70a
Karan Uday <karan.uday@fisdom.com>
2021-11-22 20:44:26 +0530

    Fixes for WEBAPP-1925
    - Converted AccountExistsDialog into functional component
    - Added cases for missing mobile/email or pan

--- react_webviews/src/login_and_registration/components/AccountAlreadyExistDialog.js
+++ react_webviews/src/login_and_registration/components/AccountAlreadyExistDialog.js
@@ -1,55 +1,66 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useMemo, useState } from "react";
 import WVBottomSheet from "../../common/ui/BottomSheet/WVBottomSheet";
 import { getConfig } from "utils/functions";
 import { isEmpty } from "lodash";
 import "./commonStyles.scss";
 
 const product = getConfig().productName;
-export class AccountAlreadyExistDialog extends Component {
-  constructor(props) {
-    super(props);
+export function AccountAlreadyExistDialog({
+  data = {},
+  isOpen,
+  onClose,
+  type,
+  ...props
+}) {
+  const { mobile, email, pan_number } = useMemo(() => isEmpty(data) ? {} : data, [data]);
+  const secondaryDetail = useMemo(() => type === 'email' ? mobile : email, [type, mobile, email]);
+  const [subtitleText, setSubtitleText] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [secondaryDetailExists, setSecondaryDetailExists] = useState(isEmpty(secondaryDetail));
 
-    this.state = {
-      loading: false,
-    };
-  }
+  useEffect(() => {
+    const detailExists = isEmpty(secondaryDetail);
+    setSecondaryDetailExists(detailExists);
+    if (detailExists) {
+      setSubtitleText(`
+        Your ${type === "email" ? "email address" : "mobile number"} is already
+        registered with {secondaryDetailExists && <span>some other account</span>
+      `);
+    } else {
+      setSubtitleText('Your account is already registered with us');
+    }
+  }, [secondaryDetail]);
 
-
-  render() {
-    const { data = {}, isOpen, onClose, type, next, editDetails } = this.props;
-    const alternateRegisteredAccount = type === "email" ? isEmpty(data.mobile) : isEmpty(data.email) || isEmpty(data);
-    return (
-      <WVBottomSheet
-        isOpen={isOpen}
-        onClose={onClose}
-        title={`Account already exists!`}
-        image={require(`../../assets/${product}/bottomsheet_account_exist.svg`)}
-        button1Props={{
-          color: "secondary",
-          variant: "outlined",
-          title: `EDIT ${type === "email" ? "EMAIL" : "NUMBER"}`,
-          onClick: editDetails,
-        }}
-        button2Props={{
-          color: "secondary",
-          variant: "contained",
-          title: "CONTINUE",
-          showLoader: this.state.loading,
-          onClick: () => {
-            next(type, data)
-            this.setState({
-              loading: true,
-            })
-          }
-        }}
-        classes={{
-          container: "account-already-exists-container",
-        }}
-      >
-        <p className="text">
-          Your {type === "email" ? "email address" : "mobile number"} is already
-          registered with {alternateRegisteredAccount && <span>some other account</span>}
-        </p>
+  return (
+    <WVBottomSheet
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Account already exists!"
+      image={require(`assets/${product}/bottomsheet_account_exist.svg`)}
+      button1Props={{
+        color: "secondary",
+        variant: "outlined",
+        title: `EDIT ${type === "email" ? "EMAIL" : "NUMBER"}`,
+        onClick: props.editDetails,
+      }}
+      button2Props={{
+        color: "secondary",
+        variant: "contained",
+        title: "CONTINUE",
+        showLoader: loading,
+        onClick: () => {
+          props.next(type, data)
+          setLoading(true);
+        }
+      }}
+      classes={{
+        container: "account-already-exists-container",
+      }}
+    >
+      <p className="text">
+        {subtitleText}
+      </p>
+      {(secondaryDetailExists || pan_number) &&
         <div
           style={{
             display: "flex",
@@ -57,39 +68,37 @@ export class AccountAlreadyExistDialog extends Component {
             justifyContent: "space-around",
           }}
         >
-          {(type === "email" ? data?.mobile : data?.email) && (
-            <div className="details">
-              <img
-                src={require(`../../assets/bottom_sheet_icon_${type}.svg`)}
-                alt=""
-                style={{ paddingRight: "10px" }}
-              />
-              <span className="text">
-                {type === "email" ? data?.mobile : data?.email}
-              </span>
-            </div>
-          )}
-          {(type === "email" ? data?.mobile : data?.email) && data?.pan_number && (
-            <div style={{ flexBasis: "20%" }}>
-              <p className="text" style={{ textAlign: "center" }}>
-                |
-              </p>
-            </div>
-          )}
-          {data?.pan_number && (
-            <div className="details">
-              <img
-                src={require(`../../assets/bottom_sheet_icon_pan.svg`)}
-                alt=""
-                style={{ paddingRight: "10px" }}
-              />
-              <span className="text">{data?.pan_number}</span>
-            </div>
+          <div className="details">
+            <img
+              src={require(`../../assets/bottom_sheet_icon_${type}.svg`)}
+              alt=""
+              style={{ paddingRight: "10px" }}
+            />
+            <span className="text">
+              {secondaryDetailExists ? secondaryDetail : 'Not found'}
+            </span>
+          </div>
+          {pan_number && (
+            <>
+              <div style={{ flexBasis: "20%" }}>
+                <p className="text" style={{ textAlign: "center" }}>
+                  |
+                </p>
+              </div>
+              <div className="details">
+                <img
+                  src={require(`../../assets/bottom_sheet_icon_pan.svg`)}
+                  alt=""
+                  style={{ paddingRight: "10px" }}
+                />
+                <span className="text">{pan_number}</span>
+              </div>
+            </>
           )}
         </div>
-      </WVBottomSheet>
-    );
-  }
+      }
+    </WVBottomSheet>
+  );
 }
 
 export default AccountAlreadyExistDialog;
