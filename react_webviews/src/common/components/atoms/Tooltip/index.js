539c250b5f9fef59a7e4c8e3473b8daf136335fd
AlekhyaBathula <alekhya@fisdom.com>
2021-12-23 18:08:16 +0530

    Renamed tootip to index

--- react_webviews/src/common/components/atoms/Tooltip/index.js
+++ react_webviews/src/common/components/atoms/Tooltip/index.js
@@ -1,1 +1,13 @@
-export { default } from "./Tooltip";
+import React from "react";
+import Tooltip from "@mui/material/Tooltip";
+
+const ToolTip = (props) => {
+  const { title = "", children, ...restProps } = props;
+  return (
+    <Tooltip title={title} {...restProps}>
+      {children}
+    </Tooltip>
+  );
+};
+
+export default ToolTip;
