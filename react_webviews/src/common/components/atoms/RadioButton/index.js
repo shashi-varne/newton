9786a454efb75ee22bb650d964fa00542e586722
AlekhyaBathula <alekhya@fisdom.com>
2021-12-24 18:33:32 +0530

    Added prop types

--- react_webviews/src/common/components/atoms/RadioButton/index.js
+++ react_webviews/src/common/components/atoms/RadioButton/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Radio from "@mui/material/Radio";
 import { noop } from "lodash";
+import PropTypes from "prop-types";
 
 const RadioButton = (props) => {
   const {
     isSelected = false,
     isDisabled = false,
     onChange = noop,
+    dataAid = "",
     ...restProps
   } = props;
   return (
@@ -14,9 +16,16 @@ const RadioButton = (props) => {
       disabled={isDisabled}
       checked={isSelected}
       onChange={onChange}
+      data-aid={`atom-radio-button-${dataAid}`}
       {...restProps}
     />
   );
 };
 
 export default RadioButton;
+
+RadioButton.propTypes = {
+  isSelected: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  onChange: PropTypes.func,
+}
