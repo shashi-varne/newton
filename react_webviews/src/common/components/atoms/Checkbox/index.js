3640ebceffe3e1f24d7979d013918597957f0bf1
AlekhyaBathula <alekhya@fisdom.com>
2021-12-24 18:21:37 +0530

    Added prop types

--- react_webviews/src/common/components/atoms/Checkbox/index.js
+++ react_webviews/src/common/components/atoms/Checkbox/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Checkbox from "@mui/material/Checkbox";
 import { noop } from "lodash";
+import PropTypes from "prop-types";
 
 const CheckBox = (props) => {
   const {
     isSelected = false,
     isDisabled = false,
     onChange = noop,
+    dataAid, 
     ...restProps
   } = props;
   return (
@@ -14,9 +16,16 @@ const CheckBox = (props) => {
       disabled={isDisabled}
       checked={isSelected}
       onChange={onChange}
+      data-aid={`atom-checkbox-${dataAid}`}
       {...restProps}
     />
   );
 };
 
 export default CheckBox;
+
+CheckBox.propTypes = {
+  isSelected: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  onChange: PropTypes.func,
+};
