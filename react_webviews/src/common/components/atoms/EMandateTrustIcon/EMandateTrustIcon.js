84685745bf0894f6a6e30a215c4986b85a2520b7
AlekhyaBathula <alekhya@fisdom.com>
2021-12-24 19:39:20 +0530

    Added emandate trust icon

---
+++ react_webviews/src/common/components/atoms/EMandateTrustIcon/EMandateTrustIcon.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Imgc } from "../../../ui/Imgc";
+import color from "../../../../theme/colors";
+import "./EMandateTrustIcon.scss";
+
+const EMandateTrustIcon = (props) => {
+  const { className = "", dataAid = "" } = props;
+  return (
+    <div
+      className={`atom-e-mandate-trust-icon ${className}`}
+      data-aid={`atom-e-mandate-trust-icon-${dataAid}`}
+    >
+      <Typography variant="body5" color={color.content.secondary}>
+        e-mandate powered by
+      </Typography>
+      <Imgc
+        className="aemti-icon"
+        src={require("assets/trust_icons_emandate.svg")}
+        alt="NACH"
+      />
+    </div>
+  );
+};
+
+export default EMandateTrustIcon;
