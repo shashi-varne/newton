20a2fb922bf3c9a4aec64ccd7d02d8c1fcb70271
fdamitsingh <amit.singh@fisdom.com>
2022-01-01 23:47:10 +0530

    Add: Collapse Molecule

---
+++ react_webviews/src/designSystem/molecules/Collapse/Collapse.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Typography, Collapse as CollapseLib } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import './Collapse.scss';
+
+const Collapse = ({
+  isOpen,
+  label,
+  onClick,
+  children,
+  expandedIcon,
+  collapsedIcon,
+  labelProps={},
+  childWrapperClass=''
+}) => {
+  return (
+    <div className='c-wrapper'>
+      <div className='c-label-wrapper' onClick={onClick}>
+        <Typography variant='heading3' {...labelProps}>{label}</Typography>
+        <div>{isOpen ? expandedIcon : collapsedIcon}</div>
+      </div>
+      <CollapseLib in={isOpen}>
+        <div className={`c-child-wrapper ${childWrapperClass}`}>{children}</div>
+      </CollapseLib>
+    </div>
+  );
+};
+
+Collapse.defaultProps = {
+  expandedIcon: <KeyboardArrowUpIcon />,
+  collapsedIcon: <KeyboardArrowDownIcon />,
+};
+
+export default Collapse;
