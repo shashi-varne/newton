59f07add3d8b769e98a82e5c8c41a9be4b95394a
fdamitsingh <amit.singh@fisdom.com>
2022-01-03 01:08:06 +0530

    MODIFY: added prop desciption and color support for title and subtitle.

--- react_webviews/src/designSystem/molecules/InfoCard/InfoCard.js
+++ react_webviews/src/designSystem/molecules/InfoCard/InfoCard.js
@@ -1,25 +1,37 @@
+/*
+  Below are props description
+  imgSrc : source path of the image.
+  imgProps(object): other props used for image.
+  title(string): Title using variant heading4.
+  subtitle(string): subtitle using variant body2.
+  titleColor,subtitleColor : it is strongly recommended to only use foundation colors for these props.
+  Example: 
+  titleColor: foundationColors.secondary.mango.300
+*/
+
 import React from 'react';
 import Box from '@mui/material/Box';
 import { Imgc } from 'common/ui/Imgc';
 import { Typography } from '@mui/material';
+import PropTypes from 'prop-types';
 import './InfoCard.scss';
 
-const InfoCard = ({ ImgSrc, imgProps = {}, title, subTitle }) => {
+const InfoCard = ({ imgSrc, imgProps = {}, title, titleColor, subtitle, subtitleColor }) => {
   return (
     <Box sx={infoCardWrapperSxStyle}>
       <Imgc
-        src={ImgSrc}
+        src={imgSrc}
         style={{ height: '32px', width: '32px' }}
         {...imgProps}
       />
       <div className='ic-text-wrapper'>
-        <Typography variant='heading4'>{title}</Typography>
+        <Typography variant='heading4' color={titleColor}>{title}</Typography>
         <Typography
           className='ic-subtitle-text'
           variant='body2'
-          color='foundationColors.content.secondary'
+          color={subtitleColor ? subtitleColor :'foundationColors.content.secondary'}
         >
-          {subTitle}
+          {subtitle}
         </Typography>
       </div>
     </Box>
@@ -29,6 +41,7 @@ const InfoCard = ({ ImgSrc, imgProps = {}, title, subTitle }) => {
 export default InfoCard;
 
 const infoCardWrapperSxStyle = {
+  backgroundColor: 'foundationColors.supporting.white',
   border: '1px solid',
   borderColor: 'foundationColors.supporting.athensGrey',
   padding: '16px',
@@ -36,3 +49,8 @@ const infoCardWrapperSxStyle = {
   display: 'flex',
   flexDirection: 'row',
 };
+
+InfoCard.defaultProps = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string
+}
