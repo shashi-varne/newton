9909021cf034d7dbd82cf4842b19d7f15bc520a7
fdamitsingh <amit.singh@fisdom.com>
2022-01-03 02:40:55 +0530

    MODIFY: 1. added default and proptypes to the component. 2. added props description.

--- react_webviews/src/designSystem/molecules/BlogContent/BlogContent.js
+++ react_webviews/src/designSystem/molecules/BlogContent/BlogContent.js
@@ -1,37 +1,55 @@
+/*
+  props description:
+  title(string),
+  subtitle(string),
+  titleColor, subtitleColor: strongly recommended to use foundation colors.
+  Example:
+  titleColor: 'foundationColors.secondary.mango.300'
+*/
+
 import React from 'react';
 import { Imgc } from '../../../common/ui/Imgc';
 import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
 import './BlogContent.scss';
 
 const BlogContent = ({
-  ImgSrc,
+  imgSrc,
   imgProps = {},
   title,
-  titleProps = {},
+  titleColor,
   subtitle,
-  subtitleProps = {},
+  subtitleColor,
 }) => {
   return (
     <div className='bc-wrapper'>
       <Imgc
-        src={ImgSrc}
+        src={imgSrc}
         style={{ height: '62px', width: '92px', padding: '4px' }}
         {...imgProps}
       />
       <div className='bc-text-wrapper'>
-        <Typography variant='body1' {...titleProps}>
+        <Typography variant='body1' color={titleColor}>
           {title}
         </Typography>
         <Typography
           variant='body5'
-          color='foundationColors.content.secondary'
-          {...subtitleProps}
+          color={subtitleColor}
         >
           {subtitle}
         </Typography>
       </div>
     </div>
   );
+};
+
+BlogContent.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary'
+};
+
+BlogContent.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
 };
 
 export default BlogContent;
