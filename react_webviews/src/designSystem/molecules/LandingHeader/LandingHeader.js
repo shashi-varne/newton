c396e1e9e47fae56b49a235e750b5be1c7bcc1a0
fdamitsingh <amit.singh@fisdom.com>
2022-01-03 05:09:22 +0530

    MODIFY: 1. added color support for the typography. 2. added default props and prop validation. 3. prop description.

--- react_webviews/src/designSystem/molecules/LandingHeader/LandingHeader.js
+++ react_webviews/src/designSystem/molecules/LandingHeader/LandingHeader.js
@@ -1,10 +1,30 @@
-import { Box, Typography } from '@mui/material';
-import format from 'date-fns/format';
+/*
+  Prop description:
+  LandingHeader:
+    variant: one of type => 'center', 'slide', 'date'
+
+  LandingHeader.Title:
+    titleColor: strongly recommended to only use foundation colors.
+    
+  LandingHeader.Subtitle:
+    variant: will inherit the variant from the parent.
+    date: if variant is date then bydefault the current date will be picked, but this value can be overriden.
+    subtitleColor, dateColor: strongly recommended to only use foundation colors.
+
+  Example to pass color:
+    titleColor: 'foundationColors.secondary.mango.300'
+*/
+
+
+
 import React, { Children } from 'react';
+import { Box, Typography } from '@mui/material';
 import { Imgc } from '../../../common/ui/Imgc';
+import format from 'date-fns/format';
+import PropTypes from 'prop-types';
 import './LandingHeader.scss';
 
-const LandingHeader = ({ variant, children, date, imageSrc, imageProps = {} }) => {
+const LandingHeader = ({ variant, children, imageSrc, imageProps = {} }) => {
   const variantClass = variant === 'center' ? 'center-align' : '';
   return (
     <Box className={`landing-header-wrapper ${variantClass}`}>
@@ -16,7 +36,6 @@ const LandingHeader = ({ variant, children, date, imageSrc, imageProps = {} }) =
       {Children.map(children, (el) => {
           return React.cloneElement(el, {
             variant,
-            date
           });
         }
       )}
@@ -24,11 +43,16 @@ const LandingHeader = ({ variant, children, date, imageSrc, imageProps = {} }) =
   );
 };
 
-LandingHeader.Title = ({ children }) => {
-  return <Typography variant='heading1'>{children}</Typography>;
+LandingHeader.propTypes = {
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(['side', 'center', 'date'])
 };
 
-LandingHeader.Subtitle = ({ children, date, variant }) => {
+LandingHeader.Title = ({ children, titleColor }) => {
+  return <Typography variant='heading1' color={titleColor}>{children}</Typography>;
+};
+
+LandingHeader.Subtitle = ({ children, date, variant, subtitleColor, dateColor }) => {
   let formattedData;
   if(variant === 'date') {
     date = date ? date : new Date();
@@ -36,8 +60,8 @@ LandingHeader.Subtitle = ({ children, date, variant }) => {
   }
   return (
     <div>
-      {date && (
-        <Typography className='lh-date-wrapper' variant='body2' color='foundationColors.content.secondary'>
+      {variant === 'date' && (
+        <Typography className='lh-date-wrapper' variant='body2' color={dateColor}>
           {formattedData}
         </Typography>
       )}
@@ -45,12 +69,25 @@ LandingHeader.Subtitle = ({ children, date, variant }) => {
       <Typography
         className='lh-subtitle'
         variant='body2'
-        color='foundationColors.content.secondary'
+        color={subtitleColor}
       >
         {children}
       </Typography>
     </div>
   );
+};
+
+LandingHeader.Title.propTypes = {
+  children: PropTypes.node,
+};
+
+LandingHeader.Subtitle.propTypes = {
+  children: PropTypes.node,
+};
+
+LandingHeader.Subtitle.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary',
+  dateColor: 'foundationColors.content.secondary'
 };
 
 export default LandingHeader;
