2b63a5f9d1dfdd2add9ede3f48de513c0074cfdd
fdamitsingh <amit.singh@fisdom.com>
2022-01-03 02:24:48 +0530

    MODIFY: 1. added label color support. 2. add prop description.

--- react_webviews/src/designSystem/molecules/Collapse/Collapse.js
+++ react_webviews/src/designSystem/molecules/Collapse/Collapse.js
@@ -1,7 +1,22 @@
+/*
+  props description:
+  label(string),
+  isOpen(bool),
+  onClick(function): controls the dropdown,
+  expandedIcon: Icon which will be shown when the component is expanded
+  collapsedIcon: Icon which will be shown when the component is collapsed
+  childWrapperClass: add styling to the child component.
+  labelColor: strongly recommended to use foundation colors.
+  Example:
+  labelColor: 'foundationColors.secondary.mango.300'
+*/
+
+
 import React from 'react';
 import { Typography, Collapse as CollapseLib } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PropTypes from 'prop-types';
 import './Collapse.scss';
 
 const Collapse = ({
@@ -11,13 +26,13 @@ const Collapse = ({
   children,
   expandedIcon,
   collapsedIcon,
-  labelProps={},
-  childWrapperClass=''
+  labelColor,
+  childWrapperClass
 }) => {
   return (
     <div className='c-wrapper'>
       <div className='c-label-wrapper' onClick={onClick}>
-        <Typography variant='heading3' {...labelProps}>{label}</Typography>
+        <Typography variant='heading3' color={labelColor}>{label}</Typography>
         <div className='c-icon-wrapper'>{isOpen ? expandedIcon : collapsedIcon}</div>
       </div>
       <CollapseLib in={isOpen}>
@@ -26,6 +41,13 @@ const Collapse = ({
     </div>
   );
 };
+
+Collapse.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
 
 Collapse.defaultProps = {
   expandedIcon: <KeyboardArrowUpIcon />,
