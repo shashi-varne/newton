22f87349424c11ba498498bfe8c7cf33d36f8f14
AlekhyaBathula <alekhya@fisdom.com>
2022-01-03 13:34:01 +0530

    Added common styles

--- react_webviews/src/freedom_plan/index.js
+++ react_webviews/src/freedom_plan/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import NotFound from "../common/components/NotFound";
+import "./commonStyles.scss";
 
 function FreedomPlan(props) {
   const { url } = props.match;
