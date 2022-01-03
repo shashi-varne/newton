05d81053322019a2c8aa86ffba0b47fe5004bd65
fdamitsingh <amit.singh@fisdom.com>
2022-01-03 20:19:47 +0530

    ADD: Card Horizontal molecules

---
+++ react_webviews/src/designSystem/molecules/CardHorizontal/CardHorizontal.js
@@ -0,0 +1,85 @@
+import { Box, Button, Typography } from '@mui/material';
+import React, { useMemo } from 'react';
+import { Imgc } from '../../../common/ui/Imgc';
+
+import './CardHorizontal.scss';
+
+const CardHorizontal = ({
+  iconSrc,
+  iconProps = {},
+  ilstSrc,
+  ilstProps = {},
+  title,
+  titleColor,
+  subtitle,
+  subtitleColor,
+  description,
+  descriptionColor,
+  actionLink,
+  status,
+  variant = 'product',
+}) => {
+  const heroCardVariant = variant === 'heroCard';
+  const titleVariant = heroCardVariant ? 'heading3' : 'body1';
+  const heroCardStyle = useMemo(() => {
+    if(heroCardVariant) {
+        titleColor = 'foundationColors.supporting.white';
+        subtitleColor = 'foundationColors.primary.200';
+        return {
+            titleColor,
+            subtitleColor
+        }
+    }
+  },[]);
+  return (
+    <Box className={`ch-wrapper ${heroCardVariant && 'ch-hero-wrapper'}`}>
+      <div>
+        {!heroCardVariant && (
+          <Imgc
+            src={iconSrc}
+            style={{ width: '32px', height: '32px' }}
+            {...iconProps}
+          />
+        )}
+        {title && (
+          <Typography
+            className='mt-4'
+            variant={titleVariant}
+            color={heroCardStyle?.titleColor || titleColor}
+          >
+            {title}
+          </Typography>
+        )}
+        {status && <div className='mt-4'>{status}</div>}
+        {subtitle && (
+          <Typography className='mt-4' variant='body2' color={heroCardStyle?.subtitleColor || subtitleColor}>
+            {subtitle}
+          </Typography>
+        )}
+        {description && (
+          <Typography className='mt-4' variant='body2' color={descriptionColor}>
+            {description}
+          </Typography>
+        )}
+        <Button
+          className={`mt-4 btn-text ${heroCardVariant && 'btn-primary'}`}
+          variant='text'
+        >
+          {actionLink}
+        </Button>
+      </div>
+      <Imgc
+        src={ilstSrc}
+        style={{ width: '110px', height: '110px', marginLeft: '4px' }}
+        {...ilstProps}
+      />
+    </Box>
+  );
+};
+
+CardHorizontal.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary',
+  descriptionColor: 'foundationColors.content.tertiary',
+};
+
+export default CardHorizontal;
