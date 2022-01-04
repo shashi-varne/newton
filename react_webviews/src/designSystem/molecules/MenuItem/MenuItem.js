a7b4312345a59173abcd8fc7d56b3d14a482dfe8
fdamitsingh <amit.singh@fisdom.com>
2022-01-05 04:22:20 +0530

    MODIFY: Added proptype validation and props description

--- react_webviews/src/designSystem/molecules/MenuItem/MenuItem.js
+++ react_webviews/src/designSystem/molecules/MenuItem/MenuItem.js
@@ -1,6 +1,16 @@
+/*
+    Props Description:
+    title(string),
+    subtitle(string),
+    titleColor, subtitleColor : strongly recommended to use foundation Colors.
+    Example: 
+    titleColor: 'foundationColors.secondary.mango.300'
+*/
+
 import { Typography } from '@mui/material';
 import React from 'react';
 import { Imgc } from '../../../common/ui/Imgc';
+import PropTypes from 'prop-types';
 
 import './MenuItem.scss';
 
@@ -13,9 +23,10 @@ const MenuItem = ({
   titleColor,
   subtitle,
   subtitleColor,
+  onItemClick,
 }) => {
   return (
-    <div className='mi-wrapper'>
+    <div className='mi-wrapper' onClick={onItemClick}>
       <div className='mi-left-wrapper'>
         <Imgc
           src={leftImgSrc}
@@ -33,7 +44,7 @@ const MenuItem = ({
       </div>
       <Imgc
         src={rightImgSrc}
-        style={{ width: '24px', height: '24px'}}
+        style={{ width: '24px', height: '24px' }}
         {...rightImgProps}
       />
     </div>
@@ -42,6 +53,14 @@ const MenuItem = ({
 
 MenuItem.defaultProps = {
   subtitleColor: 'foundationColors.content.secondary',
+};
+
+MenuItem.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  leftImgProps: PropTypes.object,
+  rightImgProps: PropTypes.object,
+  onItemClick: PropTypes.func,
 };
 
 export default MenuItem;
