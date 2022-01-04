4369f389e9a6fef02288ed1bdf23603049361c7d
AlekhyaBathula <alekhya@fisdom.com>
2022-01-04 21:38:34 +0530

    Added terms & conditions

---
+++ react_webviews/src/freedom_plan/mini-components/TermsAndConditions.js
@@ -0,0 +1,39 @@
+import React, { useMemo } from "react";
+import { getConfig } from "../../utils/functions";
+import Button from "../../common/ui/Button";
+import WVFullscreenDialog from "../../common/ui/FullscreenDialog/WVFullscreenDialog";
+import { getFreedomPlanTermsAndConditions } from "../common/constants";
+import noop from "lodash/noop";
+import "./mini-components.scss";
+
+const TermsAndConditions = ({ isOpen = false, close = noop }) => {
+  const freedomPlanTermsAndConditions = useMemo(
+    getFreedomPlanTermsAndConditions(getConfig().websiteLink),
+    []
+  );
+  return (
+    <WVFullscreenDialog
+      onClose={close}
+      open={isOpen}
+      closeIconPosition="left"
+      title="Freedom plan : Terms and conditions"
+      classes={{
+        wvTitle: "fp-tnc-title",
+      }}
+    >
+      <WVFullscreenDialog.Content>
+        {freedomPlanTermsAndConditions.map((text, index) => (
+          <div className="freedom-plan-terms-n-conditions flex">
+            <div className="fp-tnc-left">{index + 1}.</div>
+            <div>{text}</div>
+          </div>
+        ))}
+      </WVFullscreenDialog.Content>
+      <WVFullscreenDialog.Action>
+        <Button buttonTitle="OKAY" onClick={close} />
+      </WVFullscreenDialog.Action>
+    </WVFullscreenDialog>
+  );
+};
+
+export default TermsAndConditions;
