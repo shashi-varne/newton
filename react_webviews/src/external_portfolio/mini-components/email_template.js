025b2465c6ff7f5d75a5de1a50031b590b4b212b
Karan Uday <karan.uday@fisdom.com>
2022-01-06 01:20:36 +0530

    Email template changes to support different statement sources

--- react_webviews/src/external_portfolio/mini-components/email_template.js
+++ react_webviews/src/external_portfolio/mini-components/email_template.js
@@ -5,7 +5,9 @@ import InfoBox from './InfoBox';
 class EmailTemplate extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      copyText: `Consolidated Account Statement - ${this.props.statementSource === 'cams' ? 'CAMS' : 'KFINTECH' } Mailback Request`
+    };
   }
 
   render() {
@@ -18,25 +20,25 @@ class EmailTemplate extends Component {
       >
         <div>
           <div className="ext-pf-subheader">
-            <h4>Search your email under</h4>
-            <InfoBox
-              classes={{ root: 'info-box-cut-out'}}
-              isCopiable={true}
-              textToCopy="Consolidated Account Statement - CAMS Mailback Request"
-            >
-              <span className="info-box-body-text">
-                Consolidated Account Statement - CAMS Mailback Request
-              </span>
-            </InfoBox>
-          </div>
-          <div className="ext-pf-subheader">
-            <h4>Email looks like this</h4>
+            <h4>The email looks like this</h4>
             <img
-              src={require('assets/cas_email.jpg')}
+              src={require(`assets/${this.props.statementSource === 'cams' ? 'cas' : 'karvy'}_email.png`)}
               alt="Email"
               className="email_img"
               width="100" height="200"
             />
+          </div>
+          <div className="ext-pf-subheader">
+            <h4>Search your email under</h4>
+            <InfoBox
+              classes={{ root: 'info-box-cut-out' }}
+              isCopiable={true}
+              textToCopy={this.state.copyText}
+            >
+              <span className="info-box-body-text">
+                {this.state.copyText}
+              </span>
+            </InfoBox>
           </div>
           {this.props.children}
         </div>
