52515d2ca25249d273af2a4c1d55fbfca9d792cf
fdamitsingh <amit.singh@fisdom.com>
2022-01-05 11:50:43 +0530

    ADD: folder added for navigation pill

---
+++ react_webviews/src/designSystem/atoms/NavigationPill/Navigation.js
@@ -0,0 +1,12 @@
+import { Tab } from '@mui/material'
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <Tab label='Jump to'/>
+        </div>
+    )
+}
+
+export default Navigation
