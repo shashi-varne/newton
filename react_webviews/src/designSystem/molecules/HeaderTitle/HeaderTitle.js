758f97bd16ac4d927842475a60ab9d7826736f2e
fdamitsingh <amit.singh@fisdom.com>
2022-01-05 22:42:26 +0530

    MODIFY: passing title and subtitle as children to Header title.

--- react_webviews/src/designSystem/molecules/HeaderTitle/HeaderTitle.js
+++ react_webviews/src/designSystem/molecules/HeaderTitle/HeaderTitle.js
@@ -1,26 +1,60 @@
+/*
+  Props description:
+  titleColor, subtitleColor => strongly recommended to use only foundation colors.
+  Example: 
+  titleColor: 'foundationColors.secondary.mango.300'
+*/
+
 import { Typography } from '@mui/material';
 import React from 'react';
 import { Imgc } from '../../../common/ui/Imgc';
+import PropTypes from 'prop-types';
+
 import './HeaderTitle.scss';
 
-const HeaderTitle = ({ ImgSrc, title, subTitle, tagVariant }) => {
+const HeaderTitle = ({ ImgSrc, children, imgProps={}, tagVariant }) => {
   return (
     <div className='ht-wrapper'>
-      <Imgc src={ImgSrc} style={{ width: '40px', height: '40px' }} />
-      <div className='ht-text-wrapper'>
-        <Typography variant='heading2'>{title}</Typography>
-        {subTitle && (
-          <Typography
-            className='ht-subtitle'
-            variant='body2'
-            color='foundationColors.content.secondary'
-          >
-            {subTitle}
-          </Typography>
-        )}
-      </div>
+      <Imgc src={ImgSrc} style={{ width: '40px', height: '40px'}} {...imgProps} />
+      <div className='ht-text-wrapper'>{children}</div>
     </div>
   );
+};
+
+HeaderTitle.Title = ({ children, titleColor }) => {
+  return (
+    <Typography variant='heading2' color={titleColor}>
+      {children}
+    </Typography>
+  );
+};
+
+HeaderTitle.Subtitle = ({ children, subtitleColor }) => {
+  return (
+    <Typography
+      className='ht-subtitle'
+      variant='body2'
+      color={subtitleColor}
+    >
+      {children}
+    </Typography>
+  );
+};
+
+HeaderTitle.propTypes = {
+  children: PropTypes.node,
+}
+
+HeaderTitle.Title.propTypes = {
+  children: PropTypes.node,
+}
+
+HeaderTitle.Subtitle.propTypes = {
+  children: PropTypes.node,
+}
+
+HeaderTitle.Subtitle.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary'
 };
 
 export default HeaderTitle;
