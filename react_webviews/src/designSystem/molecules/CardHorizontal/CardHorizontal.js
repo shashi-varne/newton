0585d91705daa9c195731971e15b193d947464f0
fdamitsingh <amit.singh@fisdom.com>
2022-01-07 00:55:54 +0530

    MODIFY: added data-aid support to card Horizontal

--- react_webviews/src/designSystem/molecules/CardHorizontal/CardHorizontal.js
+++ react_webviews/src/designSystem/molecules/CardHorizontal/CardHorizontal.js
@@ -18,6 +18,7 @@ const CardHorizontal = ({
   actionLink,
   status,
   onCardClick,
+  dataAid,
   variant = 'product',
 }) => {
   const heroCardVariant = variant === 'heroCard';
@@ -33,13 +34,14 @@ const CardHorizontal = ({
     }
   },[]);
   return (
-    <Box className={`ch-wrapper ${heroCardVariant && 'ch-hero-wrapper'}`} onClick={onCardClick}>
+    <Box className={`ch-wrapper ${heroCardVariant && 'ch-hero-wrapper'}`} onClick={onCardClick} data-aid={`cardHorizontal_${dataAid}`}>
       <div>
         {!heroCardVariant && (
           <Imgc
             src={iconSrc}
             style={{ width: '32px', height: '32px' }}
             {...iconProps}
+            data-aid='top'
           />
         )}
         {title && (
@@ -47,24 +49,26 @@ const CardHorizontal = ({
             className='mt-4'
             variant={titleVariant}
             color={heroCardStyle?.titleColor || titleColor}
+            data-aid='tv_title'
           >
             {title}
           </Typography>
         )}
         {status && <div className='mt-4'>{status}</div>}
         {subtitle && (
-          <Typography className='mt-4' variant='body2' color={heroCardStyle?.subtitleColor || subtitleColor}>
+          <Typography className='mt-4' variant='body2' color={heroCardStyle?.subtitleColor || subtitleColor} data-aid='tv_subtitle'>
             {subtitle}
           </Typography>
         )}
         {description && (
-          <Typography className='mt-4' variant='body2' color={descriptionColor}>
+          <Typography className='mt-4' variant='body2' color={descriptionColor} data-aid='tv_description'>
             {description}
           </Typography>
         )}
         <Button
           className={`mt-4 btn-text ${heroCardVariant && 'btn-primary'}`}
           variant='text'
+          data-aid='button_link'
         >
           {actionLink}
         </Button>
@@ -73,6 +77,7 @@ const CardHorizontal = ({
         src={ilstSrc}
         style={{ width: '110px', height: '110px', marginLeft: '4px' }}
         {...ilstProps}
+        data-aid='right'
       />
     </Box>
   );
