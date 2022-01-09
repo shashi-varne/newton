6df58f1eb88daac759e76d83a441f38ccfca6395
fdamitsingh <amit.singh@fisdom.com>
2022-01-10 05:06:20 +0530

    MODIFY: Highlight Info. 1. added data-aid. 2. added propTypes.

--- react_webviews/src/designSystem/molecules/HighlightInfo/HighlightInfo.js
+++ react_webviews/src/designSystem/molecules/HighlightInfo/HighlightInfo.js
@@ -1,30 +1,69 @@
+/*
+  prop descripton:
+  highlightText(string/node) => use node, only if there is any style(color, font weight) change in the text.
+  highlightTextColor => strongly recommended to use only foundation colors for this.
+  background(string) => can also pass the linear gradient colors,
+  onIconClick => control event trigger on right icon click,
+  onClick => trigger event on highlight wrapper.
+*/
+
 import React from 'react';
 import { Typography, Box } from '@mui/material';
 import { Imgc } from '../../../common/ui/Imgc';
+import isFunction from 'lodash/isFunction';
+import PropTypes from 'prop-types';
 import './HighlightInfo.scss';
 
 const HighlightInfo = ({
   highlightText,
-  highlightTextProps = {},
+  highlightTextColor,
   imgcSrc,
-  imgProps = {},
-  hiSxWrapperStyle = {},
-  onIconClick
+  imgProps,
+  background,
+  onIconClick,
+  onClick,
+  dataAid,
 }) => {
+  const onRightIconClick = (e) => {
+    if (isFunction(onIconClick)) {
+      e.stopPropagation();
+      onIconClick(e);
+    }
+  };
   return (
-    <Box sx={hiSxWrapperStyle} className='hi-wrapper'>
-      <Typography variant='body5' {...highlightTextProps}>
+    <Box sx={{ background }} className='hi-wrapper' onClick={onClick} data-aid={`highlightInfo_${dataAid}`}>
+      <Typography variant='body5' color={highlightTextColor} comoponent='div' data-aid='tv_title'>
         {highlightText}
       </Typography>
-      <div className='hi-img-wrapper' onClick={onIconClick}>
-        <Imgc
-          src={imgcSrc}
-          style={{ width: '24px', height: '24px' }}
-          {...imgProps}
-        />
-      </div>
+      {imgcSrc && (
+        <div className='hi-img-wrapper' onClick={onRightIconClick}>
+          <Imgc
+            src={imgcSrc}
+            style={{ width: '24px', height: '24px' }}
+            {...imgProps}
+            dataAid='right'
+          />
+        </div>
+      )}
     </Box>
   );
+};
+
+HighlightInfo.defaultProps = {
+  background:
+    'linear-gradient(180deg, rgba(51, 207, 144, 0.2) 27.54%, rgba(130, 121, 248, 0.13) 100%)',
+  highlightTextColor: 'foundationColors.supporting.white',
+  imgProps: {},
+};
+
+HighlightInfo.propTypes = {
+  highlightText: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  highlightTextColor: PropTypes.string,
+  background: PropTypes.string,
+  onIconClick: PropTypes.func,
+  onClick: PropTypes.func,
+  imgProps: PropTypes.object,
+  dataAid: PropTypes.string,
 };
 
 export default HighlightInfo;
