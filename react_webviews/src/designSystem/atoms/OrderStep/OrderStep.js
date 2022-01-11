0dd07a8a2527667393700893c17dfc504961bf1c
fdamitsingh <amit.singh@fisdom.com>
2022-01-12 01:13:15 +0530

    UPDATE: replaced mui Typo with atom typo.

--- react_webviews/src/designSystem/atoms/OrderStep/OrderStep.js
+++ react_webviews/src/designSystem/atoms/OrderStep/OrderStep.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import Typography from '../Typography';
 import { isEmpty } from "lodash";
 import "./OrderStep.scss";
 
