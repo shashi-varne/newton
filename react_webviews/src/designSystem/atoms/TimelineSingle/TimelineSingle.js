327a526edf3274dc96cc75eb975b13ee1bbb2847
fdamitsingh <amit.singh@fisdom.com>
2022-01-12 01:11:43 +0530

    UPDATE: replaced mui typo with atom typo.

--- react_webviews/src/designSystem/atoms/TimelineSingle/TimelineSingle.js
+++ react_webviews/src/designSystem/atoms/TimelineSingle/TimelineSingle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Typography } from "@mui/material";
+import Typography from "../Typography";
 import "./TimelineSingle.scss";
 
 const activeStyles = {
