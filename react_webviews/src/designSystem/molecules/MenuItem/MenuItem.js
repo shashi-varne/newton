f9218729860266f30b679e0fec680baffa2f7207
fdamitsingh <amit.singh@fisdom.com>
2022-01-12 21:34:14 +0530

    UPDATE: added separator support for menu item.

--- react_webviews/src/designSystem/molecules/MenuItem/MenuItem.js
+++ react_webviews/src/designSystem/molecules/MenuItem/MenuItem.js
@@ -11,7 +11,7 @@ import React from 'react';
 import Typography from '../../atoms/Typography';
 import { Imgc } from '../../../common/ui/Imgc';
 import PropTypes from 'prop-types';
-
+import Separator from '../../atoms/Separator';
 import './MenuItem.scss';
 
 const MenuItem = ({
@@ -24,39 +24,41 @@ const MenuItem = ({
   subtitle,
   subtitleColor,
   onItemClick,
-  dataAid
+  dataAid,
+  separator,
 }) => {
   return (
-    <div className='mi-wrapper' onClick={onItemClick} data-aid={`menuItem_${dataAid}`}>
-      <div className='mi-left-wrapper'>
-        {
-          leftImgSrc &&
+    <div>
+      <div className='mi-wrapper' onClick={onItemClick} data-aid={`menuItem_${dataAid}`}>
+        {leftImgSrc && (
+          <Imgc
+            src={leftImgSrc}
+            style={{ width: '54px', height: '54px', marginRight: '24px' }}
+            {...leftImgProps}
+            dataAid='left'
+          />
+        )}
+
+        <div className='mi-right-wrapper'>
+          <div className='mi-text-wrapper'>
+            <Typography variant='heading4' color={titleColor} component='div' data-aid='tv_title'>
+              {title}
+            </Typography>
+            <Typography variant='body2' color={subtitleColor} component='div' data-aid='tv_subtitle'>
+              {subtitle}
+            </Typography>
+          </div>
+          {rightImgSrc && (
             <Imgc
-              src={leftImgSrc}
-              style={{ width: '54px', height: '54px' }}
-              className='mi-left-img'
-              {...leftImgProps}
-              dataAid='left'
+              src={rightImgSrc}
+              style={{ width: '24px', height: '24px' }}
+              {...rightImgProps}
+              dataAid='right'
             />
-        }
-        <div className='mi-text-wrapper'>
-          <Typography variant='heading4' color={titleColor} component='div' data-aid='tv_title'>
-            {title}
-          </Typography>
-          <Typography variant='body2' color={subtitleColor} component='div' data-aid='tv_subtitle'>
-            {subtitle}
-          </Typography>
+          )}
         </div>
       </div>
-      {
-        rightImgSrc &&
-          <Imgc
-            src={rightImgSrc}
-            style={{ width: '24px', height: '24px' }}
-            {...rightImgProps}
-            dataAid='right'
-          />
-      }
+      {separator && <Separator marginLeft={leftImgSrc ? '94px' : '16px'} />}
     </div>
   );
 };
@@ -64,7 +66,7 @@ const MenuItem = ({
 MenuItem.defaultProps = {
   subtitleColor: 'foundationColors.content.secondary',
   leftImgProps: {},
-  rightImgProps: {}
+  rightImgProps: {},
 };
 
 MenuItem.propTypes = {
