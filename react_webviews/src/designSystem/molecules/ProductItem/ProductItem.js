0a8977762116b452852868d2640da041c33837e1
fdamitsingh <amit.singh@fisdom.com>
2022-01-13 02:10:33 +0530

    UPDATE: added icon variant and separator support.

--- react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
+++ react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
@@ -2,42 +2,72 @@ import React from 'react';
 import {Imgc} from '../../../common/ui/Imgc';
 import Typography from '../../atoms/Typography';
 import Tags from '../Tags';
+import Separator from '../../atoms/Separator';
+import isEmpty from 'lodash/isEmpty';
 
 import './ProductItem.scss';
 
 const ProductItem = ({
-  leftImgSrc,
-  leftImgSrcProps = {},
+  leftImgProps = {},
   title,
+  titleColor,
   leftTagVariant = {},
-  rightTagVariant = {},
+  middleTagVariant = {},
   rightTitle,
-  rightSubtitle
+  rightTitleColor,
+  rightSubtitle,
+  rightSubtitleColor,
+  leftSectionTitle,
+  separator,
+  rightImgProps={}
 }) => {
   return (
     <div>
       <div className='product-item-wrapper'>
-        <Imgc
-          src={leftImgSrc}
+        {
+          leftImgProps?.src &&
+          <Imgc
+          src={leftImgProps?.src}
           style={{ width: '40px', height: '40px', marginRight: '8px' }}
-          {...leftImgSrcProps}
-        />
+          {...leftImgProps}
+          />
+        }
         <div className='pi-right-wrapper'>
-          <div className='pi-left-section'>
-            <Typography variant='body2'>{title}</Typography>
-            <div className='pi-ls-bottom-section'>
-              {leftTagVariant && <Tags {...leftTagVariant} />}
-              {rightTagVariant && <Tags {...rightTagVariant} />}
+          <div className='pi-child-wrapper'>
+            <div className='pi-left-section'>
+              <Typography variant='body2' color={titleColor} component='div'>{title}</Typography>
+              <div className='pi-ls-bottom-section'>
+                {!isEmpty(leftTagVariant) && <Tags {...leftTagVariant} className='pi-left-tag'/>}
+                {!isEmpty(middleTagVariant) && <Tags {...middleTagVariant} className={leftSectionTitle ? 'p-right-tag' : ''}/>}
+                {leftSectionTitle && <Typography className='left-section-text' variant='body1' align='right'>{leftSectionTitle}</Typography>}
+              </div>
+            </div>
+            <div className='pi-right-section'>
+              {
+                !rightImgProps?.src ?
+                <div className='pi-right-text-wrapper'>
+                <Typography variant='body1' align='right' color={rightTitleColor}>{rightTitle}</Typography>
+                <Typography variant='body2' align='right' color={rightSubtitleColor}>{rightSubtitle}</Typography>
+                </div>
+               :
+                <Imgc
+                src={rightImgProps?.src}
+                style={{ width: '32px', height: '32px', marginRight: '8px' }}
+                {...rightImgProps?.imgProps}
+                />
+              }
             </div>
           </div>
-          <div className='pi-right-section'>
-              <Typography variant='body1'>{rightTitle}</Typography>
-              <Typography variant='body2' color='foundationColors.content.tertiary'>{rightSubtitle}</Typography>
-          </div>
+          { separator && <Separator marginTop='16px'/> }
         </div>
+
       </div>
     </div>
   );
 };
 
 export default ProductItem;
+
+ProductItem.defaultProps = {
+  rightSubtitleColor: 'foundationColors.content.tertiary'
+}
