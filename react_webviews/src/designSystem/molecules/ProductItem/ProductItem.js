212cd5b12a774b586a27c645991b1774cf40b1b1
fdamitsingh <amit.singh@fisdom.com>
2022-01-13 14:24:13 +0530

    UPDATE: Product Item 1, Updated the proptypes.

--- react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
+++ react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
@@ -1,73 +1,131 @@
 import React from 'react';
-import {Imgc} from '../../../common/ui/Imgc';
+import { Imgc } from '../../../common/ui/Imgc';
 import Typography from '../../atoms/Typography';
 import Tags from '../Tags';
 import Separator from '../../atoms/Separator';
 import isEmpty from 'lodash/isEmpty';
+import merge from 'lodash/merge';
+import PropTypes from 'prop-types';
 
 import './ProductItem.scss';
 
-const ProductItem = ({
-  leftImgProps = {},
-  title,
-  titleColor,
-  leftTagVariant = {},
-  middleTagVariant = {},
-  rightTitle,
-  rightTitleColor,
-  rightSubtitle,
-  rightSubtitleColor,
-  leftSectionTitle,
-  separator,
-  rightImgProps={}
-}) => {
+const ProductItem = (props) => {
+  let {
+    leftImgProps,
+    rightImgProps,
+    header,
+    leftTagVariant,
+    middleTagVariant,
+    bottomTitle,
+    rightTitle,
+    rightSubtitle,
+    separator,
+  } = merge({}, defaultValues, props);
   return (
     <div>
       <div className='product-item-wrapper'>
-        {
-          leftImgProps?.src &&
+        {leftImgProps?.src && (
           <Imgc
-          src={leftImgProps?.src}
-          style={{ width: '40px', height: '40px', marginRight: '8px' }}
-          {...leftImgProps}
+            src={leftImgProps?.src}
+            style={{
+              width: '40px',
+              height: '40px',
+              marginRight: '8px',
+            }}
+            {...leftImgProps}
           />
-        }
+        )}
         <div className='pi-right-wrapper'>
           <div className='pi-child-wrapper'>
             <div className='pi-left-section'>
-              <Typography variant='body2' color={titleColor} component='div'>{title}</Typography>
+              <Typography variant='body2' color={header?.color} component='div'>
+                {header?.name}
+              </Typography>
               <div className='pi-ls-bottom-section'>
-                {!isEmpty(leftTagVariant) && <Tags {...leftTagVariant} className='pi-left-tag'/>}
-                {!isEmpty(middleTagVariant) && <Tags {...middleTagVariant} className={leftSectionTitle ? 'p-right-tag' : ''}/>}
-                {leftSectionTitle && <Typography className='left-section-text' variant='body1' align='right'>{leftSectionTitle}</Typography>}
+                {!isEmpty(leftTagVariant) && <Tags {...leftTagVariant} className='pi-left-tag' />}
+                {!isEmpty(middleTagVariant) && (
+                  <Tags {...middleTagVariant} className={bottomTitle?.name ? 'p-right-tag' : ''} />
+                )}
+                {bottomTitle?.name && (
+                  <Typography className='left-section-text' color={bottomTitle?.color} variant='body2' align='right'>
+                    {bottomTitle?.name}
+                  </Typography>
+                )}
               </div>
             </div>
             <div className='pi-right-section'>
-              {
-                !rightImgProps?.src ?
+              {!rightImgProps?.src ? (
                 <div className='pi-right-text-wrapper'>
-                <Typography variant='body1' align='right' color={rightTitleColor}>{rightTitle}</Typography>
-                <Typography variant='body2' align='right' color={rightSubtitleColor}>{rightSubtitle}</Typography>
+                  <Typography variant='body1' align='right' color={rightTitle?.color}>
+                    {rightTitle?.name}
+                  </Typography>
+                  <Typography variant='body2' align='right' color={rightSubtitle?.color}>
+                    {rightSubtitle?.name}
+                  </Typography>
                 </div>
-               :
+              ) : (
                 <Imgc
-                src={rightImgProps?.src}
-                style={{ width: '32px', height: '32px', marginRight: '8px' }}
-                {...rightImgProps?.imgProps}
+                  src={rightImgProps?.src}
+                  style={{
+                    width: '32px',
+                    height: '32px',
+                    marginLeft: '8px',
+                  }}
+                  {...rightImgProps}
                 />
-              }
+              )}
             </div>
           </div>
-          { separator && <Separator marginTop='16px'/> }
+          {separator && <Separator marginTop='16px' />}
         </div>
-
       </div>
     </div>
   );
 };
 
-export default ProductItem;
+const defaultValues = {
+  rightSubtitle: {
+    color: 'foundationColors.content.tertiary',
+  },
+  bottomTitle: {
+    color: 'foundationColors.content.secondary',
+  }
+};
 
 ProductItem.defaultProps = {
-  rightSubtitleColor: 'foundationColors.content.tertiary'
+  leftImgProps : {},
+  rightImgProps : {},
+  header : {},
+  leftTagVariant : {},
+  middleTagVariant : {},
+  bottomTitle : {},
+  rightTitle : {},
+  rightSubtitle : {},
+  separator: false,
+};
+
+ProductItem.propTypes = {
+  leftImgProps : PropTypes.object,
+  rightImgProps : PropTypes.object,
+  header : PropTypes.exact({
+    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    color: PropTypes.string
+  }),
+  leftTagVariant: PropTypes.object,
+  middleTagVariant: PropTypes.object,
+  bottomTitle: PropTypes.exact({
+    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    color: PropTypes.string
+  }),
+  rightTitle: PropTypes.exact({
+    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    color: PropTypes.string
+  }),
+  rightSubtitle: PropTypes.exact({
+    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    color: PropTypes.string
+  }),
+  separator: PropTypes.bool,
 }
+
+export default ProductItem;
