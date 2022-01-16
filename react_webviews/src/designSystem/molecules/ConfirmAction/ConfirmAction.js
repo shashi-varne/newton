2fefd377df66cc25209008b42db4b26457645a3a
fdamitsingh <amit.singh@fisdom.com>
2022-01-16 23:19:17 +0530

    ADD: added Confirm Action molecule. 2. Added story for confirm action. 3.added data-aid.

---
+++ react_webviews/src/designSystem/molecules/ConfirmAction/ConfirmAction.js
@@ -0,0 +1,68 @@
+/*
+    prop description:
+    imgProps => all the <Imgc /> props can be passed through this.
+    title(string/node) => this is text shown at the left.
+    titleColor => strongly remommended to use foundation colors for this.
+        Example: titleColor: 'foundationColors.secondary.mango.300'
+    buttonTitle => title of the button.
+    onClick => this is a click function for the button.
+*/
+
+import React from 'react';
+import { Box } from '@mui/material';
+import Typography from '../../atoms/Typography';
+import Button from '../../atoms/Button';
+import PropTypes from 'prop-types';
+import { Imgc } from '../../../common/ui/Imgc';
+
+import './ConfirmAction.scss';
+
+const ConfirmAction = ({ imgProps = {}, title, titleColor, buttonTitle, onClick }) => {
+  return (
+    <Box
+      className='confirm-action-wrapper'
+      sx={{
+        backgroundColor: 'foundationColors.action.brand',
+      }}
+    >
+      {imgProps?.src && (
+        <Imgc
+          src={imgProps?.src}
+          className='confirm-action-left-img'
+          dataAid='left'
+          {...imgProps}
+        />
+      )}
+      <div className='confirm-action-right-wrapper'>
+        {title && (
+          <Typography variant='body5' dataAid='title' color={titleColor}>
+            {title}
+          </Typography>
+        )}
+        {buttonTitle && (
+          <Button
+            isInverted
+            title={buttonTitle}
+            size='small'
+            onClick={onClick}
+            dataAid='smallWhite'
+          />
+        )}
+      </div>
+    </Box>
+  );
+};
+
+ConfirmAction.defaultProps = {
+  titleColor: 'foundationColors.supporting.white',
+};
+
+ConfirmAction.propTypes = {
+  imgProps: PropTypes.object,
+  title: PropTypes.node,
+  titleColor: PropTypes.string,
+  buttonTitle: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default ConfirmAction;
