112baa37cd455925e6bcbe284cc8429ce9c01a4d
fdamitsingh <amit.singh@fisdom.com>
2022-01-18 17:23:34 +0530

    UPDATE: using title, subtitle from the props instead of sub component;

--- react_webviews/src/designSystem/molecules/HeaderTitle/HeaderTitle.js
+++ react_webviews/src/designSystem/molecules/HeaderTitle/HeaderTitle.js
@@ -1,193 +1,100 @@
 /*
-  Prop description:
-  LandingHeader:
-    variant: one of type => 'center', 'side'
-    children: this will only accept 'LandingHeaderTitle', 'LandingHeaderSubtitle', 'LandingHeaderPoints', 'LandingHeaderImage'.
-    dataAid: unique id.
-
-  LandingHeaderTitle:
-    color: color which will be used for the child.
-    children: text for the title.
-
-  LandingHeaderSubtitle:
-    children: It will only accept Typography component, which can be a single <Typography/> component,
-              or a list of Typography component.
-              - dataAid for single Typography will be tv_subtitle.
-              - dataAid for list Typography will be tv_subtitle{idx}.
-    color: This color will be passed to all the Typography component, you can also override this color by
-           passing color to individual Typography item.
-
-  LandingHeaderPoints:
-    This component will convert the Typography child components into list with bullet points.
-    children: It will only accept Typography component, which can be a single <Typography/> component,
-              or a list of Typography component.
-              - dataAid for single Typography will be tv_subtitle.
-              - dataAid for list Typography will be tv_subtitle{idx}.
-    color: This color will be passed to all the Typography component, you can also override this color by
-           passing color to individual Typography item.
-
-  Usage of the component:
-
-   <LandingHeader variant='center'>
-
-      <LandingHeaderImage imgProps={{ src: require('assets/amazon_pay.svg') }} />
-
-      <LandingHeaderTitle>Title</LandingHeaderTitle>
-
-      <LandingHeaderSubtitle color='foundationColors.secondary.mango.300'>
-
-        <Typography>
-
-            These funds essentially {format(new Date(), 'MMM d, yyyy ')} invest in stocks of
-
-            various two line text, limit - 99 characters or 17 words
-
-        </Typography>
-
-        <Typography>
-
-          These funds essentially
-
-          <Typography
-
-            color='foundationColors.secondary.profitGreen.300'
-
-            component='span'
-
-            variant='heading4'
-
-          >of various</Typography>
-
-          two line text, limit - 99 characters or 17 words Hello World
-
-        </Typography>
-
-      </LandingHeaderSubtitle>
-
-      <LandingHeaderPoints>
-
-        <Typography>One line text, limit - 46 characters or 9 words</Typography>
-
-        <Typography>One line text, limit - 46 characters or 9 words</Typography>
-
-        <Typography color='foundationColors.secondary.profitGreen.300'>One line text, limit - 46 characters or 9 words</Typography>
-
-      </LandingHeaderPoints>
-
-    </LandingHeader>
-
-
-  NOTE: STRONGLY RECOMMENDED TO ONLY USE FOUNDATION COLORS.
-
-  Example to pass color:
-
-    color: 'foundationColors.secondary.mango.300'
-
+  Props description:
+  title, subtitle => string/node
+  titleColor, subtitleColor => pass foundation colors for these.
+  subTitleLabels: it is an array of Objects for label name and its color.
+  subTitleLabels structure is: 
+  [
+    {
+      name: 'Some Name',
+      color : 'foundationColors.secondary.profitGreen.300'
+    }
+  ]
+  NOTE: STRONGLY RECOMMENDED TO USE ONLY FOUNDATION COLORS
+  Example: 
+  titleColor: 'foundationColors.secondary.mango.300'
 */
 
-import React, { Children } from 'react';
+import React from 'react';
 import Typography from '../../atoms/Typography';
 import { Imgc } from '../../../common/ui/Imgc';
 import PropTypes from 'prop-types';
 
 import './HeaderTitle.scss';
 
-const HeaderTitle = ({ children, imgProps, dataAid }) => {
+const HeaderTitle = ({
+  imgSrc,
+  imgProps = {},
+  title,
+  titleColor,
+  subtitle,
+  subtitleColor,
+  dataAid,
+  subTitleLabels = [],
+}) => {
   return (
     <div className='ht-wrapper' data-aid={`headerTitle_${dataAid}`}>
-      {imgProps?.src && (
-        <Imgc src={imgProps?.src} className='ht-left-image' {...imgProps} dataAid='left' />
-      )}
-      <div className='ht-child-wrapper'>{children}</div>
+      {imgSrc && <Imgc src={imgSrc} className='ht-left-image' dataAid='left' {...imgProps} />}
+      <div className='ht-child-wrapper'>
+        <Typography variant='heading2' color={titleColor} dataAid='title'>
+          {title}
+        </Typography>
+        <Typography variant='body2' color={subtitleColor} dataAid='title'>
+          {subtitle}
+        </Typography>
+        {Array.isArray(subTitleLabels) && <SubtitleLabels subTitleLabels={subTitleLabels} />}
+      </div>
     </div>
   );
 };
 
-HeaderTitle.Title = ({ children, color }) => {
-  return (
-    <Typography variant='heading2' color={color} dataAid='title'>
-      {children}
-    </Typography>
-  );
-};
-
-HeaderTitle.Subtitle = ({ children, color }) => {
-  return (
-    <Typography className='ht-subtitle' variant='body2' color={color} dataAid='subtitle'>
-      {children}
-    </Typography>
-  );
-};
-
-HeaderTitle.SubtitleLabels = ({ children, color }) => {
+const SubtitleLabels = ({ subTitleLabels }) => {
+  const labelColor = 'foundationColors.content.secondary';
   return (
     <div className='ht-subtitle-labels'>
-      {Children?.map(children, (child, idx) => {
-        const childrenLength = children?.length;
-        const showSeparator = idx !== 0 && children[idx]?.type?.name === 'Typography';
-        const subtitleLabelId = childrenLength > 1 ? idx + 1 : '';
-        if (child?.type?.name !== 'Typography') {
-          const componentType = child?.type || child?.type?.name;
-          console.error(`Only supported child is Typography, passed type is ${componentType}`);
-          return null;
-        } else {
-          return (
-            <div key={idx} className='ht-subtitle-label'>
-              {showSeparator && (
-                <Typography
-                  variant='body6'
-                  color='foundationColors.supporting.cadetBlue'
-                  className='ht-label-separator'
-                  data-aid={`divider_${idx}`}
-                >
-                  |
-                </Typography>
-              )}
-              {React.cloneElement(child, {
-                variant: child?.props?.variant || 'body9',
-                color: child?.props?.color || color,
-                dataAid: `label${subtitleLabelId}`,
-                allCaps: true,
-              })}
-            </div>
-          );
-        }
+      {subTitleLabels?.map((label, idx) => {
+        if (!label?.name) return null;
+        const showSeparator = subTitleLabels[idx + 1]?.name;
+        return (
+          <div key={idx} className='ht-subtitle-label'>
+            <Typography variant='body9' color={label.color || labelColor} allCaps dataAid={`label${idx+1}`}>
+              {label.name}
+            </Typography>
+            {showSeparator && (
+              <Typography
+                variant='body6'
+                color='foundationColors.supporting.cadetBlue'
+                className='ht-label-separator'
+                data-aid={`divider_${idx+1}`}
+              >
+                |
+              </Typography>
+            )}
+          </div>
+        );
       })}
     </div>
   );
 };
 
 HeaderTitle.propTypes = {
-  children: PropTypes.node,
+  title: PropTypes.node,
+  titleColor: PropTypes.string,
+  subtitle: PropTypes.node,
+  subtitleColor: PropTypes.string,
+  subTitleLabels: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string,
+      color: PropTypes.string,
+    })
+  ),
   imgProps: PropTypes.object,
   dataAid: PropTypes.string,
 };
 
 HeaderTitle.defaultProps = {
   imgProps: {},
-};
-
-HeaderTitle.Title.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-HeaderTitle.Subtitle.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-HeaderTitle.Subtitle.defaultProps = {
-  color: 'foundationColors.content.secondary',
-};
-
-HeaderTitle.SubtitleLabels.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-HeaderTitle.SubtitleLabels.defaultProps = {
-  color: 'foundationColors.content.secondary',
+  subtitleColor: 'foundationColors.content.secondary'
 };
 
 export default HeaderTitle;
