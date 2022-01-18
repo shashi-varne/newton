a006f2d55f7390101d6d675675ca0a6b0be4dc25
fdamitsingh <amit.singh@fisdom.com>
2022-01-18 18:10:51 +0530

    UPDATE: passing title, subtitle and description props.

--- react_webviews/src/designSystem/molecules/CardVertical/CardVertical.js
+++ react_webviews/src/designSystem/molecules/CardVertical/CardVertical.js
@@ -1,153 +1,98 @@
 /*
-
-  Prop description:
-  CardVertical:
-    children: this will only accept 'CardVerticalImage','CardVerticalTitle','CardVerticalSubtitle', 'CardVerticalDescription'.
-    dataAid: unique id.
-  
-  CardVerticalImage
-    imgProps: pass the image props for Imgc component.
-  
-  CardVerticalTitle, CardVerticalSubtitle, CardVerticalDescription:
-    children: text for the title.
-    color: color which will be used for the child.
-
-  Usage of the component:
-   <CardVertical>
-      <CardVerticalImage imgProps={{src: require('assets/amazon_pay.svg')}} />
-      <CardVerticalTitle color='foundationColors.secondary.profitGreen.300'>Title</CardVerticalTitle>
-      <CardVerticalSubtitle>Subtitle</CardVerticalSubtitle>
-      <CardVerticalDescription>Description</CardVerticalDescription>
-    </CardVertical>
-
-  NOTE: STRONGLY RECOMMENDED TO ONLY USE FOUNDATION COLORS.
-  Example to pass color:
-    color: 'foundationColors.secondary.mango.300'
-
+  prop types:
+  title, subtitle, description => string/node
+  titleColor, subtitleColor, descriptionColor: can accept foundation colors.
+  Example: 
+  NOTE: STRINGLY RECOMMENDED TO USE ONLY FOUNDATION COLORS.
+  titleColor: 'foundationColors.secondary.mango.300'
+  onClick(function): control the behaviour of the card when clicked.
 */
 
-import React, { Children } from 'react';
-import { Box } from '@mui/material';
-import Typography from '../../atoms/Typography';
+import React from 'react';
+import Box from '@mui/material/Box';
+
 import { Imgc } from '../../../common/ui/Imgc';
-import isString from 'lodash/isString';
 import PropTypes from 'prop-types';
 
 import './CardVertical.scss';
+import Typography from '../../atoms/Typography';
 
-const CARD_VERTICAL_CHILDS = [
-  'CardVerticalImage',
-  'CardVerticalTitle',
-  'CardVerticalSubtitle',
-  'CardVerticalDescription',
-];
-
-export const CardVertical = ({ onClick, children, dataAid, className, sx }) => {
+const CardVertical = ({
+  imgcSrc,
+  title,
+  titleColor,
+  subtitle,
+  subtitleColor,
+  description,
+  descriptionColor,
+  onClick,
+  imgProps,
+  className,
+  dataAid,
+  sx,
+}) => {
   return (
     <Box
       onClick={onClick}
       className={`cv-wrapper ${className}`}
-      data-aid={`cardVertical_${dataAid}`}
       sx={sx}
+      data-aid={`cardVertical_${dataAid}`}
     >
-      {Children.map(children, (child) => {
-        const componentType = isString(child?.type) ? child?.type : child?.type?.name;
-        if (CARD_VERTICAL_CHILDS.indexOf(componentType) !== -1) {
-          return React.cloneElement(child);
-        } else {
-          console.error(
-            `child passed is ${componentType}, expected childs are 
-            'CardVerticalImage','CardVerticalTitle','CardVerticalSubtitle', 'CardVerticalDescription'`
-          );
-          return null;
-        }
-      })}
+      {imgcSrc && <Imgc src={imgcSrc} className='cv-img-top' dataAid='top' {...imgProps} />}
+      {title && (
+        <Typography
+          className='cv-mt-4'
+          variant='body1'
+          color={titleColor}
+          dataAid='title'
+          component='div'
+        >
+          {title}
+        </Typography>
+      )}
+      {subtitle && (
+        <Typography
+          variant='body2'
+          className='cv-mt-4'
+          color={subtitleColor}
+          dataAid='subtitle'
+          component='div'
+        >
+          {subtitle}
+        </Typography>
+      )}
+      {description && (
+        <Typography
+          variant='body2'
+          className='cv-mt-4'
+          color={descriptionColor}
+          dataAid='description'
+          component='div'
+        >
+          {description}
+        </Typography>
+      )}
     </Box>
   );
 };
 
-export const CardVerticalImage = ({ imgProps = {} }) => {
-  return (
-    <Imgc
-      src={imgProps?.src}
-      style={{ }}
-      dataAid='top'
-      className='cv-img-top'
-      {...imgProps}
-    />
-  );
-};
-
-export const CardVerticalTitle = ({ children, color }) => {
-  return (
-    <Typography
-      variant='body1'
-      color={color}
-      dataAid='title'
-      component='div'
-    >
-      {children}
-    </Typography>
-  );
-};
-
-export const CardVerticalSubtitle = ({ children, color }) => {
-  return (
-    <Typography
-      variant='body2'
-      className='cv-mt-4'
-      color={color}
-      dataAid='subtitle'
-      component='div'
-    >
-      {children}
-    </Typography>
-  );
-};
-
-export const CardVerticalDescription = ({ children, color }) => {
-  return (
-    <Typography
-      variant='body2'
-      className='cv-mt-4'
-      color={color}
-      dataAid='description'
-      component='div'
-    >
-      {children}
-    </Typography>
-  );
-};
-
-CardVerticalImage.propTypes = {
-  imgProps: PropTypes.object,
-};
-
-CardVerticalTitle.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-CardVerticalSubtitle.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-CardVerticalSubtitle.defaultProps = {
-  color: 'foundationColors.content.secondary',
-};
-
-CardVerticalDescription.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-};
-
-CardVerticalSubtitle.defaultProps = {
-  color: 'foundationColors.content.tertiary',
-};
+export default CardVertical;
 
 CardVertical.propTypes = {
-  children: PropTypes.node,
+  title: PropTypes.node,
+  titleColor: PropTypes.string,
+  subtitle: PropTypes.node,
+  subtitleColor: PropTypes.string,
+  description: PropTypes.node,
+  descriptionColor: PropTypes.string,
   onClick: PropTypes.func,
+  imgProps: PropTypes.object,
+  className: PropTypes.string,
   dataAid: PropTypes.string,
+};
+
+CardVertical.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary',
+  descriptionColor: 'foundationColors.content.tertiary',
+  imgProps: {},
 };
