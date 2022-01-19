0626a5b1021d17c77cfaedf1326ef3c7c3cfea4f
fdamitsingh <amit.singh@fisdom.com>
2022-01-19 16:14:36 +0530

    UPDATE: replaced Blog Content with Content, and added data aid.

---
+++ react_webviews/src/designSystem/molecules/Content/Content.js
@@ -0,0 +1,74 @@
+/*
+  props description:
+  title, subtitle => string/node
+  titleColor, subtitleColor: strongly recommended to use foundation colors.
+  Example:
+  titleColor: 'foundationColors.secondary.mango.300'
+*/
+
+import React from 'react';
+import { Imgc } from '../../../common/ui/Imgc';
+import Typography from '../../atoms/Typography';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import './Content.scss';
+
+const BlogContent = ({
+  imgSrc,
+  imgProps,
+  title,
+  titleColor,
+  subtitle,
+  subtitleColor,
+  onClick,
+  className,
+  dataAid,
+  sx,
+}) => {
+  return (
+    <Box
+      className={`blog-content-wrapper ${className}`}
+      onClick={onClick}
+      data-aid={`blogContent_${dataAid}`}
+      sx={sx}
+    >
+      {imgSrc && (
+        <Imgc
+          src={imgSrc}
+          className='content-left-img'
+          dataAid='left'
+          {...imgProps}
+        />
+      )}
+      <div className='bc-text-wrapper'>
+        {title && (
+          <Typography variant='body1' color={titleColor} component='div' dataAid='title'>
+            {title}
+          </Typography>
+        )}
+        {subtitle && (
+          <Typography variant='body5' color={subtitleColor} dataAid='subtitle' component='div'>
+            {subtitle}
+          </Typography>
+        )}
+      </div>
+    </Box>
+  );
+};
+
+BlogContent.defaultProps = {
+  subtitleColor: 'foundationColors.content.secondary',
+  imgProps: {},
+};
+
+BlogContent.propTypes = {
+  title: PropTypes.node,
+  titleColor: PropTypes.string,
+  subtitle: PropTypes.node,
+  subtitleColor: PropTypes.string,
+  onClick: PropTypes.func,
+  dataAid: PropTypes.string,
+  imgProps: PropTypes.object,
+};
+
+export default BlogContent;
