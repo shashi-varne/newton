855889070a5661ef9eeee40dff0fb8bbc84aa288
fdamitsingh <amit.singh@fisdom.com>
2022-01-21 21:58:30 +0530

    UPDATE: seprated the component by using bottomSection, rightSection

--- react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
+++ react_webviews/src/designSystem/molecules/ProductItem/ProductItem.js
@@ -1,132 +1,139 @@
 import React from 'react';
 import { Imgc } from '../../../common/ui/Imgc';
 import Typography from '../../atoms/Typography';
-import Tags from '../Tags';
+import Tag from '../Tag';
 import Separator from '../../atoms/Separator';
 import isEmpty from 'lodash/isEmpty';
 import merge from 'lodash/merge';
+import isFunction from 'lodash/isFunction';
 import PropTypes from 'prop-types';
 
 import './ProductItem.scss';
+import Button from '../../atoms/Button';
 
 const ProductItem = (props) => {
   let {
+    leftImgSrc,
     leftImgProps,
-    rightImgProps,
-    header,
-    leftTagVariant,
-    middleTagVariant,
-    bottomTitle,
-    rightTitle,
-    rightSubtitle,
-    separator,
+    headerTitle,
+    headerTitleColor,
+    showSeparator,
+    onClick,
+    bottomSection = {},
+    rightSection = {},
+    dataAid,
   } = merge({}, defaultValues, props);
   return (
-    <div className='product-item-parent'>
-      <div className='product-item-wrapper'>
-        {leftImgProps?.src && (
-          <Imgc
-            src={leftImgProps?.src}
-            style={{
-              width: '40px',
-              height: '40px',
-              marginRight: '8px',
-            }}
-            dataAid='left'
-            {...leftImgProps}
-          />
-        )}
-        <div className='pi-right-wrapper'>
-          <div className='pi-child-wrapper'>
-            <div className='pi-left-section'>
-              <Typography variant='body2' color={header?.color} component='div'>
-                {header?.name}
-              </Typography>
-              <div className='pi-ls-bottom-section'>
-                {!isEmpty(leftTagVariant) && <Tags {...leftTagVariant} className='pi-left-tag' />}
-                {!isEmpty(middleTagVariant) && (
-                  <Tags {...middleTagVariant} className={bottomTitle?.name ? 'p-right-tag' : ''} />
-                )}
-                {bottomTitle?.name && (
-                  <Typography className='left-section-text' color={bottomTitle?.color} variant='body2' align='right'>
-                    {bottomTitle?.name}
-                  </Typography>
-                )}
-              </div>
-            </div>
-            <div className='pi-right-section'>
-              {!rightImgProps?.src ? (
-                <div className='pi-right-text-wrapper'>
-                  <Typography variant='body1' align='right' color={rightTitle?.color}>
-                    {rightTitle?.name}
-                  </Typography>
-                  <Typography variant='body2' align='right' color={rightSubtitle?.color}>
-                    {rightSubtitle?.name}
-                  </Typography>
-                </div>
-              ) : (
-                <Imgc
-                  src={rightImgProps?.src}
-                  style={{
-                    width: '32px',
-                    height: '32px',
-                    marginLeft: '8px',
-                  }}
-                  {...rightImgProps}
-                />
-              )}
-            </div>
+    <div className='product-item-wrapper' onClick={onClick} data-aid={`productItem_${dataAid}`}>
+      {leftImgSrc && (
+        <Imgc src={leftImgSrc} className='product-item-left-img' dataAid='left' {...leftImgProps} />
+      )}
+      <div className='pi-right-wrapper'>
+        <div className='pi-child-wrapper'>
+          <div className='pi-left-section'>
+            <Typography variant='body2' color={headerTitleColor} component='div'>
+              {headerTitle}
+            </Typography>
+            <BottomSection {...bottomSection} />
           </div>
-          {separator && <Separator marginTop='16px' />}
+          {(!isEmpty(rightSection?.description) || !isEmpty(rightSection?.btnProps)) && (
+            <div className='pi-right-section'>
+              <RightSection {...rightSection} />
+            </div>
+          )}
         </div>
+        {showSeparator && <Separator marginTop='16px' />}
       </div>
     </div>
   );
 };
 
-const defaultValues = {
-  rightSubtitle: {
-    color: 'foundationColors.content.tertiary',
-  },
-  bottomTitle: {
-    color: 'foundationColors.content.secondary',
+const BottomSection = ({ tagOne = {}, tagTwo = {}, titleOne, titleOneColor }) => {
+  return (
+    <div className='pi-ls-bottom-section'>
+      {!isEmpty(tagOne) && <Tag {...tagOne} className='pi-ls-bottom-item' />}
+      {!isEmpty(tagTwo) && <Tag {...tagTwo} className='pi-ls-bottom-item' />}
+      {titleOne && (
+        <Typography
+          className='pi-ls-bottom-item'
+          color={titleOneColor}
+          variant='body2'
+          align='right'
+          component='div'
+        >
+          {titleOne}
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+const RightSection = (props) => {
+  const { btnProps = {}, description = {} } = props;
+  if (!isEmpty(btnProps)) {
+    const handleClick = (e) => {
+      if (isFunction(btnProps?.onClick)) {
+        e.stopPropagation();
+        return btnProps?.onClick(e);
+      }
+    };
+    return (
+      <Button title={btnProps?.title} variant='secondary' {...btnProps} onClick={handleClick} />
+    );
+  } else if (!isEmpty(description)) {
+    return <Description {...description} />;
   }
 };
 
-ProductItem.defaultProps = {
-  leftImgProps : {},
-  rightImgProps : {},
-  header : {},
-  leftTagVariant : {},
-  middleTagVariant : {},
-  bottomTitle : {},
-  rightTitle : {},
-  rightSubtitle : {},
-  separator: false,
+const Description = ({ title, titleColor, subtitle, subtitleColor }) => {
+  if (!title && !subtitle) return null;
+  return (
+    <div className='pi-right-text-wrapper'>
+      <Typography variant='body1' align='right' color={titleColor} component='div'>
+        {title}
+      </Typography>
+      <Typography variant='body2' align='right' color={subtitleColor} component='div'>
+        {subtitle}
+      </Typography>
+    </div>
+  );
+};
+
+const defaultValues = {
+  leftImgProps: {},
+  rightSection: {
+    description: {
+      subtitleColor: 'foundationColors.content.tertiary',
+    },
+    btnProps: {},
+  },
+  bottomSection: {
+    tagOne: {},
+    tagTwo: {},
+    titleOneColor: 'foundationColors.content.tertiary',
+  },
 };
 
 ProductItem.propTypes = {
-  leftImgProps : PropTypes.object,
-  rightImgProps : PropTypes.object,
-  header : PropTypes.exact({
-    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    color: PropTypes.string
+  leftImgProps: PropTypes.object,
+  headerTitle: PropTypes.node,
+  headerTitleColor: PropTypes.string,
+  showSeparator: PropTypes.bool,
+  bottomSection: PropTypes.exact({
+    tagOne: PropTypes.object,
+    tagTwo: PropTypes.object,
+    titleOne: PropTypes.node,
+    titleOneColor: PropTypes.string,
   }),
-  leftTagVariant: PropTypes.object,
-  middleTagVariant: PropTypes.object,
-  bottomTitle: PropTypes.exact({
-    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    color: PropTypes.string
+  rightSection: PropTypes.exact({
+    btnProps: PropTypes.object,
+    description: PropTypes.exact({
+      title: PropTypes.node,
+      titleColor: PropTypes.string,
+      subtitle: PropTypes.node,
+      subtitleColor: PropTypes.string,
+    }),
   }),
-  rightTitle: PropTypes.exact({
-    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    color: PropTypes.string
-  }),
-  rightSubtitle: PropTypes.exact({
-    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    color: PropTypes.string
-  }),
-  separator: PropTypes.bool,
-}
+};
 
 export default ProductItem;
