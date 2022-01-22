d1f095f6e3438822943256bdaa4ea7c01164c766
fdamitsingh <amit.singh@fisdom.com>
2022-01-22 19:01:25 +0530

    UPDATE: hiding left sctollbutton, if the tab is scrollable.

--- react_webviews/src/designSystem/atoms/Tabs/Tabs.stories.js
+++ react_webviews/src/designSystem/atoms/Tabs/Tabs.stories.js
@@ -1,21 +1,24 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs } from './Tabs';
 
 export default {
   component: Tabs,
   title: 'Atoms/Tabs',
   argTypes: {
-    value: {type: 'number', defaultValue: 0},
     numberOfTabOnlyForStories: { type: 'number', defaultValue: 4 },
-    width: { type: 'number', defaultValue: 343 },
+    containerWidth: { type: 'number', defaultValue: 343 },
   },
 };
 
-export const Default = ({ numberOfTabOnlyForStories, width,value, ...args }) => {
+export const Default = ({ numberOfTabOnlyForStories, containerWidth, ...args }) => {
+  const [value, setValue] = useState(0);
+  const handleTabs = (e, val) => {
+    setValue(val);
+  };
   return (
-    <Box sx={{width: `${width}px`}}>
-      <Tabs {...args} value={value}>
+    <Box sx={{ width: `${containerWidth}px` }}>
+      <Tabs {...args} value={value} onChange={handleTabs}>
         {[...Array(numberOfTabOnlyForStories).keys()].map((el, idx) => {
           return <Tab label={`Label ${idx + 1}`} />;
         })}
