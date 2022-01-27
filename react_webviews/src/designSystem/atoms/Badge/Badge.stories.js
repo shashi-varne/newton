cde16098deaaa1a500b57e3af3d1e9cfb45c10d5
AlekhyaBathula <alekhya@fisdom.com>
2022-01-28 01:48:27 +0530

    Added badge with icon story

--- react_webviews/src/designSystem/atoms/Badge/Badge.stories.js
+++ react_webviews/src/designSystem/atoms/Badge/Badge.stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Imgc } from "../../../common/ui/Imgc";
 import Badge, { BADGE_VARIANTS } from "./Badge";
 
 export default {
@@ -20,3 +21,22 @@ export default {
 };
 
 export const Default = (args) => <Badge {...args} />;
+
+export const WithIcon = (args) => (
+  <div
+    style={{
+      textAlign: "center",
+      padding: "10px",
+    }}
+  >
+    <Badge {...args}>
+      <Imgc
+        src={require("assets/cub.png")}
+        style={{
+          width: "20px",
+          height: "20px",
+        }}
+      />
+    </Badge>
+  </div>
+);
