3f03cdbf39d1d85faa710042d5d7f383f1dcd0a9
AlekhyaBathula <alekhya@fisdom.com>
2022-01-28 02:24:49 +0530

    1. Integrated badge in filter 2. Added filter story

--- react_webviews/src/designSystem/molecules/Filter/Filter.js
+++ react_webviews/src/designSystem/molecules/Filter/Filter.js
@@ -4,74 +4,74 @@
     - The structure for the above props is:
       {
         count: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-        countColor: PropTypes.string,
         imgProps: PropTypes.object,
         title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
         titleColor: PropTypes.string,
-        badgeColor: PropTypes.string,
         onClick: PropTypes.func,
     };
   Note: strongly recommended to pass only foundation color for the color related props.
   Example: badgeColor: 'foundationColors.secondary.profitGreen.300'
 */
 
-import React, { useMemo } from 'react';
-import Typography from '../../atoms/Typography';
-import Box from '@mui/material/Box';
-import { Imgc } from '../../../common/ui/Imgc';
-import PropTypes from 'prop-types';
+import React from "react";
+import Typography from "../../atoms/Typography";
+import { Imgc } from "../../../common/ui/Imgc";
+import Badge from "../../atoms/Badge";
+import PropTypes from "prop-types";
+import noop from "lodash/noop";
 
-import './Filter.scss';
+import "./Filter.scss";
 
 const Filter = ({
-  leftImgSrc,
-  leftImgProps = {},
-  rightImgSrc,
-  rightImgProps = {},
+  imgSrc,
+  imgProps = {},
+  badgeProps = {},
   title,
   titleColor,
   dataAid,
-  badgeColor,
   count,
-  countColor,
-  onClick,
+  className,
+  onClick = noop,
 }) => {
   return (
-    <div className='filter-wrapper' onClick={onClick}>
-      {leftImgSrc && (
+    <div
+      className={`filter-wrapper ${className}`}
+      onClick={onClick}
+      data-aid={`filter_${dataAid}`}
+    >
+      {imgSrc && (
         <Imgc
-          src={leftImgSrc}
-          style={{ width: '16px', height: '16px', marginRight: '8px' }}
-          {...leftImgProps}
+          src={imgSrc}
+          style={{ width: "16px", height: "16px", marginRight: "8px" }}
+          dataAid="left"
+          {...imgProps}
         />
       )}
-      <Typography variant='body2' color={titleColor} component='div'>
+      <Typography
+        variant="body2"
+        color={titleColor}
+        component="div"
+        dataAid="title"
+      >
         {title}
       </Typography>
-      {rightImgSrc && (
-        <Imgc
-          src={rightImgSrc}
-          style={{ width: '16px', height: '16px', marginRight: '8px' }}
-          {...rightImgProps}
-        />
-      )}
       {count && (
-        <Box
-          sx={{
-            backgroundColor: badgeColor,
-          }}
-          className='filter-nav-badge'
-        >
-          <Typography variant='body4' color={countColor} className='filter-nav-badge-text'>
-            {count}
-          </Typography>
-        </Box>
+        <Badge
+          badgeContent={count}
+          dataAid="number"
+          className="filter-nav-badge"
+          {...badgeProps}
+        />
       )}
     </div>
   );
 };
 
-Filter.defaultProps = {
-  badgeColor: 'foundationColors.secondary.lossRed.400',
-};
 export default Filter;
+
+Filter.propTypes = {
+  count: PropTypes.number,
+  title: PropTypes.string,
+  onClick: PropTypes.func,
+  dataAid: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
---
+++ react_webviews/src/designSystem/molecules/Filter/Filter.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Filter from "./Filter";
+
+export default {
+  component: Filter,
+  title: "Molecules/Filter",
+  argTypes: {
+    imgSrc: {
+      defaultValue: require(`assets/cub.png`),
+    },
+    count: {
+      defaultValue: 1,
+    },
+    title: {
+      defaultValue: "label",
+    },
+    dataAid: {
+      defaultValue: "sort",
+    },
+    onClick: {
+      action: "filter-clicked",
+    },
+  },
+};
+
+export const Default = (args) => <Filter {...args} />;
