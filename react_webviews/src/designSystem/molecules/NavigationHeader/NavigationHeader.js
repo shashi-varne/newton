01772e48469039dcd070a58b64cca069b6b41731
fdamitsingh <amit.singh@fisdom.com>
2022-01-28 14:08:10 +0530

    UPDATE: 1. added story. 2. moved onScroll to helper function of Navigation Header. 3. added propType check. 4. added close icon.

--- react_webviews/src/designSystem/molecules/NavigationHeader/NavigationHeader.js
+++ react_webviews/src/designSystem/molecules/NavigationHeader/NavigationHeader.js
@@ -3,75 +3,63 @@ import Button from '../../atoms/Button';
 import React, { useEffect, useRef } from 'react';
 import Typography from '../../atoms/Typography';
 import backIcon from 'assets/nav_back.svg';
+import closeIcon from 'assets/nav_close.svg';
+import { useHistory } from 'react-router-dom';
 import { Tab, Tabs } from '../../atoms/Tabs';
+import isEmpty from 'lodash/isEmpty';
+import isFunction from 'lodash/isFunction';
+import Icon from '../../atoms/Icon';
+import { onScroll } from './helperFunctions';
+import PropTypes from 'prop-types';
 
 import './NavigationHeader.scss';
-import isEmpty from 'lodash/isEmpty';
-import Icon from '../../atoms/Icon';
 
 const NavigationHeader = ({
   headerTitle,
   hideInPageTitle,
   hideHeaderTitle,
   leftIconSrc,
+  onLeftIconClick,
   hideLeftIcon,
   showCloseIcon,
-  actionText,
   actionTextProps = {},
   children,
   anchorOrigin,
   rightIconSrc,
   onRightIconClick,
   tabsProps = {},
+  tabChilds = [],
 }) => {
-  const headerRef = useRef();
+  const navHeaderWrapperRef = useRef();
   const subtitleRef = useRef();
-  const headerTitleRef = useRef();
+  const inPageTitleRef = useRef();
+  const history = useHistory();
   useEffect(() => {
     if (anchorOrigin?.current) {
-      anchorOrigin.current.addEventListener('scroll', onScroll);
+      anchorOrigin.current.addEventListener('scroll', handleOnScroll);
     }
   }, []);
-  const onScroll = (e) => {
-    const el = headerRef.current;
-    const anchorScrollToTopPosition = e?.target?.scrollTop;
-    const navHeaderTitle = document.getElementsByClassName('nav-header-title')[0];
-    const headerTitleHeight = headerTitleRef?.current?.getBoundingClientRect()?.height || 0;
-    const subtitleHeight = subtitleRef?.current?.getBoundingClientRect()?.height || 0;
-    const headerTitleSubtitleHeight = headerTitleHeight + subtitleHeight;
-    const opacityValue = 1 - anchorScrollToTopPosition / subtitleHeight;
-    subtitleRef.current.style.opacity = opacityValue > 0 ? opacityValue : 0;
-    let defaultHeight = el.style.paddingTop || 60;
-    if (window.innerWidth < 500) {
-      defaultHeight = 0;
-    }
-    if (headerTitleRef?.current) {
-      headerTitleRef.current.style.transition = 'transform 350ms';
-    }
-    if (anchorScrollToTopPosition >= headerTitleSubtitleHeight) {
-      el.classList.add('nav-header-fixed');
-      el.style.top = `${defaultHeight - headerTitleSubtitleHeight}px`;
-      e['target']['style']['paddingTop'] = `${el?.getBoundingClientRect()?.height}px`;
-    } else {
-      el.classList.remove('nav-header-fixed');
-      el.style.top = '0px';
-      e['target']['style']['paddingTop'] = '0px';
-    }
-    if (!navHeaderTitle || hideInPageTitle) return;
-    navHeaderTitle.style.transition = 'opacity 350ms';
-    if (anchorScrollToTopPosition > headerTitleHeight) {
-      navHeaderTitle.style.opacity = '1';
-    } else {
-      navHeaderTitle.style.opacity = '0';
-    }
+  const handleOnScroll = (anchorOriginEl) => {
+    onScroll(anchorOriginEl, navHeaderWrapperRef, subtitleRef, inPageTitleRef, hideInPageTitle);
   };
-  const leftIcon = leftIconSrc ? leftIconSrc : showCloseIcon ? '' : backIcon;
+
+  const handleLeftIconClick = (e) => {
+    if (isFunction(onLeftIconClick)) {
+      return onLeftIconClick(e);
+    }
+    history.goBack();
+  };
+  const leftIcon = leftIconSrc ? leftIconSrc : showCloseIcon ? closeIcon : backIcon;
   return (
-    <div className='nav-header-wrapper' ref={headerRef}>
+    <div className='nav-header-wrapper' ref={navHeaderWrapperRef}>
       <section className='nav-header-top-section'>
         <div className='nav-header-left'>
           {!hideLeftIcon && (
-            <IconButton classes={{ root: 'nav-left-icn-btn' }} className='nav-hl-icon-wrapper'>
+            <IconButton
+              classes={{ root: 'nav-left-icn-btn' }}
+              className='nav-hl-icon-wrapper'
+              onClick={handleLeftIconClick}
+            >
               <Icon src={leftIcon} size='24px' />
             </IconButton>
           )}
@@ -88,20 +76,22 @@ const NavigationHeader = ({
         </div>
         <div className='nav-header-right'>
           {rightIconSrc && <Icon src={rightIconSrc} size='24px' onClick={onRightIconClick} />}
-          {actionText && <Button variant='link' title={actionText} {...actionTextProps} />}
+          {actionTextProps?.title && (
+            <Button variant='link' title={actionTextProps?.title} {...actionTextProps} />
+          )}
         </div>
       </section>
       {!(hideInPageTitle || hideHeaderTitle) && (
-        <div className='nav-bar-title-wrapper' ref={headerTitleRef}>
+        <div className='nav-bar-title-wrapper' ref={inPageTitleRef}>
           <Typography variant='heading2'>{headerTitle}</Typography>
         </div>
       )}
       <section className='nav-bar-subtitle-wrapper' ref={subtitleRef}>
         {children}
       </section>
-      {!isEmpty(tabsProps) && (
+      {!isEmpty(tabsProps) && !isEmpty(tabChilds) && (
         <section className='nav-bar-tabs-wrapper'>
-          <TabsSection tabs={tabsProps?.tabs} tabChild={tabsProps?.tabChild} />
+          <TabsSection tabs={tabsProps} tabChilds={tabChilds} />
         </section>
       )}
     </div>
@@ -141,16 +131,42 @@ export const NavigationHeaderPoints = ({ children, color, dataIdx }) => {
   );
 };
 
-const TabsSection = ({ tabs, tabChild }) => {
+const TabsSection = ({ tabs, tabChilds }) => {
   const { selectedTab = 0, onTabChange, ...restTabs } = tabs;
   return (
     <Tabs value={selectedTab} onChange={onTabChange} {...restTabs}>
-      {tabChild?.map((el, idx) => {
+      {tabChilds?.map((el, idx) => {
         const value = el?.value || idx;
         return <Tab key={idx} label={el?.label} value={value} {...el} />;
       })}
     </Tabs>
   );
+};
+
+NavigationHeader.propTypes = {
+  headerTitle: PropTypes.node,
+  hideInPageTitle: PropTypes.bool,
+  hideHeaderTitle: PropTypes.bool,
+  onLeftIconClick: PropTypes.func,
+  hideLeftIcon: PropTypes.bool,
+  showCloseIcon: PropTypes.bool,
+  actionTextProps: PropTypes.object,
+  children: PropTypes.node,
+  onRightIconClick: PropTypes.func,
+  tabsProps: PropTypes.object,
+  tabChilds: PropTypes.array,
+};
+
+NavigationHeaderSubtitle.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.string,
+  dataIdx: PropTypes.number.isRequired
+};
+
+NavigationHeaderPoints.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.string,
+  dataIdx: PropTypes.number.isRequired
 };
 
 NavigationHeaderSubtitle.defaultProps = {
