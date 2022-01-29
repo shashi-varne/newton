5fb3afbbed5a6c628d94c853ae7e26554dc4b0e4
fdamitsingh <amit.singh@fisdom.com>
2022-01-30 01:29:38 +0530

    UPDATE: container 1. Added Footer component to take care of button layout, and support of topChild/bottomChild.

--- react_webviews/src/designSystem/organisms/Container/Container.js
+++ react_webviews/src/designSystem/organisms/Container/Container.js
@@ -1,34 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import NavigationHeader from '../../molecules/NavigationHeader';
 import {
   NavigationHeaderPoints,
   NavigationHeaderSubtitle,
 } from '../../molecules/NavigationHeader/NavigationHeader';
-import Button from '../../atoms/Button';
 import isArray from 'lodash/isArray';
+import Footer from './Footer';
 
 import './Container.scss';
 
-const Container = ({ headerProps = {}, children, className }) => {
-  const { headerTitle, subtitle, points = [], ...restHeaderProps } = headerProps;
+const Container = ({ headerProps = {}, children, className, footer = {} }) => {
   const containerRef = useRef();
+  const footerWrapperRef = useRef();
+  const { headerTitle, subtitle, points = [], ...restHeaderProps } = headerProps;
+  useEffect(() => {
+    if (footerWrapperRef?.current && containerRef.current) {
+      console.log("eight",footerWrapperRef?.current?.getBoundingClientRect());
+      containerRef.current.style.paddingBottom = `${footerWrapperRef?.current?.getBoundingClientRect()?.height}px`;
+    }
+  }, [footer?.direction, footerWrapperRef?.current]);
   return (
     <Box ref={containerRef} className={`container-wrapper ${className}`}>
       <NavigationHeader headerTitle={headerTitle} anchorOrigin={containerRef} {...restHeaderProps}>
         {subtitle && <NavigationHeaderSubtitle dataIdx={1}>{subtitle}</NavigationHeaderSubtitle>}
-        {isArray(points) && points?.map((point, idx) => {
-          return (
-            <NavigationHeaderPoints key={idx} dataIdx={idx + 1}>
-              {point}
-            </NavigationHeaderPoints>
-          );
-        })}
+        {isArray(points) &&
+          points?.map((point, idx) => {
+            return (
+              <NavigationHeaderPoints key={idx} dataIdx={idx + 1}>
+                {point}
+              </NavigationHeaderPoints>
+            );
+          })}
       </NavigationHeader>
       <main className='container-content-wrapper'>{children}</main>
-      <footer className='container-footer-wrapper'>
-        <Button title='Continue' />
-      </footer>
+      <div className='container-footer-wrapper' ref={footerWrapperRef}>
+        <Footer {...footer} />
+      </div>
     </Box>
   );
 };
