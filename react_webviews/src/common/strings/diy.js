1438c6670d9c902a8dfeba3b20d831585bc3a853
fdamitsingh <amit.singh@fisdom.com>
2022-01-30 05:43:15 +0530

    ADD: Complete Kyc 1. added assets 2. added strings

---
+++ react_webviews/src/common/strings/diy.js
@@ -0,0 +1,4 @@
+export const DIY = {
+    CompleteKyc: 'Complete KYC to start investing',
+    CompleteKycSubtitle: 'SEBI has made KYC mandatory for investing in mutual funds'
+}
---
+++ react_webviews/src/pages/DIY/CompleteKyc/CompleteKyc.js
@@ -0,0 +1,70 @@
+import React, { useMemo } from 'react';
+import Container from '../../../designSystem/organisms/Container';
+import {
+  LandingHeader,
+  LandingHeaderImage,
+  LandingHeaderSubtitle,
+  LandingHeaderTitle,
+} from '../../../designSystem/molecules/LandingHeader';
+import Typography from '../../../designSystem/atoms/Typography';
+import EMandateTrustIcon from '../../../designSystem/atoms/EMandateTrustIcon/EMandateTrustIcon';
+import { getConfig } from '../../../utils/functions';
+import checkedIcon from 'assets/checked.svg';
+import { DIY } from '../../../common/strings/diy';
+import Icon from '../../../designSystem/atoms/Icon';
+
+import './CompleteKyc.scss';
+
+const CompleteKyc = () => {
+  const { productName } = useMemo(getConfig, []);
+  return (
+    <Container
+      headerProps={{
+        hideHeaderTitle: true,
+      }}
+      footer={{
+        button1Props: {
+          title: 'Continue',
+        },
+      }}
+      className='complete-kyc-wrapper'
+    >
+      <LandingHeader variant='side'>
+        <LandingHeaderImage imgSrc={require(`assets/${productName}/complete_kyc.svg`)} />
+        <LandingHeaderTitle>{DIY.CompleteKyc}</LandingHeaderTitle>
+        <LandingHeaderSubtitle dataIdx={1}>{DIY.CompleteKycSubtitle}</LandingHeaderSubtitle>
+      </LandingHeader>
+      <section className='ck-points-wrapper'>
+        <div className='ck-points-list'>
+          {COMPLETE_KYC_POINTS?.map((point, idx) => {
+            return (
+              <div className='ck-points-item' key={idx}>
+                <Icon src={checkedIcon} size='24px' />
+                <Typography
+                  variant='body2'
+                  align='left'
+                  dataAid={`point${idx + 1}`}
+                  component='div'
+                >
+                  {point}
+                </Typography>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <div className='ck-trust-icon-wrapper'>
+        <EMandateTrustIcon opacity='0.6' />
+      </div>
+    </Container>
+  );
+};
+
+const COMPLETE_KYC_POINTS = [
+  'Single KYC for mutual funds & stocks',
+  '100% Digital, Aadhaar based KYC ',
+  'Convenient DigiLocker integration',
+  'One-time, instant, safe and easy ',
+];
+
+export default CompleteKyc;
