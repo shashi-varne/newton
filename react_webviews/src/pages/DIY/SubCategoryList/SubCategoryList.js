31f8473cd0a252c871a780946575daa4f142806d
fdamitsingh <amit.singh@fisdom.com>
2022-01-31 01:17:20 +0530

    UPDATE: Sub Category list 1. added support of confirm Action button

--- react_webviews/src/pages/DIY/SubCategoryList/SubCategoryList.js
+++ react_webviews/src/pages/DIY/SubCategoryList/SubCategoryList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import CardVertical from '../../../designSystem/molecules/CardVertical';
 import WrapperBox from '../../../designSystem/atoms/WrapperBox';
 import Container from '../../../designSystem/organisms/Container';
 import { CATEGORY_LISTS } from './constants';
 
 import './SubCategoryList.scss';
+import { getConfig } from '../../../utils/functions';
 
 const SubCategoryList = () => {
+  const { productName } = useMemo(getConfig, []);
   return (
     <Container
       headerProps={{
@@ -14,10 +16,13 @@ const SubCategoryList = () => {
       }}
       className='diy-sub-category-cv-wrapper'
       footer={{
-          button1Props: {
-              title: 'Continue'
-          },
+        confirmActionProps: {
+          buttonTitle: 'View Cart',
+          title: '2 item saved in your cart',
+          imgSrc: require('assets/amazon_pay.svg'),
+        },
       }}
+      noFooter={productName === 'finity'}
     >
       <div className='diy-sc-cv-lists'>
         {CATEGORY_LISTS?.map((category, idx) => {
