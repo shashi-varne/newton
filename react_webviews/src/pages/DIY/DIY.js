ef8570d2679689238515b6d58f336e7ce2466402
fdamitsingh <amit.singh@fisdom.com>
2022-01-31 15:53:19 +0530

    UPDATE: Sub category Funds 1. Added story 2. Added mock data and assets.

--- react_webviews/src/pages/DIY/DIY.js
+++ react_webviews/src/pages/DIY/DIY.js
@@ -2,6 +2,7 @@ import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import getTheme from '../../theme';
+import SubCategoryFunds from './SubCategoryFunds';
 
 const DIY = (props) => {
   const theme = getTheme();
@@ -10,6 +11,7 @@ const DIY = (props) => {
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <Switch>
+          <Route path={`${url}/sub-category-funds`} component={SubCategoryFunds} />
         </Switch>
       </ThemeProvider>
     </StyledEngineProvider>
