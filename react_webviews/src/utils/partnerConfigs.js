081dc1e3e24e7704e02ad92db5050bc7629dbed8
AlekhyaBathula <alekhya@fisdom.com>
2022-02-10 11:44:57 +0530

    updated nfo marketing banners

--- react_webviews/src/utils/partnerConfigs.js
+++ react_webviews/src/utils/partnerConfigs.js
@@ -55,32 +55,20 @@ export const commonCardsConfig = {
   landingMarketingBanners: [],
   nfoBanners: [
     {
-      image: "nfo5.svg",
+      image: "nfo.svg",
       type: "nfo",
-      startDate: "01/17/2022",
-      endDate: "01/30/2022",
-    },
-    {
-      image: "nfo2.svg",
-      type: "nfo",
-      endDate: "01/26/2022",
-    },
-    {
-      image: "nfo3.svg",
-      type: "nfo",
-      endDate: "01/26/2022",
-    },
-    {
-      image: "nfo4.svg",
-      type: "nfo",
-      startDate: "01/21/2022",
-      endDate: "02/03/2022",
+      endDate: "02/17/2022",
     },
     {
       image: "nfo1.svg",
       type: "nfo",
-      endDate: "01/23/2022",
+      endDate: "02/13/2022",
     },
+    {
+      image: "nfo2.svg",
+      type: "nfo",
+      endDate: "02/17/2022",
+    }
   ],
 };
 
