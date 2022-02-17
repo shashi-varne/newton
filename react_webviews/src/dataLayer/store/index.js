1d200203127439eb3b81cf1838a3f2b703028048
fdamitsingh <amit.singh@fisdom.com>
2022-02-17 12:29:53 +0530

    ADD: Screen 1. redux structure with store setup

---
+++ react_webviews/src/dataLayer/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import sagas from '../sagas';
+import combinedReducers from './dataStore/reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: combinedReducers,
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(sagas);
