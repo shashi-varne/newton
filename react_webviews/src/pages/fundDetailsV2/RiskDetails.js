b37d1b25de9052a1c6c7dff74d2567e79c407d50
fdamitsingh <amit.singh@fisdom.com>
2022-02-23 12:32:41 +0530

    UPDATE: Fund details 1. added tooltip to risk details.

--- react_webviews/src/pages/fundDetailsV2/RiskDetails.js
+++ react_webviews/src/pages/fundDetailsV2/RiskDetails.js
@@ -6,6 +6,8 @@ import BarMeter from '../../designSystem/atoms/BarMeter';
 import Separator from '../../designSystem/atoms/Separator';
 import Typography from '../../designSystem/atoms/Typography';
 import CollapsibleSection from '../../designSystem/molecules/CollapsibleSection';
+import Tooltip from '../../designSystem/atoms/Tooltip';
+import Icon from '../../designSystem/atoms/Icon';
 
 const barData = [
   {
@@ -33,6 +35,12 @@ const barData = [
   },
 ];
 
+const TOOLTIP_MEASURES = {
+  Alpha: 'Excess return generated over the benchmark',
+  Beta: 'Degree of sensitivity to benchmark',
+  squared: 'The deviation of the performance from the average over a period',
+};
+
 const barMeterData = (riskValue) => {
   // eslint-disable-next-line no-unused-expressions
   const newBarData = barData?.map((el, idx) => el);
@@ -48,10 +56,20 @@ const getBarIndex = (riskValue) => {
 
 const RiskDetails = () => {
   const [isRiskOpen, setIsRiskOpen] = useState(false);
+  const [isTooltipOpen, setIsTooltipOpen] = useState({
+    Alpha: false,
+    Beta: false,
+    squared: false,
+  });
   const fundData = useSelector(getFundData);
   const handleRiskAction = () => {
     setIsRiskOpen(!isRiskOpen);
   };
+
+  const handleTooltip = (riskMeasureName) => () => {
+    setIsTooltipOpen({ ...isTooltipOpen, [riskMeasureName]: !isTooltipOpen[riskMeasureName] });
+  };
+
   return (
     <Box sx={{ mt: 4 }}>
       <CollapsibleSection label='Risk details' isOpen={isRiskOpen} onClick={handleRiskAction}>
@@ -76,18 +94,52 @@ const RiskDetails = () => {
           <Stack sx={{ mt: 4, mb: 2 }} direction='column' spacing={3}>
             <Typography variant='heading4'>Risk measures</Typography>
             {fundData?.risk?.risk_measures?.map((riskMeasure, idx) => {
-              if(!riskMeasure?.name.match(/Alpha|Beta|squared/i)) return;
+              let tooltipInfo = '';
+              let riskMeasureName = '';
+              if (!riskMeasure?.name.match(/Alpha|Beta|squared/i)) return;
+              if (riskMeasure?.name.match(/Alpha/)) {
+                tooltipInfo = TOOLTIP_MEASURES['Alpha'];
+                riskMeasureName = 'Alpha';
+              } else if (riskMeasure?.name.match(/Beta/)) {
+                tooltipInfo = TOOLTIP_MEASURES['Beta'];
+                riskMeasureName = 'Beta';
+              } else if (riskMeasure?.name.match(/squared/)) {
+                tooltipInfo = TOOLTIP_MEASURES['squared'];
+                riskMeasureName = 'squared';
+              }
+
               return (
                 <Stack key={idx} direction='column' spacing={2}>
                   <Stack direction='row' justifyContent='space-between'>
                     <Typography variant='body8' color='foundationColors.content.secondary'>
                       {riskMeasure?.name}
                     </Typography>
-                    <Typography variant='heading4' color='foundationColors.content.secondary'>
-                      + {riskMeasure?.value}%
-                    </Typography>
+                    <Stack
+                      direction='row'
+                      spacing='6px'
+                      justifyContent='flex-start'
+                      alignItems='center'
+                    >
+                      <Typography variant='heading4' color='foundationColors.content.secondary'>
+                        + {riskMeasure?.value}%
+                      </Typography>
+                      <div>
+                        <Tooltip open={isTooltipOpen[riskMeasureName]} title={tooltipInfo}>
+                          <Stack width='16px' height='16px'>
+                            <Icon
+                              src={require('assets/info_icon_ds.svg')}
+                              size='16px'
+                              className='ec_info_icon'
+                              alt='info_icon'
+                              dataAid='right'
+                              onClick={handleTooltip(riskMeasureName)}
+                            />
+                          </Stack>
+                        </Tooltip>
+                      </div>
+                    </Stack>
                   </Stack>
-                  { idx < 2 && <Separator />}
+                  {idx < 2 && <Separator />}
                 </Stack>
               );
             })}
