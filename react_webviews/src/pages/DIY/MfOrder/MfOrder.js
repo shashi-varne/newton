af362a79b314e5769d0c095d56373bc5d1353707
fdamitsingh <amit.singh@fisdom.com>
2022-02-25 14:47:50 +0530

    UPDATE: Mf ORder details 1. separated the no mf order and order item 2. added lottie

--- react_webviews/src/pages/DIY/MfOrder/MfOrder.js
+++ react_webviews/src/pages/DIY/MfOrder/MfOrder.js
@@ -1,43 +1,32 @@
-import { Grow, IconButton, Stack } from '@mui/material';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import {
-  InvestmentCard,
-  InvestmentCardBottomRow,
-  InvestmentCardHeaderRow,
-  InvestmentCardInputRow,
-  InvestmentCardPillsRow,
-} from '../../../designSystem/molecules/InvestmentCard/InvestmentCard';
+import { Stack } from '@mui/material';
+import React, { useEffect, useMemo, useState } from 'react';
 import WrapperBox from '../../../designSystem/atoms/WrapperBox';
 import Container from '../../../designSystem/organisms/Container';
-import Icon from '../../../designSystem/atoms/Icon';
-import { dateOrdinal, formatAmountInr, storageService } from '../../../utils/validators';
+import { formatAmountInr, storageService } from '../../../utils/validators';
 import EstimationCard from '../../../designSystem/molecules/EstimationCard';
 import BottomSheet from '../../../designSystem/organisms/BottomSheet';
-import SipDateSelector from '../../../designSystem/molecules/SipDateSelector';
-import HeaderTitle from '../../../designSystem/molecules/HeaderTitle';
 import { useDispatch, useSelector } from 'react-redux';
 import Toast from '../../../designSystem/atoms/ToastMessage';
 import TrusIcon from '../../../designSystem/atoms/TrustIcon';
-
-import Api from '../../../utils/api';
 import {
   filterMfOrders,
   getfundOrderDetails,
   setFundOrderDetails,
-  setMfOrders,
 } from '../../../dataLayer/store/dataStore/reducers/diy';
 import { CART } from '../../../dashboard/DIY/constants';
 import isEmpty from 'lodash/isEmpty';
-import { validateMfOrderFunds } from './helperFunction';
 import Typography from '../../../designSystem/atoms/Typography';
 import { Pill, Pills } from '../../../designSystem/atoms/Pills/Pills';
 import { getConfig } from '../../../utils/functions';
 import values from 'lodash/values';
 import scrollIntoView from 'scroll-into-view-if-needed';
+import Api from '../../../utils/api';
 
 import './MfOrder.scss';
+import FundOrderItem from './FundOrderItem';
+import NoMfOrders from './NoMfOrders';
 
-const investmentAmountTile = {
+export const investmentAmountTile = {
   sip: 'SIP amount',
   lumpsum: 'Lumpsum amount',
 };
@@ -53,7 +42,7 @@ const MfOrder = () => {
   const [parentInvestmentType, setParentInvestmentType] = useState('sip');
   const [isInvestmentValid, setIsInvestmentValid] = useState({});
   const dispatch = useDispatch();
-  const { fundOrderDetails, mfOrders } = useSelector((state) => state?.mfOrdersReducer);
+  const { fundOrderDetails, mfOrders } = useSelector((state) => state?.mfOrders);
   const { productName } = useMemo(getConfig, []);
   const isProductFisdom = productName === 'fisdom';
 
@@ -151,13 +140,14 @@ const MfOrder = () => {
       }}
       footer={{
         button1Props: {
-          title: isEmpty(fundOrderDetails) ? 'Invest Now' : 'Continue',
+          title: 'Continue',
           onClick: handlePlaceOrders,
+          disabled: isEmpty(fundOrderDetails),
         },
       }}
     >
       {isEmpty(fundOrderDetails) ? (
-        <NoMfOrders showNoOrders={isEmpty(fundOrderDetails)} />
+        <NoMfOrders />
       ) : (
         <>
           <Stack direction='column' spacing={2} component='section' className='mf-order-wrapper'>
@@ -227,162 +217,3 @@ const MfOrder = () => {
 };
 
 export default MfOrder;
-
-const FundOrderItem = ({
-  fundDetails,
-  isProductFisdom,
-  handleInvestmentCard,
-  parentInvestmentType,
-  setIsInvestmentValid,
-}) => {
-  const [investmentType, setInvestmentType] = useState(parentInvestmentType);
-  const [selectedDate, setSelectedDate] = useState(fundDetails.addl_purchase?.sip?.default_date);
-  const [isSipSelectorOpen, setIsSipSelectorOpen] = useState(false);
-  const [amount, setAmount] = useState('');
-  const dispatch = useDispatch();
-  const minAmount = fundDetails?.addl_purchase[investmentType]?.min;
-  const maxAmount = fundDetails?.addl_purchase[investmentType]?.max;
-  const multiple = fundDetails?.addl_purchase[investmentType]?.mul;
-  const { message, showError } = validateMfOrderFunds(amount, minAmount, maxAmount, multiple);
-  const isInvestmentAllowed = {
-    sip: {
-      allowed: fundDetails?.sip,
-      errorMessage: fundDetails?.sip ? '' : 'Sip investment not allowed',
-    },
-    lumpsum: {
-      allowed: fundDetails?.onetime,
-      errorMessage: fundDetails?.onetime ? '' : 'Lumpsum investment not allowed',
-    },
-  };
-  const fundOrderItemRef = useRef();
-  useEffect(() => {
-    setIsInvestmentValid((prevState) => {
-      return {
-        ...prevState,
-        [fundDetails?.isin]: {
-          validationError: showError,
-          amountError: !amount,
-          orderItemRef: fundOrderItemRef.current,
-        },
-      };
-    });
-  }, [showError]);
-  useEffect(() => {
-    setInvestmentType(parentInvestmentType);
-  }, [parentInvestmentType]);
-
-  useEffect(() => {
-    const orderData = {
-      mfid: fundDetails.mfid,
-      amount,
-      mfname: fundDetails.mfname,
-    };
-    if (investmentType === 'sip') {
-      orderData.sip_date = selectedDate;
-      orderData.sip_dates = fundDetails.addl_purchase?.sip?.sip_dates;
-      orderData.default_date = fundDetails.addl_purchase?.sip?.default_date;
-    }
-    const order = {
-      [fundDetails.mfid]: orderData,
-    };
-    dispatch(setMfOrders(order));
-  }, [investmentType, amount, selectedDate]);
-
-  const handleAmountValue = (e) => {
-    setAmount(e.target.value);
-  };
-
-  const closeSipDateSheet = () => {
-    setIsSipSelectorOpen(false);
-  };
-
-  const openSipSelectorSheet = () => {
-    setIsSipSelectorOpen(true);
-  };
-
-  const handleSelectedDate = (date) => {
-    setSelectedDate(date);
-  };
-  const handleInvestmentType = (e, value) => {
-    setInvestmentType(value);
-  };
-
-  return (
-    <div ref={fundOrderItemRef}>
-      {!isProductFisdom && (
-        <HeaderTitle sx={{ mb: 2 }} title={fundDetails.mfname} imgSrc={fundDetails.amc_logo_big} />
-      )}
-      <WrapperBox elevation={1} className='mf-investment-card-wrapper'>
-        {isProductFisdom && (
-          <IconButton className='mf-ic-close' onClick={handleInvestmentCard(fundDetails)}>
-            <Icon src={require('assets/close_grey.svg')} size='24px' />
-          </IconButton>
-        )}
-        <InvestmentCard>
-          {isProductFisdom && (
-            <InvestmentCardHeaderRow title={fundDetails.mfname} imgSrc={fundDetails.amc_logo_big} />
-          )}
-          <InvestmentCardPillsRow
-            title='Investment Type'
-            hideSeparator={!isProductFisdom}
-            hide={isProductFisdom}
-            pillsProps={{
-              value: investmentType,
-              onChange: handleInvestmentType,
-            }}
-            pillsChild={[
-              {
-                label: 'SIP',
-                value: 'sip',
-              },
-              {
-                label: 'Lumpsum',
-                value: 'lumpsum',
-              },
-            ]}
-          />
-          <InvestmentCardInputRow
-            title={investmentAmountTile[investmentType]}
-            subtitle={isInvestmentAllowed[investmentType].errorMessage || message}
-            subtitleColor={
-              !isInvestmentAllowed[investmentType].allowed || showError
-                ? 'foundationColors.secondary.lossRed.400'
-                : 'foundationColors.content.tertiary'
-            }
-            inputFieldProps={{
-              prefix: '₹',
-              value: amount,
-              onChange: handleAmountValue,
-              disabled: !isInvestmentAllowed[investmentType].allowed,
-            }}
-          />
-          <InvestmentCardBottomRow
-            hide={parentInvestmentType === 'lumpsum' || investmentType === 'lumpsum'}
-            leftTitle='Monthly SIP date'
-            rightTitle={`${dateOrdinal(selectedDate)} every month`}
-            onRightSectionClick={openSipSelectorSheet}
-            rightImgSrc={require('assets/arrw_down.svg')}
-          />
-        </InvestmentCard>
-        <SipDateSelector
-          sipDates={fundDetails.addl_purchase?.sip?.sip_dates}
-          selectedDate={selectedDate}
-          isOpen={isSipSelectorOpen}
-          onClose={closeSipDateSheet}
-          handleSelectedDate={handleSelectedDate}
-        />
-      </WrapperBox>
-    </div>
-  );
-};
-
-const NoMfOrders = ({ showNoOrders }) => {
-  return (
-    <Grow in={showNoOrders} timeout={450}>
-      <Stack sx={{ height: '70vh' }} spacing='12px' alignItems='center' justifyContent='center'>
-        <Icon src={require('assets/no_mf_orders.svg')} width='140px' height='120px' />
-        <Typography align='center'>Please add at least one fund to make an investment</Typography>
-      </Stack>
-    </Grow>
-  );
-};
