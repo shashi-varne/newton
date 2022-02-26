496d48b138d9581bda1926222702b4a954ada48a
AlekhyaBathula <alekhya@fisdom.com>
2022-02-27 00:00:25 +0530

    Added pathname mapper

---
+++ react_webviews/src/pages/DIY/common/constants.js
@@ -0,0 +1,7 @@
+export const DIY_PATHNAME_MAPPER = {
+  subcategoryList: "/diyv2/sub-category-list",
+  subcategoryFunds: "/diyv2/sub-category-funds",
+  subcategoryFundList: "/diyv2/sub-category/fund-list",
+  fundDetails: "/diyv2/fund-details",
+  diyInvestLanding: "/invest/explore",
+};
