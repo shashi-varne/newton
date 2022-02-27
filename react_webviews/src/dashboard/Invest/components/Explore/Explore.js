167e03c2b38ccbdb2b79519f4f5870694d716ea1
AlekhyaBathula <alekhya@fisdom.com>
2022-02-28 03:27:33 +0530

    Fixed resetting filter data

--- react_webviews/src/dashboard/Invest/components/Explore/Explore.js
+++ react_webviews/src/dashboard/Invest/components/Explore/Explore.js
@@ -24,7 +24,8 @@ import { flowName } from '../../constants'
 import { isNewIframeDesktopLayout } from '../../../../utils/functions'
 import Api from '../../../../utils/api';
 import { useDispatch } from 'react-redux'
-import { fetchDiyCategoriesAndTrendingFunds, setFundsCart } from 'businesslogic/dataStore/reducers/diy';
+import { fetchDiyCategoriesAndTrendingFunds, setFundsCart, setFilteredFundList } from 'businesslogic/dataStore/reducers/diy';
+import { DEFAULT_FILTER_DATA } from 'businesslogic/constants/diy'
 
 const screen = 'diyLanding';
 const InvestExplore = (props) => {
@@ -38,6 +39,7 @@ const InvestExplore = (props) => {
   useEffect(() => {
     dispatch(fetchDiyCategoriesAndTrendingFunds({Api, screen}));    
     dispatch(setFundsCart([]));
+    dispatch(setFilteredFundList({ filterOptions: DEFAULT_FILTER_DATA }));
   },[])
   const newIframeDesktopLayout = isNewIframeDesktopLayout() || (partnerCode === 'moneycontrol' && !isMobileDevice);
 
