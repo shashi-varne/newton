82ce763448bd53ae8fc1fdbe0d20f6fe30bb964a
AlekhyaBathula <alekhya@fisdom.com>
2022-02-28 19:34:01 +0530

    Minor fixes

--- react_webviews/src/featureComponent/DIY/Filters/FilterNavigation.js
+++ react_webviews/src/featureComponent/DIY/Filters/FilterNavigation.js
@@ -6,7 +6,7 @@ const FilterNavigation = ({returnLabel, handleSortClick, handleFilterClick, hand
   return (
     <Stack direction='row' justifyContent='space-between' sx={{boxShadow:'1', bgcolor: 'foundationColors.supporting.white', m: '0px -16px !important'}}>
       <Filter title='Sort' onClick={handleSortClick} imgSrc={require('assets/union.svg')} dataAid="1" />
-      <Filter title='Filter' onClick={handleFilterClick} imgSrc={require('assets/filter.svg')} count={count} dataAid="2" />
+      <Filter title='Filter' onClick={handleFilterClick} imgSrc={require('assets/filter.svg')} filterCount={filterCount} dataAid="2" />
       <Filter title={`${returnLabel} returns`} onClick={handleReturnClick} imgSrc={require('assets/duration.svg')} dataAid="3" />
     </Stack>
   );
