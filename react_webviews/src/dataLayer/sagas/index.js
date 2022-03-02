d0ba041ef4decad166b0ea0564b36c6a8b0f1d0d
fdamitsingh <amit.singh@fisdom.com>
2022-03-02 22:22:31 +0530

    removed local data layer.

--- react_webviews/src/dataLayer/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import sagaFetchFundDetails from './fundDetailsSaga';
-import diySaga from './diySaga';
-
-export default function* rootSaga() {
-	yield all([sagaFetchFundDetails(), diySaga()]);
-}
--- react_webviews/src/dataLayer/store/dataStore/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import diy from './diy';
-import fundDetails from './fundDetails';
-
-export default combineReducers({
-	fundDetails,
-    diy,
-});
