74be62c3ac9b7966b6809e932a4035669ac887d2
fdamitsingh <amit.singh@fisdom.com>
2022-03-10 01:37:00 +0530

    update: separated complete kyc view and container.

--- react_webviews/src/pages/DIY/CompleteKyc/CompleteKyc.js
+++ react_webviews/src/pages/DIY/CompleteKyc/CompleteKyc.js
@@ -1,48 +1,21 @@
-import React, { useMemo } from 'react';
-import Container from '../../../designSystem/organisms/ContainerWrapper';
 import Stack from '@mui/material/Stack';
+import checkedIcon from 'assets/checked.svg';
+import { DIY } from 'businesslogic/strings/diy';
+import Lottie from 'lottie-react';
+import React from 'react';
+import Icon from '../../../designSystem/atoms/Icon';
+import TrustIcon from '../../../designSystem/atoms/TrustIcon';
+import Typography from '../../../designSystem/atoms/Typography';
 import {
   LandingHeader,
   LandingHeaderSubtitle,
-  LandingHeaderTitle,
+  LandingHeaderTitle
 } from '../../../designSystem/molecules/LandingHeader';
-import Typography from '../../../designSystem/atoms/Typography';
-import TrustIcon from '../../../designSystem/atoms/TrustIcon';
-import { getConfig } from '../../../utils/functions';
-import checkedIcon from 'assets/checked.svg';
-import { DIY } from 'businesslogic/strings/diy';
-import Icon from '../../../designSystem/atoms/Icon';
-import { navigate as navigateFunc } from '../../../utils/functions';
-import { DIY_PATHNAME_MAPPER } from '../common/constants';
-import Lottie from 'lottie-react';
-
+import Container from '../../../designSystem/organisms/ContainerWrapper';
 import './CompleteKyc.scss';
 
-const CompleteKyc = (props) => {
-  const navigate = navigateFunc.bind(props);
-  const { productName, Web, isIframe } = useMemo(getConfig, []);
-  const onClick = () => {
-    const event = {
-      event_name: 'journey_details',
-      properties: {
-        journey: {
-          name: 'mf',
-          trigger: 'cta',
-          journey_status: 'incomplete',
-          next_journey: 'kyc',
-        },
-      },
-    };
-    // send event
-    if (!Web) {
-      window.callbackWeb.eventCallback(event);
-    } else if (isIframe) {
-      const message = JSON.stringify(event);
-      window.callbackWeb.sendEvent(message);
-    }
-    navigate(DIY_PATHNAME_MAPPER.kycWeb);
-  };
 
+const CompleteKyc = ({ onClick, productName }) => {
   return (
     <Container
       headerProps={{
@@ -55,15 +28,15 @@ const CompleteKyc = (props) => {
         },
       }}
       className='complete-kyc-wrapper'
-      dataAid="completeKyc"
+      dataAid='completeKyc'
     >
-      <LandingHeader variant='side' dataAid="completeKyc" >
+      <LandingHeader variant='side' dataAid='completeKyc'>
         <Lottie
           animationData={require(`assets/${productName}/lottie/kyc_complete.json`)}
           autoPlay
           loop
           className='kyc-compl-lottie-anim'
-          data-aid="iv_top"
+          data-aid='iv_top'
         />
         <LandingHeaderTitle>{DIY.completeKycTitle}</LandingHeaderTitle>
         <LandingHeaderSubtitle dataIdx={1}>{DIY.completeKycSubtitle}</LandingHeaderSubtitle>
@@ -73,8 +46,8 @@ const CompleteKyc = (props) => {
           {COMPLETE_KYC_POINTS?.map((data, idx) => {
             return (
               <Stack key={idx} direction='row' spacing={2} alignItems='center'>
-                <Icon src={checkedIcon} size='24px' dataAid={`left${idx+1}`} />
-                <Typography variant='body2' align='left' component='div' dataAid={data.dataAid} >
+                <Icon src={checkedIcon} size='24px' dataAid={`left${idx + 1}`} />
+                <Typography variant='body2' align='left' component='div' dataAid={data.dataAid}>
                   {data.label}
                 </Typography>
               </Stack>
@@ -83,17 +56,17 @@ const CompleteKyc = (props) => {
         </Stack>
       </section>
       <div className='ck-trust-icon-wrapper'>
-        <TrustIcon variant='secure' opacity='0.6' dataAid="1" />
+        <TrustIcon variant='secure' opacity='0.6' dataAid='1' />
       </div>
     </Container>
   );
 };
 
 const COMPLETE_KYC_POINTS = [
-  { label: DIY.singleKyc, dataAid: "singleKyc" },
-  { label: DIY.digitalKyc, dataAid: "aadhaar" },
-  { label: DIY.digilockerIntegeration, dataAid: "digiLocker" },
-  { label: DIY.instantSafe, dataAid: "oneTime" },
+  { label: DIY.singleKyc, dataAid: 'singleKyc' },
+  { label: DIY.digitalKyc, dataAid: 'aadhaar' },
+  { label: DIY.digilockerIntegeration, dataAid: 'digiLocker' },
+  { label: DIY.instantSafe, dataAid: 'oneTime' },
 ];
 
 export default CompleteKyc;
--- react_webviews/src/pages/DIY/DiyV2.js
+++ react_webviews/src/pages/DIY/DiyV2.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import CompleteKyc from './CompleteKyc';
 import InvestmentProcess from './InvestmentProcess';
 import SubCategoryList from './SubCategoryList';
 import SubCategoryFunds from './SubCategoryFunds';
 import SubCategoryFundList from './SubCategoryFundList';
 import DiyLandingContainer from '../../containers/diy/diyLandingContainer';
+import completeKycContainer from '../../containers/diy/completeKycContainer';
 const DIY = (props) => {
   const { url } = props.match;
   return (
@@ -13,7 +13,7 @@ const DIY = (props) => {
       <Route path={`${url}/sub-category-list`} component={SubCategoryList} />
       <Route path={`${url}/sub-category-funds`} component={SubCategoryFunds} />
       <Route exact path={`${url}/:diyType/landing`} component={DiyLandingContainer} />
-      <Route path={`${url}/complete-kyc`} component={CompleteKyc} />
+      <Route path={`${url}/complete-kyc`} component={completeKycContainer} />
       <Route path={`${url}/invest-process`} component={InvestmentProcess} />
       <Route path={`${url}/sub-category/fund-list`} component={SubCategoryFundList} />
     </Switch>
