3b2c944d27a3cff5f72afa98b878c6d9ad7781ee
fdamitsingh <amit.singh@fisdom.com>
2022-03-10 02:01:13 +0530

    update: separated subcategorylist view and container.

--- react_webviews/src/pages/DIY/DiyV2.js
+++ react_webviews/src/pages/DIY/DiyV2.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import SubCategoryList from './SubCategoryList';
 import SubCategoryFunds from './SubCategoryFunds';
 import SubCategoryFundList from './SubCategoryFundList';
 import DiyLandingContainer from '../../containers/diy/diyLandingContainer';
 import completeKycContainer from '../../containers/diy/completeKycContainer';
 import investmentProcessContainer from '../../containers/diy/investmentProcessContainer';
+import subCategoryListContainer from '../../containers/diy/subCategoryListContainer';
 const DIY = (props) => {
   const { url } = props.match;
   return (
     <Switch>
-      <Route path={`${url}/sub-category-list`} component={SubCategoryList} />
+      <Route path={`${url}/sub-category-list`} component={subCategoryListContainer} />
       <Route path={`${url}/sub-category-funds`} component={SubCategoryFunds} />
       <Route exact path={`${url}/:diyType/landing`} component={DiyLandingContainer} />
       <Route path={`${url}/complete-kyc`} component={completeKycContainer} />
--- react_webviews/src/pages/DIY/SubCategoryList/SubCategoryList.js
+++ react_webviews/src/pages/DIY/SubCategoryList/SubCategoryList.js
@@ -1,65 +1,17 @@
-import React, { useMemo } from 'react';
+import { VIEW_TYPE_MAPPER } from 'businesslogic/constants/diy';
+import isArray from 'lodash/isArray';
+import PropTypes from 'prop-types';
+import React from 'react';
+import WrapperBox from '../../../designSystem/atoms/WrapperBox';
 import CardVertical from '../../../designSystem/molecules/CardVertical';
 import CategoryCard from '../../../designSystem/molecules/CategoryCard';
-import WrapperBox from '../../../designSystem/atoms/WrapperBox';
 import Container from '../../../designSystem/organisms/ContainerWrapper';
-import isArray from 'lodash/isArray';
-import { getConfig } from '../../../utils/functions';
-import PropTypes from 'prop-types';
-import replace from 'lodash/replace';
-
-import {
-  setDiyTypeData,
-  getDiyTypeData,
-  getDiySubcategoryData,
-  getDiyCartCount
-} from "businesslogic/dataStore/reducers/diy";
-import { useDispatch, useSelector } from "react-redux";
-import { navigate as navigateFunc } from "../../../utils/functions";
-import './SubCategoryList.scss';
-import { DIY_PATHNAME_MAPPER } from '../common/constants';
-import { hideDiyCartFooter } from "businesslogic/utils/diy/functions";
-import { VIEW_TYPE_MAPPER } from 'businesslogic/constants/diy';
 import { validateKycAndRedirect } from '../common/functions';
-import useUserKycHook from '../../../kyc/common/hooks/userKycHook';
-import { nativeCallback } from '../../../utils/native_callback';
+import './SubCategoryList.scss';
 
 const SubCategoryList = (props) => {
-  const navigate = navigateFunc.bind(props);
-  const dispatch = useDispatch();
-  const diyTypeData = useSelector(getDiyTypeData);
-  const cartCount = useSelector(getDiyCartCount);
-  const subcategoryData = useSelector((state) => getDiySubcategoryData(state, diyTypeData.category, diyTypeData.subcategory));
-  const { productName } = useMemo(getConfig, []);
-  const hideFooter = useMemo(hideDiyCartFooter(productName, cartCount), [productName, cartCount]);
-  const { kyc, isLoading, user } = useUserKycHook();
-  const handleCardClick = (subcategoryOption, subcategoryOptionName) => () => {
-    dispatch(
-      setDiyTypeData({
-        subcategoryOption,
-      })
-    );
-    const formatedCategory = replace(diyTypeData.subcategory,/_/g,' ').toLowerCase();
-    const categoryEvent = `${diyTypeData.category.toLowerCase()} ${formatedCategory}`;
-    const subCategoryEvent = subcategoryOptionName.toLowerCase();
-    sendEvents(categoryEvent, subCategoryEvent);
-    navigate(DIY_PATHNAME_MAPPER.subcategoryFundList);
-  };
-
-  const sendEvents = (category, subCategory) => {
-    const eventObj = {
-      event_name: 'diy_sub_category_clicked',
-      properties: {
-        category: category || '',
-        user_action: 'next',
-        sub_category: subCategory || '',
-        user_application_status: kyc?.application_status_v2 || 'init',
-        user_investment_status: user?.active_investment,
-        user_kyc_status: kyc?.mf_kyc_processed || false,
-      },
-    };
-    nativeCallback({ events: eventObj });
-  };
+  const { subcategoryData, cartCount, navigate, kyc, hideFooter, isLoading, handleCardClick } =
+    props;
 
   return (
     <Container
@@ -83,7 +35,7 @@ const SubCategoryList = (props) => {
       isPageLoading={isLoading}
       dataAid={subcategoryData?.design_id}
     >
-      <div className="diy-sc-cv-lists">
+      <div className='diy-sc-cv-lists'>
         {isArray(subcategoryData.options) &&
           subcategoryData.options?.map((data, idx) => {
             return (
@@ -104,20 +56,16 @@ const SubcategoryOptionCard = ({ viewType, data, onClick }) => {
   return (
     <>
       {viewType === VIEW_TYPE_MAPPER.imageCaurosel ? (
-        <div className="diy-sc-cv-item" onClick={onClick}>
+        <div className='diy-sc-cv-item' onClick={onClick}>
           <CategoryCard
-            variant="large"
+            variant='large'
             imgSrc={data?.image_url}
             title={data?.name}
             dataAid={data?.design_id}
           />
         </div>
       ) : (
-        <WrapperBox
-          elevation={1}
-          className="diy-sc-cv-item"
-          onClick={onClick}
-        >
+        <WrapperBox elevation={1} className='diy-sc-cv-item' onClick={onClick}>
           <CardVertical
             imgSrc={data?.image_url}
             title={data?.name}
