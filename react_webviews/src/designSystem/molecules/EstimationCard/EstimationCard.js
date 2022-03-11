74c7c43fe8d8f02a837e3b53f3ce06f845c7cf03
fdamitsingh <amit.singh@fisdom.com>
2022-03-11 17:18:15 +0530

    update: UPDATED ESTIMATION CARD TO TAKE CARE OF tooltip

--- react_webviews/src/designSystem/molecules/EstimationCard/EstimationCard.js
+++ react_webviews/src/designSystem/molecules/EstimationCard/EstimationCard.js
@@ -14,7 +14,7 @@ import PropTypes from 'prop-types';
 import Typography from '../../atoms/Typography';
 import Tooltip from '../../atoms/Tooltip';
 import './EstimationCard.scss';
-import { ClickAwayListener } from '@mui/material';
+import { ClickAwayListener, Stack } from '@mui/material';
 import isFunction from 'lodash/isFunction';
 import Icon from '../../atoms/Icon';
 
@@ -43,50 +43,62 @@ const EstimationCard = ({
       onClick={onClick}
       data-aid={`estimationCard_${dataAid}`}
     >
-      <div className='ec-left-wrapper'>
-        {leftTitle && (
-          <div className='ec-left-title-wrapper'>
-            <Typography variant='body1' color={leftTitleColor} component='div' dataAid='title1'>
-              {leftTitle}
+      <Stack direction='column' flex={1}>
+        <Stack direction='row' justifyContent='space-between' spacing={1}>
+          {leftTitle && (
+            <Box sx={{ flexBasis: '100%' }}>
+              <Typography variant='body1' color={leftTitleColor} component='div' dataAid='title1'>
+                {leftTitle}
+                {(iconSrc || toolTipText) && (
+                  <InfoTooltip
+                    dataAid={toolTipDataAid}
+                    toolTipText={toolTipText}
+                    onIconClick={onIconClick}
+                    iconSrc={iconSrc}
+                  />
+                )}
+              </Typography>
+            </Box>
+          )}
+          {rightTitle && (
+            <Typography
+              variant='body2'
+              color={rightTitleColor}
+              component='div'
+              dataAid='title2'
+              align='right'
+              sx={{ flexBasis: '100%' }}
+            >
+              {rightTitle}
             </Typography>
-            {(iconSrc || toolTipText ) && <InfoTooltip dataAid={toolTipDataAid} toolTipText={toolTipText} onIconClick={onIconClick} iconSrc={iconSrc}/>}
-          </div>
-        )}
-        {leftSubtitle && (
-          <Typography
-            variant='body5'
-            color={leftSubtitleColor}
-            component='div'
-            dataAid='subtitle1'
-          >
-            {leftSubtitle}
-          </Typography>
-        )}
-      </div>
-      <div className='ec-right-wrapper'>
-        {rightTitle && (
-          <Typography
-            variant='body2'
-            color={rightTitleColor}
-            component='div'
-            dataAid='title2'
-            align='right'
-          >
-            {rightTitle}
-          </Typography>
-        )}
-        {rightSubtitle && (
-          <Typography
-            variant='body4'
-            color={rightSubtitleColor}
-            component='div'
-            dataAid='subtitle2'
-            align='right'
-          >
-            {rightSubtitle}
-          </Typography>
-        )}
-      </div>
+          )}
+        </Stack>
+        <Stack direction='row' justifyContent='space-between' spacing={1}>
+          {leftSubtitle && (
+            <Typography
+              variant='body5'
+              color={leftSubtitleColor}
+              component='div'
+              dataAid='subtitle1'
+              sx={{ flexBasis: '100%' }}
+            >
+              {leftSubtitle}
+            </Typography>
+          )}
+          {rightSubtitle && (
+            <Typography
+              variant='body4'
+              color={rightSubtitleColor}
+              component='div'
+              dataAid='subtitle2'
+              align='right'
+              sx={{ flexBasis: '100%' }}
+            >
+              {rightSubtitle}
+            </Typography>
+          )}
+        </Stack>
+      </Stack>
     </Box>
   );
 };
@@ -100,41 +112,43 @@ const InfoTooltip = ({ dataAid, toolTipText, onIconClick, iconSrc }) => {
     setIsTooltipOpen(false);
   };
   const handleIconClick = (e) => {
-    if(toolTipText) {
+    if (toolTipText) {
       e.stopPropagation();
       setIsTooltipOpen(true);
-    } 
-    if(isFunction(onIconClick)) {
+    }
+    if (isFunction(onIconClick)) {
       e.stopPropagation();
       onIconClick(e);
     }
   };
   return (
-    <ClickAwayListener onClickAway={handleTooltipClose}>
-      <div>
-        <Tooltip
-          PopperProps={{
-            disablePortal: true,
-          }}
-          disableFocusListener
-          disableHoverListener
-          disableTouchListener
-          open={isTooltipOpen}
-          title={toolTipText}
-          dataAid={dataAid}
-        >
-          <div onClick={handleIconClick}>
-            <Icon
-              src={iconSrc}
-              size='16px'
-              className='ec_info_icon'
-              alt='info_icon'
-              dataAid='right'
-            />
-          </div>
-        </Tooltip>
-      </div>
-    </ClickAwayListener>
+    <Box component='span'>
+      <ClickAwayListener onClickAway={handleTooltipClose}>
+        <Box sx={{ pl: 1 }} component='span' className='ec-tooltip-wrapper'>
+          <Tooltip
+            PopperProps={{
+              disablePortal: true,
+            }}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            open={isTooltipOpen}
+            title={toolTipText}
+            dataAid={dataAid}
+          >
+            <div onClick={handleIconClick}>
+              <Icon
+                src={iconSrc}
+                size='16px'
+                className='ec_info_icon'
+                alt='info_icon'
+                dataAid='right'
+              />
+            </div>
+          </Tooltip>
+        </Box>
+      </ClickAwayListener>
+    </Box>
   );
 };
 
@@ -159,7 +173,7 @@ EstimationCard.defaultProps = {
 
 InfoTooltip.defaultProps = {
   iconSrc: require('assets/ec_info.svg'),
-}
+};
 
 const esSxStyle = {
   backgroundColor: 'foundationColors.supporting.white',
