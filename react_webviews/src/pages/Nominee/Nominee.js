65ed97e79ce85c32e17c81ea55215c565b65f6c0
Dhiraj Singh <dhiraj.singh@fisdom.com>
2022-05-05 16:23:26 +0530

    routes updated

--- react_webviews/src/pages/Nominee/Nominee.js
+++ react_webviews/src/pages/Nominee/Nominee.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import { Route, Switch } from "react-router-dom";
+import NomineeVerifiedContainer from "../../containers/nominee/nomineeVerifiedContainer";
+import NomineeSubmittedContainer from "../../containers/nominee/nomineeSubmittedContainer";
 
 const Nominee = (props) => {
   const { url } = props.match;
 
   return (
     <Switch>
+      <Route path={`${url}/verified`} component={NomineeVerifiedContainer} />
+      <Route path={`${url}/submitted`} component={NomineeSubmittedContainer} />
     </Switch>
   );
 };
