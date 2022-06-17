90cf05eb07be4284c26cf76de0de86f74076c9c5
Dhiraj Singh <dhiraj.singh@fisdom.com>
2022-06-17 17:57:06 +0530

    config change to show referral card for fisdom

--- react_webviews/src/utils/partnerConfigs.js
+++ react_webviews/src/utils/partnerConfigs.js
@@ -96,7 +96,7 @@ export const basePartnerConfig = {
     riskEnabledFunnels: false,
     referralConfig: {
       applyRefferal: true, // same as hide_apply_referral but with opposite value
-      shareRefferal: false, // same as hide_share_referral but with opposite value
+      shareRefferal: true, // same as hide_share_referral but with opposite value
     },
   },
   finity: {
