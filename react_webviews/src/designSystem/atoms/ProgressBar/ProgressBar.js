e57fa9a7008aa7238568f058683bf7a899b6fad0
shashi-varne <shashi.varne04@gmail.com>
2022-06-22 17:32:41 +0530

    PR comments

--- react_webviews/src/designSystem/atoms/ProgressBar/ProgressBar.js
+++ react_webviews/src/designSystem/atoms/ProgressBar/ProgressBar.js
@@ -1,7 +1,7 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
 import "./ProgressBar.scss";
-import PropTypes from "prop-types";
+import PropTypes, { number, string } from "prop-types";
 import Typography from "../Typography";
 
 function ProgressBar({
@@ -11,49 +11,45 @@ function ProgressBar({
   dataAidSuffix,
   title,
   label,
+  titleColor,
+  labelColor,
 }) {
+  console.log(titleColor, labelColor);
   return (
     <Box
       data-aid={`progressBar_${dataAidSuffix}`}
       className="progress-container"
     >
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        className="text-row"
-      >
-        <Typography
-          variant="body8"
-          style={{ color: "foundationColors.content.primary" }}
-          dataAid="tv_key"
+      {(!!title || !!label) && (
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          className="text-row"
         >
-          {title}
-        </Typography>
-        <Typography
-          variant="body8"
-          style={{ color: "foundationColors.content.primary" }}
-          dataAid="tv_value"
-        >
-          {label}
-        </Typography>
-      </Stack>
+          <Typography
+            variant="body8"
+            color={{ color: titleColor }}
+            dataAid="tv_key"
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="body8"
+            color={{ color: labelColor }}
+            dataAid="tv_value"
+          >
+            {label}
+          </Typography>
+        </Stack>
+      )}
       <Box
         className="progress-bar-container"
-        sx={{
-          backgroundColor: progressBackgroundColor
-            ? progressBackgroundColor
-            : "foundationColors.supporting.athensGrey",
-        }}
+        sx={{ backgroundColor: progressBackgroundColor }}
       >
         <Box
           className="progress-percentage"
-          sx={{
-            width: `${percentage}%`,
-            backgroundColor: progressColor
-              ? progressColor
-              : "foundationColors.supporting.cadetBlue",
-          }}
+          sx={{ width: `${percentage}%`, backgroundColor: progressColor }}
         ></Box>
       </Box>
     </Box>
@@ -61,19 +57,25 @@ function ProgressBar({
 }
 
 ProgressBar.defaultProps = {
-  progressBackgroundColor: "",
-  progressColor: "",
+  progressBackgroundColor: "foundationColors.supporting.athensGrey",
+  progressColor: "foundationColors.supporting.cadetBlue",
   percentage: 0,
   title: "",
   label: "",
+  dataAidSuffix: "",
+  titleColor: "foundationColors.content.primary",
+  labelColor: "foundationColors.content.primary",
 };
 
-ProgressBar.proptypes = {
+ProgressBar.propTypes = {
   progressBackgroundColor: PropTypes.string,
   progressColor: PropTypes.string,
-  percentage: PropTypes.number,
-  title: PropTypes.string,
-  label: PropTypes.string,
+  percentage: PropTypes.number.isRequired,
+  title: PropTypes.oneOf([PropTypes.number, PropTypes.string]),
+  label: PropTypes.oneOf([PropTypes.number, PropTypes.string]),
+  dataAidSuffix: PropTypes.string.isRequired,
+  titleColor: PropTypes.string,
+  labelColor: PropTypes.string,
 };
 
 export default ProgressBar;
