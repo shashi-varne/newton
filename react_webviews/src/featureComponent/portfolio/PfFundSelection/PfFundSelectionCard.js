397c6b962be91bcf91e94cbea99a590853f5d31e
shashi-varne <shashi.varne04@gmail.com>
2022-06-22 18:16:23 +0530

    PR comments resolved

--- react_webviews/src/featureComponent/portfolio/PfFundSelection/PfFundSelectionCard.js
+++ react_webviews/src/featureComponent/portfolio/PfFundSelection/PfFundSelectionCard.js
@@ -2,46 +2,52 @@ import { Box, Stack } from "@mui/material";
 import Checkbox from "designSystem/atoms/Checkbox";
 import Separator from "designSystem/atoms/Separator";
 import React from "react";
-import Icon from "../../../atoms/Icon";
-import Typography from "../../../atoms/Typography";
-import Tag from "../../../molecules/Tag/Tag";
+import Icon from "designSystem/atoms/Icon";
+import Typography from "designSystem/atoms/Typography";
+import Tag from "designSystem/molecules/Tag/Tag";
 import "./PfFundSelectionCard.scss";
 import PropTypes from "prop-types";
 import { noop } from "lodash-es";
 
 function PfFundSelectionCard({
   checked,
-  topTitle,
-  topLabel,
-  leftTitle,
-  leftSubtitle,
-  middleTitle,
-  middleSubtitle,
-  rightTitle,
-  rightSubtitle,
-  bottomTitle,
-  bottomSubtitle,
-  bottomLabel,
+  textProps,
   leftImgSrc,
   rightImgSrc,
   middleImgSrc,
   bottomImgSrc,
   onClick,
+  textColors,
 }) {
+  const {
+    topTitle,
+    topLabel,
+    leftTitle,
+    leftSubtitle,
+    middleTitle,
+    middleSubtitle,
+    rightTitle,
+    rightSubtitle,
+    bottomTitle,
+    bottomSubtitle,
+    bottomLabel,
+    dataAidSuffix,
+  } = textProps;
   return (
     <Box
       sx={{
         backgroundColor: "foundationColors.supporting.white",
       }}
-      className="container"
+      className="pf-selection-container"
       onClick={onClick}
+      data-aid={`pfFundSelectionCard_${dataAidSuffix}`}
     >
       <RowContainer className="top-row" alignItems="flex-start">
         <RowContainer justifyContent="flex-start">
           <Checkbox checked={checked} className="checkbox" />
           <Typography
             variant="body1"
-            color="foundationColors.content.primary"
+            color={textColors?.topTitle || "foundationColors.content.primary"}
             dataAid="title"
             className="main-title"
           >
@@ -50,7 +56,7 @@ function PfFundSelectionCard({
         </RowContainer>
         <Typography
           variant="body1"
-          color="foundationColors.content.primary"
+          color={textColors.topLabel || "foundationColors.content.primary"}
           dataAid="label"
           className="top-label"
         >
@@ -66,6 +72,8 @@ function PfFundSelectionCard({
           subtitle={leftSubtitle}
           img={leftImgSrc}
           id={1}
+          titleColor={textColors?.leftTitle}
+          subtitleColor={textColors?.leftSubtitle}
         />
         <CardItem
           align="center"
@@ -73,6 +81,8 @@ function PfFundSelectionCard({
           subtitle={middleSubtitle}
           img={middleImgSrc}
           id={2}
+          titleColor={textColors?.middleTitle}
+          subtitleColor={textColors?.middleSubtitle}
         />
         <CardItem
           align="right"
@@ -80,13 +90,17 @@ function PfFundSelectionCard({
           subtitle={rightSubtitle}
           img={rightImgSrc}
           id={3}
+          titleColor={textColors?.rightTitle}
+          subtitleColor={textColors?.rightSubtitle}
         />
       </RowContainer>
       <Separator dataAid={"1"} />
       <RowContainer className="bottom-row">
         <Typography
           variant="body5"
-          color="foundationColors.content.secondary"
+          color={
+            textColors?.bottomTitle || "foundationColors.content.secondary"
+          }
           dataAid="info"
         >
           {bottomTitle}
@@ -98,6 +112,7 @@ function PfFundSelectionCard({
           subtitle={bottomSubtitle}
           img={bottomImgSrc}
           id={4}
+          subtitleColor={textColors?.bottomSubtitle}
         />
       </RowContainer>
     </Box>
@@ -117,13 +132,22 @@ const RowContainer = ({ children, ...props }) => {
   );
 };
 
-const CardItem = ({ img, title, subtitle, align, hideTitle, id }) => {
+const CardItem = ({
+  img,
+  title,
+  subtitle,
+  align,
+  hideTitle,
+  id,
+  titleColor,
+  subtitleColor,
+}) => {
   return (
     <Box sx={{ textAlign: align }}>
       {!hideTitle && (
         <Typography
           variant="body5"
-          color="foundationColors.content.secondary"
+          color={titleColor || "foundationColors.content.secondary"}
           dataAid={`key${id}`}
         >
           {title}
@@ -142,9 +166,10 @@ const CardItem = ({ img, title, subtitle, align, hideTitle, id }) => {
           variant={hideTitle ? "body5" : "body2"}
           dataAid={hideTitle ? "subtitle" : `value${id}`}
           color={
-            hideTitle
+            subtitleColor ||
+            (hideTitle
               ? "foundationColors.content.secondary"
-              : "foundationColors.content.primary"
+              : "foundationColors.content.primary")
           }
         >
           {subtitle}
@@ -156,42 +181,26 @@ const CardItem = ({ img, title, subtitle, align, hideTitle, id }) => {
 
 PfFundSelectionCard.defaultProps = {
   checked: false,
-  topTitle: "",
-  topLabel: "",
-  leftTitle: "",
-  leftSubtitle: "",
-  middleTitle: "",
-  middleSubtitle: "",
-  rightTitle: "",
-  rightSubtitle: "",
-  bottomTitle: "",
-  bottomSubtitle: "",
-  bottomLabel: "",
+  textProps: PropTypes.object,
+  textColors: PropTypes.object,
   leftImgSrc: "",
   rightImgSrc: "",
   middleImgSrc: "",
   bottomImgSrc: "",
   onClick: noop,
+  dataAidSuffix: "",
 };
 
 PfFundSelectionCard.propTypes = {
   checked: PropTypes.bool,
-  topTitle: PropTypes.string,
-  topLabel: PropTypes.string,
-  leftTitle: PropTypes.string,
-  leftSubtitle: PropTypes.string,
-  middleTitle: PropTypes.string,
-  middleSubtitle: PropTypes.string,
-  rightTitle: PropTypes.string,
-  rightSubtitle: PropTypes.string,
-  bottomTitle: PropTypes.string,
-  bottomSubtitle: PropTypes.string,
-  bottomLabel: PropTypes.string,
+  textProps: PropTypes.object,
+  textColors: PropTypes.object,
   leftImgSrc: PropTypes.string,
   rightImgSrc: PropTypes.string,
   middleImgSrc: PropTypes.string,
   bottomImgSrc: PropTypes.string,
   onClick: PropTypes.func,
+  dataAidSuffix: PropTypes.string.isRequired,
 };
 
 export default PfFundSelectionCard;
