479f1806c7f1919ef482c6c886e4f11e00f39af3
shashi-varne <shashi.varne04@gmail.com>
2022-06-24 17:12:28 +0530

    Minor fix

--- react_webviews/src/featureComponent/portfolio/PfFundDetailCard/subComponents/TopSection.js
+++ react_webviews/src/featureComponent/portfolio/PfFundDetailCard/subComponents/TopSection.js
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
-import Icon from "../../../../atoms/Icon";
-import Typography from "../../../../atoms/Typography";
+import Icon from "designSystem/atoms/Icon";
+import Typography from "designSystem/atoms/Typography";
 import RowContainer from "./RowContainer";
 
 const TopSection = ({
