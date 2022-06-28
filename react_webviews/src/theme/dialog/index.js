57fc239fdc015a0a8a8a44d443730b5edda489e7
Dhiraj Singh <dhiraj.singh@fisdom.com>
2022-06-28 12:00:30 +0530

    dailog bordeer radius for desktop fixed

--- react_webviews/src/theme/dialog/index.js
+++ react_webviews/src/theme/dialog/index.js
@@ -1,44 +1,45 @@
-import { createTheme, Slide } from '@mui/material';
-import React from 'react';
+import { createTheme, Slide } from "@mui/material";
+import React from "react";
 
 const defaultTheme = createTheme();
 
 const SlideTransition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction='up' ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export const dialogStylesOverride = () => {
   return {
     paper: customPaperStyling,
-    root: customRootStyling
+    root: customRootStyling,
   };
 };
 
 const customRootStyling = (props) => {
-  if (props?.ownerState?.variant === 'bottomsheet') {
+  if (props?.ownerState?.variant === "bottomsheet") {
     return {
       backgroundColor: "transparent !important",
-      [defaultTheme.breakpoints.up('sm')]: {
-        top: "60px !important"
-      }
+      [defaultTheme.breakpoints.up("sm")]: {
+        top: "60px !important",
+      },
     };
   }
-}
+};
 
 const customPaperStyling = (props) => {
-  if (props?.ownerState?.variant === 'bottomsheet') {
+  if (props?.ownerState?.variant === "bottomsheet") {
     return {
-      position: 'fixed !important',
-      bottom: '0 !important',
-      width: '100% !important',
+      position: "fixed !important",
+      bottom: "0 !important",
+      width: "100% !important",
       top: "unset !important",
       margin: "0px !important",
       borderRadius: 12,
       borderBottomLeftRadius: 0,
       borderBottomRightRadius: 0,
-      [defaultTheme.breakpoints.up('sm')]: {
-        bottom: "unset !important"
-      }
+      [defaultTheme.breakpoints.up("sm")]: {
+        bottom: "unset !important",
+        borderRadius: "16px !important",
+      },
     };
   }
 };
